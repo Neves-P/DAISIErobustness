b46e79a7f914c95ba3c60249d4a2e56c40ed05c2
joshwlambert <j.w.l.lambert@rug.nl>
2020-02-13 23:17:55 +0100

    linting and sectioing

--- R/run_robustness.R
+++ R/run_robustness.R
@@ -1,28 +1,26 @@
 #' Run robustness analysis pipeline
 #'
-#' @param parameter_space A string with the parameter space to run.
+#' @param parameter_space A string with the parameter space to run. Can
+#' be \code{"oceanic_ontogeny"}, \code{"oceanic_sea_level"},
+#' \code{"oceanic_ontogeny_sea_level"},
+#' \code{"nonoceanic"}, \code{"nonoceanic_sea_level"}, or
+#' \code{"nonoceanic_land_bridge"}
 #'
 #' @export
 run_robustness <- function(parameter_space) {
 
-  # "oceanic_ontogeny"
-  # "oceanic_sea_level"
-  # "oceanic_ontogeny_sea_level"
-  # "nonoceanic"
-  # "nonoceanic_sea_level"
-  # "nonoceanic_land_bridge"
 
-  file_domain <- "https://raw.githubusercontent.com/Neves-P/DAISIErobustness/master/data/"
+# Selecting parameter space -----------------------------------------------
+  file_domain <-
+    "https://raw.githubusercontent.com/Neves-P/DAISIErobustness/master/data/"
   file <- paste0(file_domain, parameter_space, ".txt")
-
-  ### Pilot ###
   parameters <- readr::read_delim(
     file = file, delim = "  "
   )
 
   set.seed(1)
 
-
+# Initialising objects ----------------------------------------------------
   geodynamics_simulations_output <- list()
   geodynamics_ML_output <- list()
   constant_simulations_output <- list()
@@ -35,6 +33,7 @@ run_robustness <- function(parameter_space) {
   constant_simulations_2_output <- list()
   error <- list()
   baseline_error <- list()
+
   for (l in seq_len(nrow(parameters))) {
     cat("parameter set", l, "\n")
     area_pars <- DAISIE::create_area_pars(
@@ -56,7 +55,8 @@ run_robustness <- function(parameter_space) {
               parameters$laa[l]
     )
     simulation_pars <- DAISIE::create_default_pars(
-      island_ontogeny = DAISIE::translate_island_ontogeny(parameters$island_ontogeny),
+      island_ontogeny = DAISIE::translate_island_ontogeny(
+        parameters$island_ontogeny),
       sea_level = DAISIE::translate_sea_level(parameters$sea_level),
       area_pars = area_pars,
       hyper_pars = hyper_pars,
@@ -74,8 +74,9 @@ run_robustness <- function(parameter_space) {
     simulation_pars$sea_level_frequency <- parameters$sea_level_frequency[l]
     simulation_pars$extcutoff <- parameters$extcutoff
 
-    replicates <- 2
+    replicates <- 1000
 
+# Geodynamics simulations -------------------------------------------------
     geodynamics_simulations <- DAISIE::DAISIE_sim_time_dependent(
       time = simulation_pars$time,
       M = simulation_pars$M,
@@ -92,15 +93,15 @@ run_robustness <- function(parameter_space) {
       sample_freq = Inf
     )
 
+# Conditioning simulation -------------------------------------------------
     stt_rows <- c()
     n_spec <- c()
     n_colonists <- c()
-    for (n_reps in 1:length(geodynamics_simulations)) {
+    for (n_reps in seq_len(geodynamics_simulations)) {
       stt_rows[n_reps] <- nrow(geodynamics_simulations[[n_reps]][[1]]$stt_all)
       n_spec[n_reps] <- as.numeric(geodynamics_simulations[[n_reps]][[1]]$stt_all[stt_rows[n_reps], "present"])
       n_colonists[n_reps] <- length(geodynamics_simulations[[n_reps]])
     }
-
     prop_rep_over_20_spec <- length(which(n_spec >= 20))
     prop_rep_over_5_cols <- length(which(n_colonists > 5))
     if ((prop_rep_over_20_spec / replicates) < 0.95 ||
@@ -108,6 +109,8 @@ run_robustness <- function(parameter_space) {
       print("95% of replicates did not have 20 species or
             did not have 5 colonisation to the present")
     } else {
+
+# Maximum likelihood estimation -------------------------------------------
       geodynamics_ML_results <- list()
       mean_medians <- DAISIE::DAISIE_calc_sumstats_pcrates(
         pars = simulation_pars$pars,
@@ -133,11 +136,13 @@ run_robustness <- function(parameter_space) {
             verbose = 0
           )
         )
-        if (class(geodynamics_ML_results[[i]]) == 'try-error') {
+        if (class(geodynamics_ML_results[[i]]) == "try-error") {
           geodynamics_ML_results[[i]] <- "No convergence"
         }
       }
 
+
+# First constant rate simulations -----------------------------------------
       constant_simulations_1 <- list()
       for (i in seq_along(geodynamics_ML_results)) {
         constant_simulations_1[[i]] <- DAISIE::DAISIE_sim_constant_rate(
@@ -155,8 +160,7 @@ run_robustness <- function(parameter_space) {
         )
       }
 
-
-      # Conduct error -----------------------------------------------------------
+      # Calculate error -----------------------------------------------------------
       error[[l]] <- list()
       n_colonists <- c()
       for (n_reps in 1:replicates) {
@@ -188,9 +192,11 @@ run_robustness <- function(parameter_space) {
         error$num_colonist_error <- num_colonist_geodynamamics - num_colonist_constant_1
       }
 
+
+# Maximum likelihood estimation -------------------------------------------
       constant_results <- list()
       for (i in seq_along(constant_simulations_1)) {
-        for(j in seq_along(constant_simulations_1[[i]]))
+        for (j in seq_along(constant_simulations_1[[i]]))
           try(
             constant_results[[i]] <- DAISIE::DAISIE_ML_CS(
               datalist = constant_simulations_1[[i]][[j]],
@@ -208,12 +214,13 @@ run_robustness <- function(parameter_space) {
               verbose = 0
             )
           )
-        if (class(constant_results[[i]]) == 'try-error') {
+        if (class(constant_results[[i]]) == "try-error") {
           constant_results[[i]] <- "No convergence"
         }
       }
 
 
+# Second constant rate simulations ----------------------------------------
       constant_simulations_2 <- list()
       for (i in seq_along(constant_results)) {
         constant_simulations_2[[i]] <- DAISIE::DAISIE_sim_constant_rate(
@@ -235,9 +242,8 @@ run_robustness <- function(parameter_space) {
       constant_simulations_output[l] <- constant_simulations_1
       constant_results_output[l] <- constant_results
       constant_simulations_2_output[l] <- constant_simulations_2
-      #   save.image(file = "pilot_results.RData")
 
-      # Conduct baseline error --------------------------------------------------
+      # Calculate baseline error --------------------------------------------------
       baseline_error[[l]] <- list()
       for (n_reps in 1:replicates) {
         constant_1_event_times <- constant_simulations_1[[n_reps]][[1]][[1]]$stt_all[, 1]
@@ -256,16 +262,21 @@ run_robustness <- function(parameter_space) {
         stt_last_row_constant_1 <- nrow(constant_simulations_1[[n_reps]][[1]][[1]]$stt_all[, 5])
         num_spec_constant_1 <- constant_simulations_1[[n_reps]][[1]][[1]]$stt_all[stt_last_row_geodynamics, 5]
 
-        stt_last_row_constant_1 <- nrow(constant_simulations_2[[n_reps]][[1]][[1]]$stt_all[, 5])
-        num_spec_constant_1 <- constant_simulations_2[[n_reps]][[1]][[1]]$stt_all[stt_last_row_constant_1, 5]
+        stt_last_row_constant_1 <-
+          nrow(constant_simulations_2[[n_reps]][[1]][[1]]$stt_all[, 5])
+        num_spec_constant_1 <-
+          constant_simulations_2[[n_reps]][[1]][[1]]$stt_all[stt_last_row_constant_1, 5]
 
         #TODO: Better error calculations
         baseline_error$num_spec_error$num_spec_error <- num_spec_geodynamics - num_spec_constant_1
 
 
-        num_colonist_geodynamamics <- length(geodynamics_simulations[[n_reps]]) - 1
-        num_colonist_constant_1 <- length(constant_simulations_1[[n_reps]]) - 1
-        baseline_error$num_colonist_error <- num_colonist_geodynamamics - num_colonist_constant_1
+        num_colonist_geodynamamics <-
+          length(geodynamics_simulations[[n_reps]]) - 1
+        num_colonist_constant_1 <-
+          length(constant_simulations_1[[n_reps]]) - 1
+        baseline_error$num_colonist_error <-
+          num_colonist_geodynamamics - num_colonist_constant_1
       }
     }
   }
@@ -279,7 +290,5 @@ run_robustness <- function(parameter_space) {
     constant_results = constant_results,
     constant_simulations_2 = constant_simulations_2
   )
-
   return(output_list)
-
 }
