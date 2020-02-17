98dd4f1edb7821e14bcef49782d879b8bf40454a
Neves-P <p.m.santos.neves@rug.nl>
2020-02-17 17:50:44 +0100

    Correct typo in sea_level_freq

--- scripts/generate_param_space.R
+++ scripts/generate_param_space.R
@@ -24,21 +24,21 @@ island_gradient_angle <- 0
 extcutoff <- 1000
 x_s <- 0
 x_nonend <- 0
-i <- 2
-DAISIE::DAISIE_calc_sumstats_pcrates(
-  pars = c(lac[i], 1, K[i], gam[i], 1),
-  ext_pars = c(mu_min, mu_p[i]),
-  totaltime = 2.55,
-  area_pars = DAISIE::create_area_pars(
-    max_area = 13500,
-    proportional_peak_t = 0.53,
-    peak_sharpness = 1,
-    total_island_age = 2.864,
-    sea_level_amplitude = 0,
-    sea_level_frequency = 0,
-    island_gradient_angle = 0
-  )
-)
+# i <- 2
+# DAISIE::DAISIE_calc_sumstats_pcrates(
+#   pars = c(lac[i], 1, K[i], gam[i], 1),
+#   ext_pars = c(mu_min, mu_p[i]),
+#   totaltime = 2.55,
+#   area_pars = DAISIE::create_area_pars(
+#     max_area = 13500,
+#     proportional_peak_t = 0.53,
+#     peak_sharpness = 1,
+#     total_island_age = 2.864,
+#     sea_level_amplitude = 0,
+#     sea_level_frequency = 0,
+#     island_gradient_angle = 0
+#   )
+# )
 
 maui_parameter_set <- expand.grid(
   time = time,
@@ -139,7 +139,7 @@ peak_time <- 0
 sharpness <- 0
 total_island_age <- 2.55
 sea_level_amplitude <- 60
-sea_level_frequency <- 2.55
+sea_level_frequency <- 2.55 * 10
 island_gradient_angle <- c(25, 50, 75)
 extcutoff <- 1000
 x_s <- 0
@@ -169,3 +169,28 @@ oceanic_sea_level <- expand.grid(
   stringsAsFactors = FALSE
 )
 write.csv(oceanic_sea_level, "data/oceanic_sea_level.csv", row.names = FALSE)
+
+
+# Oceanic ontogeny sea level ----------------------------------------------
+#
+time <- 2.864
+M <- 1000
+lac <- c(0.0005, 0.00075, 0.001)
+mu_min <- 1
+mu_p <- c(25, 50, 75)
+K <- c(0.001, 0.01, Inf)
+gam <- c(0.00001, 0.00005, 0.0001)
+laa <- c(1, 2)
+island_ontogeny <- "beta"
+sea_level <- "sine"
+max_area <- 13500
+peak_time <- 0.53
+sharpness <- 1
+total_island_age <- 2.864
+sea_level_amplitude <- 60
+sea_level_frequency <- 2.55 * 10
+island_gradient_angle <- c(25, 50, 75)
+extcutoff <- 1000
+x_s <- 0
+x_nonend <- 0
+
