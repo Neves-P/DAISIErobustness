5d852a2c2aae4f208a57d8b6de16ccbbe82ac8fb
joshwlambert <j.w.l.lambert@rug.nl>
2020-02-18 14:40:39 +0100

    updated tests

---
+++ tests/testthat/test-tests.R
@@ -0,0 +1,41 @@
+test_that("oceanic_sea_level works across parameter sets", {
+  expect_equal(2 * 2, 4)
+})
+
+test_that("oceanic_ontogeny_sea_level works across parameter sets", {
+  expect_equal(2 * 2, 4)
+})
+test_that("nonoceanic_sea_level works across parameter sets", {
+  expect_equal(2 * 2, 4)
+})
+
+test_that("nonoceanic_land_bridge works across parameter sets", {
+  expect_equal(2 * 2, 4)
+})
+
+test_that("'time_dep' rates simulations", {
+  expect_equal(2 * 2, 4)
+})
+
+test_that("'rate_shift' rates simulations", {
+  expect_equal(2 * 2, 4)
+})
+
+test_that("oceanic_ontogeny output is correct", {
+  expect_equal(2 * 2, 4)
+})
+
+test_that("oceanic_sea_level output is correct", {
+  expect_equal(2 * 2, 4)
+})
+
+test_that("oceanic_ontogeny_sea_level output is correct", {
+  expect_equal(2 * 2, 4)
+})
+test_that("nonoceanic_sea_level output is correct", {
+  expect_equal(2 * 2, 4)
+})
+
+test_that("nonoceanic_land_bridge output is correct", {
+  expect_equal(2 * 2, 4)
+})
