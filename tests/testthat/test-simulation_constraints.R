9afe06cf52fa672e897d9b2f28877f220e85a9d3
joshwlambert <j.w.l.lambert@rug.nl>
2020-03-02 14:48:48 +0100

    updated simulation_constraints tests

--- tests/testthat/test-simulation_constraints.R
+++ tests/testthat/test-simulation_constraints.R
@@ -2,6 +2,7 @@ context("simulation_constraints")
 
 test_that("test simulation_constraints returns TRUE when simulation
           meets constraints for oceanic_ontogeny", {
+  if (Sys.getenv("TRAVIS") != "" || Sys.getenv("APPVEYOR") != "") {
   param_space <- load_param_space(
     param_space_name = "oceanic_ontogeny")
   set.seed(1)
@@ -20,10 +21,14 @@ test_that("test simulation_constraints returns TRUE when simulation
     replicates = 2)
 
   expect_equal(simulation_constraints, TRUE)
+  } else {
+    skip("Run only on TRAVIS and AppVeyor")
+  }
 })
 
 test_that("test simulation_constraints returns FALSE when simulation
           does not meet constraints for oceanic_ontogeny", {
+  if (Sys.getenv("TRAVIS") != "" || Sys.getenv("APPVEYOR") != "") {
   param_space <- load_param_space(
     param_space_name = "oceanic_ontogeny")
   set.seed(1)
@@ -42,6 +47,9 @@ test_that("test simulation_constraints returns FALSE when simulation
     replicates = 2)
 
   expect_equal(simulation_constraints, FALSE)
+  } else {
+    skip("Run only on TRAVIS and AppVseyor")
+  }
 })
 
 test_that("test simulation_constraints returns TRUE when simulation
@@ -69,6 +77,7 @@ test_that("test simulation_constraints returns TRUE when simulation
 
 test_that("test simulation_constraints returns FALSE when simulation
           does not meet constraints for oceanic_sea_level", {
+  if (Sys.getenv("TRAVIS") != "" || Sys.getenv("APPVEYOR") != "") {
   param_space <- load_param_space(
     param_space_name = "oceanic_sea_level")
   set.seed(1)
@@ -87,32 +96,37 @@ test_that("test simulation_constraints returns FALSE when simulation
     replicates = 2)
 
   expect_equal(simulation_constraints, FALSE)
+  } else {
+    skip("Run only on TRAVIS and AppVeyor")
+  }
 })
 
 test_that("test simulation_constraints returns TRUE when simulation
           meets constraints for oceanic_ontogeny_sea_level", {
-  skip("WIP")
+  skip("cannot find param_set that passes keep looking")
   param_space <- load_param_space(
     param_space_name = "oceanic_ontogeny_sea_level")
   set.seed(1)
   simulation_pars <- extract_param_set(
     param_space_name = "oceanic_ontogeny_sea_level",
     param_space = param_space,
-    param_set = 1)
+    param_set = 25)
   geodynamic_simulations <- geodynamic_simulations(
     param_space_name = "oceanic_ontogeny_sea_level",
     simulation_pars = simulation_pars,
-    replicates = 2)
+    replicates = 2,
+    verbose = FALSE)
 
   simulation_constraints <- simulation_constraints(
-    geodynamic_simulations)
-  expect_true(simulation_constraints)
+    simulations = geodynamic_simulations,
+    replicates = 2)
+
   expect_equal(simulation_constraints, TRUE)
 })
 
 test_that("test simulation_constraints returns FALSE when simulation
           does not meet constraints for oceanic_ontogeny_sea_level", {
-  skip("WIP")
+  if (Sys.getenv("TRAVIS") != "" || Sys.getenv("APPVEYOR") != "") {
   param_space <- load_param_space(
     param_space_name = "oceanic_ontogeny_sea_level")
   set.seed(1)
@@ -123,17 +137,22 @@ test_that("test simulation_constraints returns FALSE when simulation
   geodynamic_simulations <- geodynamic_simulations(
     param_space_name = "oceanic_ontogeny_sea_level",
     simulation_pars = simulation_pars,
-    replicates = 2)
+    replicates = 2,
+    verbose = FALSE)
 
   simulation_constraints <- simulation_constraints(
-    geodynamic_simulations)
-  expect_false(simulation_constraints)
+    simulations = geodynamic_simulations,
+    replicates = 2)
+
   expect_equal(simulation_constraints, FALSE)
+  } else {
+    skip("Run only on TRAVIS and AppVeyor")
+  }
 })
 
 test_that("test simulation_constraints returns TRUE when simulation
           meets constraints for nonoceanic", {
-  skip("WIP")
+  if (Sys.getenv("TRAVIS") != "" || Sys.getenv("APPVEYOR") != "") {
   param_space <- load_param_space(
     param_space_name = "nonoceanic")
   set.seed(1)
@@ -144,60 +163,72 @@ test_that("test simulation_constraints returns TRUE when simulation
   geodynamic_simulations <- geodynamic_simulations(
     param_space_name = "nonoceanic",
     simulation_pars = simulation_pars,
-    replicates = 2)
+    replicates = 2,
+    verbose = FALSE)
 
   simulation_constraints <- simulation_constraints(
-    geodynamic_simulations)
-  expect_true(simulation_constraints)
+    simulations = geodynamic_simulations,
+    replicates = 2)
+
   expect_equal(simulation_constraints, TRUE)
+  } else {
+    skip("Run only on TRAVIS and AppVeyor")
+  }
 })
 
 
 test_that("test simulation_constraints returns FALSE when simulation
           does not meet constraints for nonoceanic", {
-  skip("WIP")
+  if (Sys.getenv("TRAVIS") != "" || Sys.getenv("APPVEYOR") != "") {
   param_space <- load_param_space(
     param_space_name = "nonoceanic")
   set.seed(1)
   simulation_pars <- extract_param_set(
     param_space_name = "nonoceanic",
     param_space = param_space,
-    param_set = 1)
+    param_set = 2)
   geodynamic_simulations <- geodynamic_simulations(
-    param_space_name = "oceanic_ontogeny",
+    param_space_name = "nonoceanic",
     simulation_pars = simulation_pars,
-    replicates = 2)
+    replicates = 2,
+    verbose = FALSE)
 
   simulation_constraints <- simulation_constraints(
-    geodynamic_simulations)
-  expect_false(simulation_constraints)
+    geodynamic_simulations,
+    replicates = 2)
+
   expect_equal(simulation_constraints, FALSE)
+  } else {
+    skip("Run only on TRAVIS and AppVeyor")
+  }
 })
 
 test_that("test simulation_constraints returns TRUE when simulation
           meets constraints for nonoceanic_sea_level", {
-  skip("WIP")
+  skip("Cannot find param_set that passes keep looking")
   param_space <- load_param_space(
     param_space_name = "nonoceanic_sea_level")
   set.seed(1)
   simulation_pars <- extract_param_set(
     param_space_name = "nonoceanic_sea_level",
     param_space = param_space,
-    param_set = 1)
+    param_set = 10)
   geodynamic_simulations <- geodynamic_simulations(
     param_space_name = "nonoceanic_sea_level",
     simulation_pars = simulation_pars,
-    replicates = 2)
+    replicates = 2,
+    verbose = FALSE)
 
   simulation_constraints <- simulation_constraints(
-    geodynamic_simulations)
-  expect_true(simulation_constraints)
+    simulations = geodynamic_simulations,
+    replicates = 2)
+
   expect_equal(simulation_constraints, TRUE)
 })
 
 test_that("test simulation_constraints returns FALSE when simulation
           does not meet constraints for nonoceanic_sea_level", {
-  skip("WIP")
+  if (Sys.getenv("TRAVIS") != "" || Sys.getenv("APPVEYOR") != "") {
   param_space <- load_param_space(
     param_space_name = "nonoceanic_sea_level")
   set.seed(1)
@@ -208,43 +239,67 @@ test_that("test simulation_constraints returns FALSE when simulation
   geodynamic_simulations <- geodynamic_simulations(
     param_space_name = "nonoceanic_sea_level",
     simulation_pars = simulation_pars,
-    replicates = 2)
+    replicates = 2,
+    verbose = FALSE)
 
   simulation_constraints <- simulation_constraints(
-    geodynamic_simulations)
-  expect_false(simulation_constraints)
+    simulations = geodynamic_simulations,
+    replicates = 2)
+
   expect_equal(simulation_constraints, FALSE)
+  } else {
+    skip("Run only on TRAVIS and AppVeyor")
+  }
 })
 
 test_that("test simulation_constraints returns TRUE when simulation meets
-          constraints for oceanic_ontogeny", {
-  skip("WIP")
-  param_space <- load_param_space(param_space_name = "oceanic_ontogeny")
+          constraints for nonoceanic_land_bridge", {
+  if (Sys.getenv("TRAVIS") != "" || Sys.getenv("APPVEYOR") != "") {
+  param_space <- load_param_space(
+    param_space_name = "nonoceanic_land_bridge")
   set.seed(1)
-  simulation_pars <- extract_param_set(param_space_name = "oceanic_ontogeny",
-                                       param_space = param_space,
-                                       param_set = 1)
-  geodynamic_simulations <- geodynamic_simulations(param_space_name = "oceanic_ontogeny",
-                                                   simulation_pars = simulation_pars,
-                                                   replicates = 2)
+  simulation_pars <- extract_param_set(
+    param_space_name = "nonoceanic_land_bridge",
+    param_space = param_space,
+    param_set = 4)
+  geodynamic_simulations <- geodynamic_simulations(
+    param_space_name = "nonoceanic_land_bridge",
+    simulation_pars = simulation_pars,
+    replicates = 2,
+    verbose = FALSE)
 
-  simulation_constraints <- simulation_constraints(geodynamic_simulations)
-  expect_true(simulation_constraints)
+  simulation_constraints <- simulation_constraints(
+    simulations = geodynamic_simulations,
+    replicates = 2)
+
   expect_equal(simulation_constraints, TRUE)
+  } else {
+    skip("Run only on TRAVIS and AppVeyor")
+  }
 })
 
-test_that("test simulation_constraints returns TRUE when simulation meets constraints for oceanic_ontogeny", {
-  skip("WIP")
-  param_space <- load_param_space(param_space_name = "oceanic_ontogeny")
+test_that("test simulation_constraints returns FALSE when simulation meets
+          constraints for nonoceanic_land_bridge", {
+  if (Sys.getenv("TRAVIS") != "" || Sys.getenv("APPVEYOR") != "") {
+  param_space <- load_param_space(
+    param_space_name = "nonoceanic_land_bridge")
   set.seed(1)
-  simulation_pars <- extract_param_set(param_space_name = "oceanic_ontogeny",
-                                       param_space = param_space,
-                                       param_set = 1)
-  geodynamic_simulations <- geodynamic_simulations(param_space_name = "oceanic_ontogeny",
-                                                   simulation_pars = simulation_pars,
-                                                   replicates = 2)
+  simulation_pars <- extract_param_set(
+    param_space_name = "nonoceanic_land_bridge",
+    param_space = param_space,
+    param_set = 1)
+  geodynamic_simulations <- geodynamic_simulations(
+    param_space_name = "nonoceanic_land_bridge",
+    simulation_pars = simulation_pars,
+    replicates = 2,
+    verbose = FALSE)
 
-  simulation_constraints <- simulation_constraints(geodynamic_simulations)
-  expect_true(simulation_constraints)
-  expect_equal(simulation_constraints, TRUE)
+  simulation_constraints <- simulation_constraints(
+    simulations = geodynamic_simulations,
+    replicates = 2)
+
+  expect_equal(simulation_constraints, FALSE)
+  } else {
+    skip("Run only on TRAVIS and AppVeyor")
+  }
 })
