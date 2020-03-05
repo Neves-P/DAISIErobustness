c0772335a464dbe88fe7599777cc47d3f4d4c40b
Neves-P <p.m.santos.neves@rug.nl>
2020-03-05 16:50:33 +0100

    Fix test-simulation_constraints

--- tests/testthat/test-simulation_constraints.R
+++ tests/testthat/test-simulation_constraints.R
@@ -4,20 +4,20 @@ test_that("test simulation_constraints returns TRUE when simulation
           meets constraints for oceanic_ontogeny", {
   if (Sys.getenv("TRAVIS") != "" || Sys.getenv("APPVEYOR") != "") {
     #geodynamic simulation output from oceanic_ontogeny param_set 1
-    stt_all <- matrix(nrow = 112, ncol = 5)
-    colnames(stt_all) <- c("Time", "nI", "nA", "nC", "present")
-    stt_all[1, ] <- c(2.54999999999999982, 0, 0, 0, 0)
-    stt_all[2, ] <- c(2.53852689750492555, 1, 0, 0, 1)
-    stt_all[3, ] <- c(2.53419858304282020, 0, 0, 0, 0)
-    stt_all[29, ] <- c(1.381159255315790979, 1, 0, 10, 6)
-    stt_all[41, ] <- c(0.970857228597442390, 4, 4, 8, 11)
-    stt_all[95, ] <- c(0.167138343063989225, 3, 7, 15, 15)
-    stt_all[105, ] <- c(0.032361981288045794, 3, 5, 9, 11)
-    stt_all[106, ] <- c(0.024259422462705960, 2, 5, 9, 10)
-    stt_all[107, ] <- c(0.000000000000000000, 2, 5, 9, 10)
+    stt_all_rep_1 <- matrix(nrow = 112, ncol = 5)
+    colnames(stt_all_rep_1) <- c("Time", "nI", "nA", "nC", "present")
+    stt_all_rep_1[1, ] <- c(2.54999999999999982, 0, 0, 0, 0)
+    stt_all_rep_1[2, ] <- c(2.53852689750492555, 1, 0, 0, 1)
+    stt_all_rep_1[3, ] <- c(2.53419858304282020, 0, 0, 0, 0)
+    stt_all_rep_1[29, ] <- c(1.381159255315790979, 1, 0, 10, 6)
+    stt_all_rep_1[41, ] <- c(0.970857228597442390, 4, 4, 8, 11)
+    stt_all_rep_1[95, ] <- c(0.167138343063989225, 3, 7, 15, 15)
+    stt_all_rep_1[105, ] <- c(0.032361981288045794, 3, 5, 9, 11)
+    stt_all_rep_1[106, ] <- c(0.024259422462705960, 2, 5, 9, 10)
+    stt_all_rep_1[107, ] <- c(0.000000000000000000, 2, 5, 9, 10)
     replicate_1_island_info <- list(island_age = 2.55,
                                     not_present = 990,
-                                    stt_all = stt_all)
+                                    stt_all = stt_all_rep_1)
     replicate_1_taxon_list_1 <- list(branching_times = c(2.55, 0.16583840374981),
                                      stac = 4,
                                      missing_species = 0)
@@ -57,17 +57,125 @@ test_that("test simulation_constraints returns TRUE when simulation
                                                           0.31633454531757),
                                       stac = 2,
                                       missing_species = 0)
-    fake_geodynamic_simulations <- list(replicate_1_island_info,
-                                        replicate_1_taxon_list_1,
-                                        replicate_1_taxon_list_2,
-                                        replicate_1_taxon_list_3,
-                                        replicate_1_taxon_list_4,
-                                        replicate_1_taxon_list_5,
-                                        replicate_1_taxon_list_6,
-                                        replicate_1_taxon_list_7,
-                                        replicate_1_taxon_list_8,
-                                        replicate_1_taxon_list_9,
-                                        replicate_1_taxon_list_10)
+
+    stt_all_rep_2 <- matrix(nrow = 112, ncol = 5)
+    colnames(stt_all_rep_2) <- c("Time", "nI", "nA", "nC", "present")
+    stt_all_rep_2[1, ] <- c(2.54999999999999982, 0, 0, 0, 0)
+    stt_all_rep_2[2, ] <- c(2.535442543402314, 1, 0, 0, 1)
+    stt_all_rep_2[3, ] <- c(2.5136776976105741, 2, 0, 0, 2)
+    stt_all_rep_2[29, ] <- c(1.2536692059058565, 6, 3, 6, 11)
+    stt_all_rep_2[41, ] <- c(1.098239318217729, 5, 5, 6, 12)
+    stt_all_rep_2[95, ] <- c(0.15660341198161953, 8, 4, 20, 20)
+    stt_all_rep_2[105, ] <- c(0.062880806820883262, 5, 6, 14, 17)
+    stt_all_rep_2[106, ] <- c(0.058923041353504679, 5, 5, 14, 16)
+    stt_all_rep_2[107, ] <- c(0.049614706925862784, 5, 6, 12, 16)
+    stt_all_rep_2[112, ] <- c(0, 4, 7, 14, 17)
+    replicate_2_island_info <- list(island_age = 2.55,
+                                    not_present = 983,
+                                    stt_all = stt_all_rep_2)
+    replicate_2_taxon_list_1 <- list(branching_times = c(2.55, 1.3646216142922600),
+                                     stac = 2,
+                                     missing_species = 0)
+    replicate_2_taxon_list_2 <- list(branching_times = c(2.55, 0.18979746459221999),
+                                     stac = 4,
+                                     missing_species = 0)
+    replicate_2_taxon_list_3 <- list(branching_times = c(2.55, 1.3814870390776499),
+                                     stac = 2,
+                                     missing_species = 0)
+    replicate_2_taxon_list_4 <- list(branching_times = c(2.55,
+                                                         0.2287328720092800),
+                                     stac = 2,
+                                     missing_species = 0)
+    replicate_2_taxon_list_5 <- list(branching_times = c(2.55,
+                                                         0.502890928033639995,
+                                                         0.025014655426829801),
+                                     stac = 2,
+                                     missing_species = 0)
+    replicate_2_taxon_list_6 <- list(branching_times = c(2.55,
+                                                         1.0867871355265399),
+                                     stac = 2,
+                                     missing_species = 0)
+    replicate_2_taxon_list_7 <- list(branching_times = c(2.55,
+                                                         2.20946446172892985,
+                                                         0.18718010795970999),
+                                     stac = 2,
+                                     missing_species = 0)
+    replicate_2_taxon_list_8 <- list(branching_times = c(2.55,
+                                                         1.9398675492035500),
+                                     stac = 4,
+                                     missing_species = 0)
+    replicate_2_taxon_list_9 <- list(branching_times = c(2.55,
+                                                         0.2556480294376100),
+                                     stac = 2,
+                                     missing_species = 0)
+    replicate_2_taxon_list_10 <- list(branching_times = c(2.55,
+                                                          0.049216575819319701,
+                                                          0.041751277381669699),
+                                      stac = 2,
+                                      missing_species = 0)
+    replicate_2_taxon_list_11 <- list(branching_times = c(2.55,
+                                                          1.4542575571389600),
+                                      stac = 2,
+                                      missing_species = 0)
+    replicate_2_taxon_list_12 <- list(branching_times = c(2.55,
+                                                          0.78094350930355005,
+                                                          0.53119896449050996),
+                                      stac = 2,
+                                      missing_species = 0)
+    replicate_2_taxon_list_13 <- list(branching_times = c(2.55,
+                                                          1.750438149273400024,
+                                                          0.825128332064629988,
+                                                          0.630073756670819995,
+                                                          0.076288965344079696),
+                                      stac = 2,
+                                      missing_species = 0)
+    replicate_2_taxon_list_14 <- list(branching_times = c(2.54999999999999982,
+                                                          0.26286072054514997),
+                                      stac = 4,
+                                      missing_species = 0)
+    replicate_2_taxon_list_15 <- list(branching_times = c(2.55,
+                                                          0.88312928676604996),
+                                      stac = 4,
+                                      missing_species = 0)
+    replicate_2_taxon_list_16 <- list(branching_times = c(2.55,
+                                                          1.76549203903469998,
+                                                          0.25762452140046999),
+                                      stac = 2,
+                                      missing_species = 0)
+    replicate_2_taxon_list_17 <- list(branching_times = c(2.55,
+                                                          0.34139873571694002),
+                                      stac = 2,
+                                      missing_species = 0)
+    fake_geodynamic_simulations <- list(list(replicate_1_island_info,
+                                             replicate_1_taxon_list_1,
+                                             replicate_1_taxon_list_2,
+                                             replicate_1_taxon_list_3,
+                                             replicate_1_taxon_list_4,
+                                             replicate_1_taxon_list_5,
+                                             replicate_1_taxon_list_6,
+                                             replicate_1_taxon_list_7,
+                                             replicate_1_taxon_list_8,
+                                             replicate_1_taxon_list_9,
+                                             replicate_1_taxon_list_10),
+                                        list(
+                                          replicate_2_island_info,
+                                          replicate_2_taxon_list_1,
+                                          replicate_2_taxon_list_2,
+                                          replicate_2_taxon_list_3,
+                                          replicate_2_taxon_list_4,
+                                          replicate_2_taxon_list_5,
+                                          replicate_2_taxon_list_6,
+                                          replicate_2_taxon_list_7,
+                                          replicate_2_taxon_list_8,
+                                          replicate_2_taxon_list_9,
+                                          replicate_2_taxon_list_10,
+                                          replicate_2_taxon_list_11,
+                                          replicate_2_taxon_list_12,
+                                          replicate_2_taxon_list_13,
+                                          replicate_2_taxon_list_14,
+                                          replicate_2_taxon_list_15,
+                                          replicate_2_taxon_list_16,
+                                          replicate_2_taxon_list_17))
 
 
   simulation_constraints <- simulation_constraints(
