c3a35216ad5b759029cacc27acefb759e3840f89
Neves-P <p.m.santos.neves@rug.nl>
2020-03-08 16:55:06 +0100

    Correct folder selection for submit_robustness

--- scripts/submit_robustness.R
+++ scripts/submit_robustness.R
@@ -5,20 +5,29 @@
 #' \code{"oceanic_ontogeny_sea_level"},
 #' \code{"nonoceanic_sea_level"}, or
 #' @param account A string with the p-number of the user to submit the jobs.
+#' @param session A session to pass, if already open.
+#' @param replicates How many replicates to run per parameter set.
+#' @param num_job Number of job to run. Corresponds to a row in the param_space
+#'   tibble.
+#' @param partition What Peregrine HPCC to use. "gelifes" and "regular" are
+#'   available.
+#' @param folder What Peregrine HPCC folder to use. Defaults to "data", but
+#'   "home" is also allowed.
 #'   Requires access to the Peregrine HPCC at the University of Groningen.
-#' @param upload_scripts Boolean to determine whether scripts should be
-#' uploaded with every parameter space.
-#'
 #' @export
 submit_robustness <- function(param_space_name,
                               account,
                               session = NA,
                               replicates = 100,
                               num_job = c(1, 100),
-                              partition = "gelifes") {
-
+                              partition = "gelifes",
+                              folder = 'data') {
+  testit::assert(is.character(folder))
+  if(substr(folder, start = 1, stop = 1) != "/"){
+    folder <- paste0('/', folder)
+  }
   remotes::install_github("Giappo/jap@pedro")
-  jap::upload_jap_scripts(account = account, session = NA)
+  jap::upload_jap_scripts(account = account, session = NA, folder = folder)
 
   session <- jap::open_session(account = account)
 
@@ -30,6 +39,7 @@ submit_robustness <- function(param_space_name,
       account = account,
       session = session,
       partition = partition,
+      folder = folder,
       fun_arguments = paste0(
         "param_space_name = '",
         param_space_name,
