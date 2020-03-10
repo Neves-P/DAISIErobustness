0c70695fc4df90233469bf37ff5c7839ce6a94d4
joshwlambert <j.w.l.lambert@rug.nl>
2020-03-10 12:24:48 +0000

    removed non-functioning tests and added skips to test-oceanic_simulation

--- tests/testthat/test-oceanic_simulation.R
+++ tests/testthat/test-oceanic_simulation.R
@@ -2,7 +2,6 @@ context("oceanic_simulations")
 
 test_that("test oceanic simulation output is correct for
           oceanic_ontogeny", {
-  if (Sys.getenv("TRAVIS") != "" || Sys.getenv("APPVEYOR") != "") {
   #ML output from oceanic_ontogeny param_set 1
   geodynamic_ml <- list()
   geodynamic_ml[[1]] <- c(0.4270512648004822,
@@ -44,7 +43,6 @@ test_that("test oceanic simulation output is correct for
     ml = geodynamic_ml,
     simulation_pars = simulation_pars,
     verbose = FALSE)
-
   expect_length(oceanic_simulations, 2)
   expect_length(oceanic_simulations[[1]][[1]], 9)
   expect_equal(oceanic_simulations[[1]][[1]][[1]]$island_age, 2.55)
@@ -64,85 +62,16 @@ test_that("test oceanic simulation output is correct for
                c(2.55, 0.79436892889236))
   expect_equal(oceanic_simulations[[2]][[1]][[2]]$stac, 4)
   expect_equal(oceanic_simulations[[2]][[1]][[2]]$missing_species, 0)
-  } else {
-    skip("Run only on TRAVIS and AppVeyor")
-  }
 })
 
 test_that("test oceanic simulation output is correct for
           oceanic_sea_level", {
-  skip("WIP")
-  param_space <- load_param_space(
-    param_space_name = "oceanic_sea_level")
-  set.seed(1)
-  simulation_pars <- extract_param_set(
-    param_space_name = "oceanic_sea_level",
-    param_space = param_space,
-    param_set = 1)
-  ml <- #make ml from a run
-  oceanic_simulations <- oceanic_simulations(
-    ml = ml,
-    simulation_pars = simulation_pars)
-
-  expect_length(oceanic_simulations, 2)
-  expect_length(oceanic_simulations[[1]], 100)
-  expect_equal(oceanic_simulations[[1]][[1]]$island_age, 2.55)
-  expect_equal(oceanic_simulations[[1]][[1]]$not_present, 900)
-  expect_matrix(oceanic_simulations[[1]][[1]]$stt_all)
-  expect_equal(nrow(oceanic_simulations[[1]][[1]]$stt_all), 600)
-  expect_equal(ncol(oceanic_simulations[[1]][[1]]$stt_all), 5)
-  expect_equal(oceanic_simulations[[1]][[2]]$branching_times,
-               c(2.55, 0.5))
-  expect_equal(oceanic_simulations[[1]][[2]]$stac, 2)
-  expect_equal(oceanic_simulations[[1]][[2]]$missing_species, 0)
-  expect_length(oceanic_simulations[[2]], 100)
-  expect_equal(oceanic_simulations[[2]][[1]]$island_age, 2.55)
-  expect_equal(oceanic_simulations[[2]][[1]]$not_present, 900)
-  expect_matrix(oceanic_simulations[[2]][[1]]$stt_all)
-  expect_equal(nrow(oceanic_simulations[[2]][[1]]$stt_all), 600)
-  expect_equal(ncol(oceanic_simulations[[2]][[1]]$stt_all), 5)
-  expect_equal(oceanic_simulations[[2]][[2]]$branching_times,
-               c(2.55, 0.5))
-  expect_equal(oceanic_simulations[[2]][[2]]$stac, 2)
-  expect_equal(oceanic_simulations[[2]][[2]]$missing_species, 0)
+  skip("cannot find param_set that passes conditioning")
 })
 
 test_that("test oceanic simulation output is correct for
           oceanic_ontogeny_sea_level", {
-  skip("WIP")
-  param_space <- load_param_space(
-    param_space_name = "oceanic_ontogeny_sea_level")
-  set.seed(1)
-  simulation_pars <- extract_param_set(
-    param_space_name = "oceanic_ontogeny_sea_level",
-    param_space = param_space,
-    param_set = 1)
-  ml <- #make ml from a run
-  oceanic_simulations <- oceanic_simulations(
-    ml = ml,
-    simulation_pars = simulation_pars)
-
-  expect_length(oceanic_simulations, 2)
-  expect_length(oceanic_simulations[[1]], 100)
-  expect_equal(oceanic_simulations[[1]][[1]]$island_age, 2.55)
-  expect_equal(oceanic_simulations[[1]][[1]]$not_present, 900)
-  expect_matrix(oceanic_simulations[[1]][[1]]$stt_all)
-  expect_equal(nrow(oceanic_simulations[[1]][[1]]$stt_all), 600)
-  expect_equal(ncol(oceanic_simulations[[1]][[1]]$stt_all), 5)
-  expect_equal(oceanic_simulations[[1]][[2]]$branching_times,
-               c(2.55, 0.5))
-  expect_equal(oceanic_simulations[[1]][[2]]$stac, 2)
-  expect_equal(oceanic_simulations[[1]][[2]]$missing_species, 0)
-  expect_length(oceanic_simulations[[2]], 100)
-  expect_equal(oceanic_simulations[[2]][[1]]$island_age, 2.55)
-  expect_equal(oceanic_simulations[[2]][[1]]$not_present, 900)
-  expect_matrix(oceanic_simulations[[2]][[1]]$stt_all)
-  expect_equal(nrow(oceanic_simulations[[2]][[1]]$stt_all), 600)
-  expect_equal(ncol(oceanic_simulations[[2]][[1]]$stt_all), 5)
-  expect_equal(oceanic_simulations[[2]][[2]]$branching_times,
-               c(2.55, 0.5))
-  expect_equal(oceanic_simulations[[2]][[2]]$stac, 2)
-  expect_equal(oceanic_simulations[[2]][[2]]$missing_species, 0)
+  skip("cannot find param_set that passes conditioning")
 })
 
 test_that("test oceanic simulation output is correct for nonoceanic", {
@@ -187,7 +116,6 @@ test_that("test oceanic simulation output is correct for nonoceanic", {
     ml = geodynamic_ml,
     simulation_pars = simulation_pars,
     verbose = FALSE)
-
   expect_length(oceanic_simulations, 2)
   expect_length(oceanic_simulations[[1]][[1]], 32)
   expect_equal(oceanic_simulations[[1]][[1]][[1]]$island_age, 2.55)
@@ -211,76 +139,10 @@ test_that("test oceanic simulation output is correct for nonoceanic", {
 
 test_that("test oceanic simulation output is correct for
           nonoceanic_sea_level", {
-  skip("WIP")
-  param_space <- load_param_space(
-    param_space_name = "nonoceanic_sea_level")
-  set.seed(1)
-  simulation_pars <- extract_param_set(
-    param_space_name = "nonoceanic_sea_level",
-    param_space = param_space,
-    param_set = 1)
-  ml <- #make ml from a run
-  oceanic_simulations <- oceanic_simulations(
-    ml = ml,
-    simulation_pars = simulation_pars)
-
-  expect_length(oceanic_simulations, 2)
-  expect_length(oceanic_simulations[[1]], 100)
-  expect_equal(oceanic_simulations[[1]][[1]]$island_age, 2.55)
-  expect_equal(oceanic_simulations[[1]][[1]]$not_present, 900)
-  expect_matrix(oceanic_simulations[[1]][[1]]$stt_all)
-  expect_equal(nrow(oceanic_simulations[[1]][[1]]$stt_all), 600)
-  expect_equal(ncol(oceanic_simulations[[1]][[1]]$stt_all), 5)
-  expect_equal(oceanic_simulations[[1]][[2]]$branching_times,
-               c(2.55, 0.5))
-  expect_equal(oceanic_simulations[[1]][[2]]$stac, 2)
-  expect_equal(oceanic_simulations[[1]][[2]]$missing_species, 0)
-  expect_length(oceanic_simulations[[2]], 100)
-  expect_equal(oceanic_simulations[[2]][[1]]$island_age, 2.55)
-  expect_equal(oceanic_simulations[[2]][[1]]$not_present, 900)
-  expect_matrix(oceanic_simulations[[2]][[1]]$stt_all)
-  expect_equal(nrow(oceanic_simulations[[2]][[1]]$stt_all), 600)
-  expect_equal(ncol(oceanic_simulations[[2]][[1]]$stt_all), 5)
-  expect_equal(oceanic_simulations[[2]][[2]]$branching_times,
-               c(2.55, 0.5))
-  expect_equal(oceanic_simulations[[2]][[2]]$stac, 2)
-  expect_equal(oceanic_simulations[[2]][[2]]$missing_species, 0)
+  skip("cannot find param_set that passes conditioning")
 })
 
 test_that("test oceanic simulation output is correct for
           nonoceanic_land_bridge", {
-  skip("WIP")
-  param_space <- load_param_space(
-    param_space_name = "nonoceanic_land_bridge")
-  set.seed(1)
-  simulation_pars <- extract_param_set(
-    param_space_name = "nonoceanic_land_bridge",
-    param_space = param_space,
-    param_set = 1)
-  ml <- #make ml from a run
-  oceanic_simulation <- oceanic_simulations(
-    ml = ml,
-    simulation_pars = simulation_pars)
-
-  expect_length(oceanic_simulations, 2)
-  expect_length(oceanic_simulations[[1]], 100)
-  expect_equal(oceanic_simulations[[1]][[1]]$island_age, 2.55)
-  expect_equal(oceanic_simulations[[1]][[1]]$not_present, 900)
-  expect_matrix(oceanic_simulations[[1]][[1]]$stt_all)
-  expect_equal(nrow(oceanic_simulations[[1]][[1]]$stt_all), 600)
-  expect_equal(ncol(oceanic_simulations[[1]][[1]]$stt_all), 5)
-  expect_equal(oceanic_simulations[[1]][[2]]$branching_times,
-               c(2.55, 0.5))
-  expect_equal(oceanic_simulations[[1]][[2]]$stac, 2)
-  expect_equal(oceanic_simulations[[1]][[2]]$missing_species, 0)
-  expect_length(oceanic_simulations[[2]], 100)
-  expect_equal(oceanic_simulations[[2]][[1]]$island_age, 2.55)
-  expect_equal(oceanic_simulations[[2]][[1]]$not_present, 900)
-  expect_matrix(oceanic_simulations[[2]][[1]]$stt_all)
-  expect_equal(nrow(oceanic_simulations[[2]][[1]]$stt_all), 600)
-  expect_equal(ncol(oceanic_simulations[[2]][[1]]$stt_all), 5)
-  expect_equal(oceanic_simulations[[2]][[2]]$branching_times,
-               c(2.55, 0.5))
-  expect_equal(oceanic_simulations[[2]][[2]]$stac, 2)
-  expect_equal(oceanic_simulations[[2]][[2]]$missing_species, 0)
+  skip("cannot find param_set that passes conditioning")
 })
