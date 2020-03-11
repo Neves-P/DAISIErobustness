a2650559b7cc217379433e6af2196ab26f9c2197
Neves-P <p.m.santos.neves@rug.nl>
2020-03-11 18:33:25 +0100

    Add DAISIE_plot_areas

--- R/default_params_doc.R
+++ R/default_params_doc.R
@@ -24,8 +24,41 @@
 #' \code{\link{DAISIE_sim_constant_rate}},
 #' \code{\link{DAISIE_sim_time_dependent}} or
 #' \code{\link{DAISIE_sim_constant_rate_shift}}
+#' @param totaltime_1 Numeric with time for which island 1 would be simulated.
+#'   Usually this will correspond to a time that's smaller than the total
+#'   island age.
+#' @param totaltime_2 Time for which island 2 would be simulated. Usually this
+#'   will correspond to a time that's smaller than the total island age.
+#'   Defaults to \code{NULL}, in which case this island is not plotted.
+#' @param area_pars_1 List of area and sea-level parameters for island 1 as
+#'   created by \code{\link[DAISIE]{create_area_pars}}.
+#' @param area_pars_2 List of area and sea-level parameters for island 1 as
+#'   created by \code{\link[DAISIE]{create_area_pars}}. Defaults to \code{NULL},
+#'   in which case the second area is not plotted.
+#' @param island_ontogeny_1 A string describing the type of
+#'   island ontogeny of island 1.
+#'   Can be \code{"const"}, \code{"beta"} for a beta function describing area
+#'   through time.
+#' @param island_ontogeny_2 A string describing the type of
+#'   island ontogeny of island 2.
+#'   Can be \code{"const"}, \code{"beta"} for a beta function describing area
+#'   through time. Defaults to \code{NULL}, in which case the second island is
+#'   not plotted.
+#' @param sea_level_1 A string describing the type of sea level for island 1.
+#' Can be \code{"const"} or \code{"sine"} for a sine function describing area
+#' through time.
+#' @param sea_level_2 A string describing the type of sea level for island 2.
+#'   Can be \code{"const"} or \code{"sine"} for a sine function describing area
+#'   through time. Defaults to \code{NULL}, in which case the second island is
+#'   not plotted.
+#' @param resolution A float between 0 and 1 which controls the resolution of
+#'   the island area calculated for plotting. Defaults to \code{0.001} and is
+#'   bound at \code{0.00001} to prevent unwieldy slowdowns.
+#' @param overlay_sea_level Logical, controling if sea level curves
+#'   should be overlaid on top of corresponding ontogeny curves.
 #' @param verbose logical determining whether to print
-#' simulation statements to the console.
+#'   simulation statements to the console.
+#'
 #'
 #' @return Nothing
 #'
@@ -39,6 +72,16 @@ default_params_doc <- function(
   ml,
   simulations_1,
   simulations_2,
+  totaltime_1,
+  totaltime_2,
+  area_pars_1,
+  area_pars_2,
+  island_ontogeny_1,
+  island_ontogeny_2,
+  sea_level_1,
+  sea_level_2,
+  resolution,
+  overlay_sea_level,
   verbose
 ) {
   # Nothing
