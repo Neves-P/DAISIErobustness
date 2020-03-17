d0b8073313639027bea39de9087bd7462a6f95c4
Neves-P <p.m.santos.neves@rug.nl>
2020-03-17 22:37:55 +0100

    verbose = FALSE supresses more output in calc_ml

--- R/calc_ml.R
+++ R/calc_ml.R
@@ -9,12 +9,29 @@ calc_ml <- function(simulations,
   ml <- list()
   if (verbose == TRUE) {
     verbose <- 1
+    for (i in seq_along(simulations)) {
+      message(paste0("Running ML ", i, " of ", length(simulations)))
+      try(
+        ml[[i]] <- DAISIE::DAISIE_ML_CS(
+          datalist = simulations[[i]][[1]],
+          datatype = "single",
+          initparsopt = c(1, 1, 40, 0.01, 1),
+          idparsopt = c(1:5),
+          parsfix = NULL,
+          idparsfix = NULL,
+          verbose = 0
+        )
+      )
+      if (class(ml[[i]]) == "try-error") {
+        ml[[i]] <- "No convergence"
+      }
+    }
   } else {
     verbose <- 0
-  }
   for (i in seq_along(simulations)) {
+    message(paste0("Running ML ", i, " of ", length(simulations)))
     try(
-      ml[[i]] <- DAISIE::DAISIE_ML_CS(
+      invisible(capture.output(ml[[i]] <- DAISIE::DAISIE_ML_CS(
         datalist = simulations[[i]][[1]],
         datatype = "single",
         initparsopt = c(1, 1, 40, 0.01, 1),
@@ -22,11 +39,12 @@ calc_ml <- function(simulations,
         parsfix = NULL,
         idparsfix = NULL,
         verbose = 0
-      )
+      )))
     )
     if (class(ml[[i]]) == "try-error") {
       ml[[i]] <- "No convergence"
     }
+  }
   }
   return(ml)
 }
