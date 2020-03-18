5e658751c2231f12a83389ea19cd944245922279
joshwlambert <j.w.l.lambert@rug.nl>
2020-03-18 21:57:40 +0000

    removed num_spec and num_col from calc_js_dist and updated calc_js_dist tests

--- R/calc_js_dist.R
+++ R/calc_js_dist.R
@@ -12,18 +12,6 @@ calc_js_dist <- function(spec_error,
                          endemic_baseline_error,
                          nonendemic_baseline_error) {
 
-  num_spec_error <- spec_error$num_spec_error
-  num_spec_baseline_error <- spec_baseline_error$num_spec_error
-  js_dist_num_spec <- sqrt(calc_js_div(num_spec_error, num_spec_baseline_error))
-  testit::assert(js_dist_num_spec >= 0)
-  testit::assert(js_dist_num_spec <= 1)
-
-  num_col_error <- spec_error$num_col_error
-  num_col_baseline_error <- spec_baseline_error$num_col_error
-  js_dist_num_col <- sqrt(calc_js_div(num_col_error, num_col_baseline_error))
-  testit::assert(js_dist_num_col >= 0)
-  testit::assert(js_dist_num_col <= 1)
-
   spec_nltt <- spec_error$nltt
   spec_baseline_nltt <- spec_baseline_error$nltt
   js_dist_spec_nltt <- sqrt(calc_js_div(spec_nltt, spec_baseline_nltt))
@@ -43,9 +31,7 @@ calc_js_dist <- function(spec_error,
   testit::assert(js_dist_nonendemic_nltt >= 0)
   testit::assert(js_dist_nonendemic_nltt <= 1)
 
-  js_dist <- list(js_dist_num_spec = js_dist_num_spec,
-                  js_dist_num_col = js_dist_num_col,
-                  js_dist_spec_nltt = js_dist_spec_nltt,
+  js_dist <- list(js_dist_spec_nltt = js_dist_spec_nltt,
                   js_dist_endemic_nltt = js_dist_endemic_nltt,
                   js_dist_nonendemic_nltt = js_dist_nonendemic_nltt)
 
@@ -59,7 +45,8 @@ calc_js_dist <- function(spec_error,
 #' @return numeric
 #' @author Pedro Neves, Joshua Lambert
 calc_js_div <- function(p, q) {
-  js_div <- 0.5 * (calc_kl_div(p, (p + q) / 2) + calc_kl_div(q, (p + q) / 2))
+  js_div <- 0.5 * calc_kl_div(p, (p + q) * 0.5) +
+            0.5 * calc_kl_div(q, (p + q) * 0.5)
   return(js_div)
 }
 
--- tests/testthat/test-calc_js_dist.R
+++ tests/testthat/test-calc_js_dist.R
@@ -1,38 +1,160 @@
 context("calc_js_dist, calc_js_div, calc_kl_div")
 
 test_that("test calc_js_dist output is correct for oceanic_ontogeny", {
-  skip("WIP")
   #errors output from oceanic_ontogeny param_set 2 with seed 1
-  spec_error <- list(num_spec_error = 0,
-                     num_col_error = 0)
-  endemic_error <- list(0)
-  nonendemic_error <- list(0)
-  spec_baseline_error <- list(0)
-  endemic_baseline_error <- list(0)
-  nonendemic_baseline_error <- list(0)
+  spec_error <- list(nltt = c(3.466459645659721, 16.779762104341131))
+  endemic_error <- list(nltt = c(4.105857769709307, 9.843170592071166))
+  nonendemic_error <- list(nltt = c(12.19399299074907, 20.13397293941591))
+
+  #baseline errors output from oceanic_ontogeny param_set 2 with seed 1
+  spec_baseline_error <- list(nltt = c())
+  endemic_baseline_error <- list(nltt = c())
+  nonendemic_baseline_error <- list(nltt = c())
+
   js_dist <- calc_js_dist(spec_error = spec_error,
                           endemic_error = endemic_error,
                           nonendemic_error = nonendemic_error,
                           spec_baseline_error = spec_baseline_error,
                           endemic_baseline_error = endemic_baseline_error,
                           nonendemic_baseline_error = nonendemic_baseline_error)
-    expect_length(js_dist, 5)
-    expect_equal(js_dist, list(js_dist_num_spec = 0,
-                               js_dist_num_col = 0,
-                               js_dist_spec_nltt = 0,
-                               js_dist_endemic_nltt = 0,
-                               js_dist_nonendemic_nltt = 0))
+
+  expect_length(js_dist, 3)
+  expect_equal(js_dist, list(js_dist_spec_nltt = 0,
+                             js_dist_endemic_nltt = 0,
+                             js_dist_nonendemic_nltt = 0))
+})
+
+test_that("test calc_js_dist output is correct for oceanic_sea_level", {
+  #errors output from oceanic_sea_level param_set 3 with seed 1
+  spec_error <- list(nltt = c(12.51245689554037, 5.248395705769793))
+  endemic_error <- list(nltt = c(6.251267253365500, 2.302925917732638))
+  nonendemic_error <- list(nltt = c(12.69555403808047, 21.94389485833001))
+
+  #baseline errors output from oceanic_sea_level param_set 3 with seed 1
+
+  spec_baseline_error <- list(0)
+  endemic_baseline_error <- list(0)
+  nonendemic_baseline_error <- list(0)
+
+  js_dist <- calc_js_dist(spec_error = spec_error,
+                          endemic_error = endemic_error,
+                          nonendemic_error = nonendemic_error,
+                          spec_baseline_error = spec_baseline_error,
+                          endemic_baseline_error = endemic_baseline_error,
+                          nonendemic_baseline_error = nonendemic_baseline_error)
+
+  expect_length(js_dist, 3)
+  expect_equal(js_dist, list(js_dist_spec_nltt = 0,
+                             js_dist_endemic_nltt = 0,
+                             js_dist_nonendemic_nltt = 0))
+})
+
+test_that("test calc_js_dist output is correct for
+          oceanic_ontogeny_sea_level", {
+  #errors output from oceanic_ontogeny param_set 2 with seed 1
+  spec_error <- list(nltt = c(3.466459645659721, 16.779762104341131))
+  endemic_error <- list(nltt = c(4.105857769709307, 9.843170592071166))
+  nonendemic_error <- list(nltt = c(12.19399299074907, 20.13397293941591))
+
+  #baseline errors output from oceanic_ontogeny param_set 2 with seed 1
+
+  spec_baseline_error <- list(0)
+  endemic_baseline_error <- list(0)
+  nonendemic_baseline_error <- list(0)
+
+  js_dist <- calc_js_dist(spec_error = spec_error,
+                          endemic_error = endemic_error,
+                          nonendemic_error = nonendemic_error,
+                          spec_baseline_error = spec_baseline_error,
+                          endemic_baseline_error = endemic_baseline_error,
+                          nonendemic_baseline_error = nonendemic_baseline_error)
+
+  expect_length(js_dist, 5)
+  expect_equal(js_dist, list(js_dist_spec_nltt = 0,
+                             js_dist_endemic_nltt = 0,
+                             js_dist_nonendemic_nltt = 0))
+})
+
+test_that("test calc_js_dist output is correct for oceanic_ontogeny", {
+  #errors output from oceanic_ontogeny param_set 2 with seed 1
+  spec_error <- list(nltt = c(3.466459645659721, 16.779762104341131))
+  endemic_error <- list(nltt = c(4.105857769709307, 9.843170592071166))
+  nonendemic_error <- list(nltt = c(12.19399299074907, 20.13397293941591))
+
+  #baseline errors output from oceanic_ontogeny param_set 2 with seed 1
+
+  spec_baseline_error <- list(0)
+  endemic_baseline_error <- list(0)
+  nonendemic_baseline_error <- list(0)
+
+  js_dist <- calc_js_dist(spec_error = spec_error,
+                          endemic_error = endemic_error,
+                          nonendemic_error = nonendemic_error,
+                          spec_baseline_error = spec_baseline_error,
+                          endemic_baseline_error = endemic_baseline_error,
+                          nonendemic_baseline_error = nonendemic_baseline_error)
+
+  expect_length(js_dist, 5)
+  expect_equal(js_dist, list(js_dist_spec_nltt = 0,
+                             js_dist_endemic_nltt = 0,
+                             js_dist_nonendemic_nltt = 0))
+})
+
+test_that("test calc_js_dist output is correct for oceanic_ontogeny", {
+  #errors output from oceanic_ontogeny param_set 2 with seed 1
+  spec_error <- list(nltt = c(3.466459645659721, 16.779762104341131))
+  endemic_error <- list(nltt = c(4.105857769709307, 9.843170592071166))
+  nonendemic_error <- list(nltt = c(12.19399299074907, 20.13397293941591))
+
+  #baseline errors output from oceanic_ontogeny param_set 2 with seed 1
+
+  spec_baseline_error <- list(0)
+  endemic_baseline_error <- list(0)
+  nonendemic_baseline_error <- list(0)
+
+  js_dist <- calc_js_dist(spec_error = spec_error,
+                          endemic_error = endemic_error,
+                          nonendemic_error = nonendemic_error,
+                          spec_baseline_error = spec_baseline_error,
+                          endemic_baseline_error = endemic_baseline_error,
+                          nonendemic_baseline_error = nonendemic_baseline_error)
+
+  expect_length(js_dist, 5)
+  expect_equal(js_dist, list(js_dist_spec_nltt = 0,
+                             js_dist_endemic_nltt = 0,
+                             js_dist_nonendemic_nltt = 0))
+})
+
+test_that("test calc_js_dist output is correct for oceanic_ontogeny", {
+  #errors output from oceanic_ontogeny param_set 2 with seed 1
+  spec_error <- list(nltt = c(3.466459645659721, 16.779762104341131))
+  endemic_error <- list(nltt = c(4.105857769709307, 9.843170592071166))
+  nonendemic_error <- list(nltt = c(12.19399299074907, 20.13397293941591))
+
+  #baseline errors output from oceanic_ontogeny param_set 2 with seed 1
+
+  spec_baseline_error <- list(0)
+  endemic_baseline_error <- list(0)
+  nonendemic_baseline_error <- list(0)
+
+  js_dist <- calc_js_dist(spec_error = spec_error,
+                          endemic_error = endemic_error,
+                          nonendemic_error = nonendemic_error,
+                          spec_baseline_error = spec_baseline_error,
+                          endemic_baseline_error = endemic_baseline_error,
+                          nonendemic_baseline_error = nonendemic_baseline_error)
+
+  expect_length(js_dist, 5)
+  expect_equal(js_dist, list(js_dist_spec_nltt = 0,
+                             js_dist_endemic_nltt = 0,
+                             js_dist_nonendemic_nltt = 0))
 })
 
 test_that("test calc_js_dist output is correct", {
-  spec_error <- list(num_spec_error = c(1, 1, 1, 1, 1),
-                     num_col_error = c(1, 1, 1, 1, 1),
-                     nltt = c(1, 1, 1, 1, 1))
+  spec_error <- list(nltt = c(1, 1, 1, 1, 1))
   endemic_error <- list(nltt = c(1, 1, 1, 1, 1))
   nonendemic_error <- list(nltt = c(1, 1, 1, 1, 1))
-  spec_baseline_error <- list(num_spec_error = c(1, 1, 1, 1, 1),
-                              num_col_error = c(1, 1, 1, 1, 1),
-                              nltt = c(1, 1, 1, 1, 1))
+  spec_baseline_error <- list(nltt = c(1, 1, 1, 1, 1))
   endemic_baseline_error <- list(nltt = c(1, 1, 1, 1, 1))
   nonendemic_baseline_error <- list(nltt = c(1, 1, 1, 1, 1))
   js_dist <- calc_js_dist(spec_error = spec_error,
@@ -41,20 +163,14 @@ test_that("test calc_js_dist output is correct", {
                           spec_baseline_error = spec_baseline_error,
                           endemic_baseline_error = endemic_baseline_error,
                           nonendemic_baseline_error = nonendemic_baseline_error)
-  expect_equal(js_dist$js_dist_num_spec, 0)
-  expect_equal(js_dist$js_dist_num_col, 0)
   expect_equal(js_dist$js_dist_spec_nltt, 0)
   expect_equal(js_dist$js_dist_endemic_nltt, 0)
   expect_equal(js_dist$js_dist_nonendemic_nltt, 0)
 
-  spec_error <- list(num_spec_error = c(2, 2, 2, 2, 2),
-                     num_col_error = c(2, 2, 2, 2, 2),
-                     nltt = c(2, 2, 2, 2, 2))
+  spec_error <- list(nltt = c(2, 2, 2, 2, 2))
   endemic_error <- list(nltt = c(2, 2, 2, 2, 2))
   nonendemic_error <- list(nltt = c(2, 2, 2, 2, 2))
-  spec_baseline_error <- list(num_spec_error = c(1, 1, 1, 1, 1),
-                              num_col_error = c(1, 1, 1, 1, 1),
-                              nltt = c(1, 1, 1, 1, 1))
+  spec_baseline_error <- list(nltt = c(1, 1, 1, 1, 1))
   endemic_baseline_error <- list(nltt = c(1, 1, 1, 1, 1))
   nonendemic_baseline_error <- list(nltt = c(1, 1, 1, 1, 1))
   js_dist <- calc_js_dist(spec_error = spec_error,
@@ -63,11 +179,19 @@ test_that("test calc_js_dist output is correct", {
                           spec_baseline_error = spec_baseline_error,
                           endemic_baseline_error = endemic_baseline_error,
                           nonendemic_baseline_error = nonendemic_baseline_error)
-  expect_true(js_dist$js_dist_num_spec != 0)
-  expect_true(js_dist$js_dist_num_col != 0)
   expect_true(js_dist$js_dist_spec_nltt != 0)
   expect_true(js_dist$js_dist_endemic_nltt != 0)
   expect_true(js_dist$js_dist_nonendemic_nltt != 0)
+})
+
+test_that("test calc_js_dist is between 0 and 1 when distributions sum to 1", {
+  error <- rnorm(n = 10, mean = 10)
+  error <- error / sum(error)
+  baseline_error <- rnorm(n = 10, mean = 10)
+  baseline_error <- baseline_error / sum(baseline_error)
+  js_dist <- calc_js_div(error, baseline_error)
+  expect_true(js_dist >= 0)
+  expect_true(js_dist <= 1)
 })
 
 test_that("test calc_kl_div output is correct", {
