f4e04c7b087951dfdf50452341a04263526047dc
joshwlambert <j.w.l.lambert@rug.nl>
2020-03-19 21:02:35 +0000

    removed simulation_pars argument from calc_error

--- R/calc_error.R
+++ R/calc_error.R
@@ -6,11 +6,9 @@
 #' @return A list with four error metrics
 #' @family error calculations
 #' @export
-calc_error <- function(
-  simulation_pars,
-  simulations_1,
-  simulations_2,
-  replicates) {
+calc_error <- function(simulations_1,
+                       simulations_2,
+                       replicates) {
   spec_error <- list()
   for (n_reps in 1:replicates) {
     simulations_1_event_times <-
@@ -52,47 +50,47 @@ calc_error <- function(
       abs(num_col_simulations_1 - num_col_simulations_2)
   }
 
-endemic_error <- list()
-for (n_reps in 1:replicates) {
-  simulations_1_event_times <- simulations_1[[n_reps]][[1]][[1]]$stt_all[, 1]
-  simulations_1_endemic_spec <- simulations_1[[n_reps]][[1]][[1]]$stt_all[, 2]
-  simulations_2_event_times <- simulations_2[[n_reps]][[1]][[1]]$stt_all[, 1]
-  simulations_2_endemic_spec <- simulations_2[[n_reps]][[1]][[1]]$stt_all[, 2]
-  endemic_error$nltt[n_reps] <- nLTT::nltt_diff_exact_extinct(
-    event_times = simulations_1_event_times,
-    species_number = simulations_1_endemic_spec,
-    event_times2 = simulations_2_event_times,
-    species_number2 = simulations_2_endemic_spec,
-    distance_method = "abs",
-    time_unit = "ago",
-    normalize = FALSE
-  )
-}
+  endemic_error <- list()
+  for (n_reps in 1:replicates) {
+    simulations_1_event_times <- simulations_1[[n_reps]][[1]][[1]]$stt_all[, 1]
+    simulations_1_endemic_spec <- simulations_1[[n_reps]][[1]][[1]]$stt_all[, 2]
+    simulations_2_event_times <- simulations_2[[n_reps]][[1]][[1]]$stt_all[, 1]
+    simulations_2_endemic_spec <- simulations_2[[n_reps]][[1]][[1]]$stt_all[, 2]
+    endemic_error$nltt[n_reps] <- nLTT::nltt_diff_exact_extinct(
+      event_times = simulations_1_event_times,
+      species_number = simulations_1_endemic_spec,
+      event_times2 = simulations_2_event_times,
+      species_number2 = simulations_2_endemic_spec,
+      distance_method = "abs",
+      time_unit = "ago",
+      normalize = FALSE
+    )
+  }
 
-nonendemic_error <- list()
-for (n_reps in 1:replicates) {
-  simulations_1_event_times <-
-    simulations_1[[n_reps]][[1]][[1]]$stt_all[, 1]
-  simulations_1_nonendemic_spec <-
-    simulations_1[[n_reps]][[1]][[1]]$stt_all[, 3] +
-    simulations_1[[n_reps]][[1]][[1]]$stt_all[, 4]
-  simulations_2_event_times <-
-    simulations_2[[n_reps]][[1]][[1]]$stt_all[, 1]
-  simulations_2_nonendemic_spec <-
-    simulations_2[[n_reps]][[1]][[1]]$stt_all[, 3] +
-    simulations_2[[n_reps]][[1]][[1]]$stt_all[, 4]
-  nonendemic_error$nltt[n_reps] <- nLTT::nltt_diff_exact_extinct(
-    event_times = simulations_1_event_times,
-    species_number = simulations_1_nonendemic_spec,
-    event_times2 = simulations_2_event_times,
-    species_number2 = simulations_2_nonendemic_spec,
-    distance_method = "abs",
-    time_unit = "ago",
-    normalize = FALSE
-  )
-}
-return(
-  list(spec_error = spec_error,
-       endemic_error = endemic_error,
-       nonendemic_error = nonendemic_error))
+  nonendemic_error <- list()
+  for (n_reps in 1:replicates) {
+    simulations_1_event_times <-
+      simulations_1[[n_reps]][[1]][[1]]$stt_all[, 1]
+    simulations_1_nonendemic_spec <-
+      simulations_1[[n_reps]][[1]][[1]]$stt_all[, 3] +
+      simulations_1[[n_reps]][[1]][[1]]$stt_all[, 4]
+    simulations_2_event_times <-
+      simulations_2[[n_reps]][[1]][[1]]$stt_all[, 1]
+    simulations_2_nonendemic_spec <-
+      simulations_2[[n_reps]][[1]][[1]]$stt_all[, 3] +
+      simulations_2[[n_reps]][[1]][[1]]$stt_all[, 4]
+    nonendemic_error$nltt[n_reps] <- nLTT::nltt_diff_exact_extinct(
+      event_times = simulations_1_event_times,
+      species_number = simulations_1_nonendemic_spec,
+      event_times2 = simulations_2_event_times,
+      species_number2 = simulations_2_nonendemic_spec,
+      distance_method = "abs",
+      time_unit = "ago",
+      normalize = FALSE
+    )
+  }
+  return(
+    list(spec_error = spec_error,
+         endemic_error = endemic_error,
+         nonendemic_error = nonendemic_error))
 }
