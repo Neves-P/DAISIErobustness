e80997490bd240e802c0ccc27beb462a59f558bd
joshwlambert <j.w.l.lambert@rug.nl>
2020-03-20 20:44:59 +0000

    removed tolerances from calc_ml tests bar one

--- tests/testthat/test-calc_ml.R
+++ tests/testthat/test-calc_ml.R
@@ -16,32 +16,21 @@ test_that("test calc_ml output is correct with geodynamic simulations", {
     geodynamic_ml <- calc_ml(
       simulations = geodynamic_simulations)
     expect_length(geodynamic_ml, 2)
-    expect_equal(geodynamic_ml[[1]]$lambda_c, 0.9516893216529831,
-                 tolerance = 0.01)
-    expect_equal(geodynamic_ml[[1]]$mu, 0.4371724756929532,
-                 tolerance = 0.01)
-    expect_equal(geodynamic_ml[[1]]$K, 26.45933105611771,
-                 tolerance = 0.01)
-    expect_equal(geodynamic_ml[[1]]$gamma, 0.005832693735317008,
-                 tolerance = 0.01)
-    expect_equal(geodynamic_ml[[1]]$lambda_a, 1.91311658602524,
-                 tolerance = 0.01)
-    expect_equal(geodynamic_ml[[1]]$loglik, -93.6464134227662,
-                 tolerance = 0.01)
+    expect_equal(geodynamic_ml[[1]]$lambda_c, 0.9516893216529831)
+    expect_equal(geodynamic_ml[[1]]$mu, 0.4371724756929532)
+    expect_equal(geodynamic_ml[[1]]$K, 26.45933105611771)
+    expect_equal(geodynamic_ml[[1]]$gamma, 0.005832693735317008)
+    expect_equal(geodynamic_ml[[1]]$lambda_a, 1.91311658602524)
+    expect_equal(geodynamic_ml[[1]]$loglik, -93.6464134227662)
     expect_equal(geodynamic_ml[[1]]$df, 5)
     expect_equal(geodynamic_ml[[1]]$conv, 0)
-    expect_equal(geodynamic_ml[[2]]$lambda_c, 1.17346081209476,
-                 tolerance = 0.01)
-    expect_equal(geodynamic_ml[[2]]$mu, 0.9780652536369839,
-                 tolerance = 0.01)
+    expect_equal(geodynamic_ml[[2]]$lambda_c, 1.17346081209476)
+    expect_equal(geodynamic_ml[[2]]$mu, 0.9780652536369839)
     expect_equal(geodynamic_ml[[2]]$K, 1.798109468901113,
                  tolerance = 0.05)
-    expect_equal(geodynamic_ml[[2]]$gamma, 0.01258190594811776,
-                 tolerance = 0.01)
-    expect_equal(geodynamic_ml[[2]]$lambda_a, 0.8917175650839453,
-                 tolerance = 0.01)
-    expect_equal(geodynamic_ml[[2]]$loglik, -133.3346436904723,
-                 tolerance = 0.01)
+    expect_equal(geodynamic_ml[[2]]$gamma, 0.01258190594811776)
+    expect_equal(geodynamic_ml[[2]]$lambda_a, 0.8917175650839453)
+    expect_equal(geodynamic_ml[[2]]$loglik, -133.3346436904723)
     expect_equal(geodynamic_ml[[2]]$df, 5)
     expect_equal(geodynamic_ml[[2]]$conv, 0)
   } else {
@@ -82,32 +71,20 @@ test_that("test calc_ml output is correct with oceanic simulations", {
     oceanic_ml <- calc_ml(
       simulations = oceanic_simulations_1)
     expect_length(oceanic_ml, 2)
-    expect_equal(oceanic_ml[[1]]$lambda_c, 0.4703865020955945,
-                 tolerance = 0.01)
-    expect_equal(oceanic_ml[[1]]$mu, 0.3024123467825012,
-                 tolerance = 0.01)
-    expect_equal(oceanic_ml[[1]]$K, 0.9604172665676682,
-                 tolerance = 0.01)
-    expect_equal(oceanic_ml[[1]]$gamma, 0.005654402886347983,
-                 tolerance = 0.01)
-    expect_equal(oceanic_ml[[1]]$lambda_a, 1.256814199481393,
-                 tolerance = 0.01)
-    expect_equal(oceanic_ml[[1]]$loglik, -86.44387709521121,
-                 tolerance = 0.01)
+    expect_equal(oceanic_ml[[1]]$lambda_c, 0.4703865020955945)
+    expect_equal(oceanic_ml[[1]]$mu, 0.3024123467825012)
+    expect_equal(oceanic_ml[[1]]$K, 0.9604172665676682)
+    expect_equal(oceanic_ml[[1]]$gamma, 0.005654402886347983)
+    expect_equal(oceanic_ml[[1]]$lambda_a, 1.256814199481393)
+    expect_equal(oceanic_ml[[1]]$loglik, -86.44387709521121)
     expect_equal(oceanic_ml[[1]]$df, 5)
     expect_equal(oceanic_ml[[1]]$conv, 0)
-    expect_equal(oceanic_ml[[2]]$lambda_c, 1.266996227093286,
-                 tolerance = 0.01)
-    expect_equal(oceanic_ml[[2]]$mu, 1.360311534579718,
-                 tolerance = 0.01)
-    expect_equal(oceanic_ml[[2]]$K, 4.29713623613709,
-                 tolerance = 0.01)
-    expect_equal(oceanic_ml[[2]]$gamma, 0.01871138624408656,
-                 tolerance = 0.01)
-    expect_equal(oceanic_ml[[2]]$lambda_a, 7.741644634429056e-08,
-                 tolerance = 0.01)
-    expect_equal(oceanic_ml[[2]]$loglik, -139.8897801706968,
-                 tolerance = 0.01)
+    expect_equal(oceanic_ml[[2]]$lambda_c, 1.266996227093286)
+    expect_equal(oceanic_ml[[2]]$mu, 1.360311534579718)
+    expect_equal(oceanic_ml[[2]]$K, 4.29713623613709)
+    expect_equal(oceanic_ml[[2]]$gamma, 0.01871138624408656)
+    expect_equal(oceanic_ml[[2]]$lambda_a, 7.741644634429056e-08)
+    expect_equal(oceanic_ml[[2]]$loglik, -139.8897801706968)
     expect_equal(oceanic_ml[[2]]$df, 5)
     expect_equal(oceanic_ml[[2]]$conv, 0)
   } else {
