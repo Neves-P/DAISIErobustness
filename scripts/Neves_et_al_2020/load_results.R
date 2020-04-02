96eb42c7b226e016eca1ff0b666430a9473dc7af
Neves-P <p.m.santos.neves@rug.nl>
2020-04-02 17:10:50 +0200

    indent load_results

--- scripts/Neves_et_al_2020/load_results.R
+++ scripts/Neves_et_al_2020/load_results.R
@@ -14,24 +14,24 @@ load_results <- function(param_space_name) {
       str_output_file[[i]] <- "failed_sim_constraints"
       for (j in seq_along(output_file$geodynamic_sim)) {
         num_spec_file[j] <- unname(output_file$geodynamic_sim[[j]][[1]][[1]]$stt_all[nrow(output_file$geodynamic_sim[[j]][[1]][[1]]$stt_all), 2] +
-          output_file$geodynamic_sim[[j]][[1]][[1]]$stt_all[nrow(output_file$geodynamic_sim[[j]][[1]][[1]]$stt_all), 3] +
-          output_file$geodynamic_sim[[j]][[1]][[1]]$stt_all[nrow(output_file$geodynamic_sim[[j]][[1]][[1]]$stt_all), 4])
+                                     output_file$geodynamic_sim[[j]][[1]][[1]]$stt_all[nrow(output_file$geodynamic_sim[[j]][[1]][[1]]$stt_all), 3] +
+                                     output_file$geodynamic_sim[[j]][[1]][[1]]$stt_all[nrow(output_file$geodynamic_sim[[j]][[1]][[1]]$stt_all), 4])
         num_col_file[j] <- unname(output_file$geodynamic_sim[[j]][[1]][[1]]$stt_all[nrow(output_file$geodynamic_sim[[j]][[1]][[1]]$stt_all), 5])
       }
       num_spec[[i]] <- num_spec_file
       num_col[[i]] <- num_col_file
     }
-      if (length_output_file == 4) { #failed ml_constraints
-        str_output_file[[i]] <- "failed_ml_constraints"
-      }
+    if (length_output_file == 4) { #failed ml_constraints
+      str_output_file[[i]] <- "failed_ml_constraints"
+    }
   }
 
   failed_sim_replicates <- length(which(str_output_file == "failed_sim_constraints"))
   failed_ml_replicates <- length(which(str_output_file == "failed_ml_constraints"))
   return(list(
-      failed_sim_replicates = failed_sim_replicates,
-      failed_ml_replicates = failed_ml_replicates,
-      num_spec = num_spec,
-      num_col = num_col
-    ))
+    failed_sim_replicates = failed_sim_replicates,
+    failed_ml_replicates = failed_ml_replicates,
+    num_spec = num_spec,
+    num_col = num_col
+  ))
 }
