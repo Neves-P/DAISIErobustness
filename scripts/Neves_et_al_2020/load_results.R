ec98cf72347f5429c418607139c6f7b7e1c0b95d
Neves-P <p.m.santos.neves@rug.nl>
2020-04-02 17:10:13 +0200

    num_spec and num_col are vectors in load_results

--- scripts/Neves_et_al_2020/load_results.R
+++ scripts/Neves_et_al_2020/load_results.R
@@ -5,18 +5,18 @@ load_results <- function(param_space_name) {
   str_output_file <- list()
   num_spec <- list()
   num_col <- list()
-  num_spec_file <- list()
-  num_col_file <- list()
+  num_spec_file <- c()
+  num_col_file <- c()
   for (i in seq_along(results_file_names)) {
     load(file.path(path, results_file_names)[i])
     length_output_file <- length(output_file)
     if (length_output_file == 1) { # failed sim_constraints
       str_output_file[[i]] <- "failed_sim_constraints"
       for (j in seq_along(output_file$geodynamic_sim)) {
-        num_spec_file[[j]] <- unname(output_file$geodynamic_sim[[j]][[1]][[1]]$stt_all[nrow(output_file$geodynamic_sim[[j]][[1]][[1]]$stt_all), 2] +
+        num_spec_file[j] <- unname(output_file$geodynamic_sim[[j]][[1]][[1]]$stt_all[nrow(output_file$geodynamic_sim[[j]][[1]][[1]]$stt_all), 2] +
           output_file$geodynamic_sim[[j]][[1]][[1]]$stt_all[nrow(output_file$geodynamic_sim[[j]][[1]][[1]]$stt_all), 3] +
           output_file$geodynamic_sim[[j]][[1]][[1]]$stt_all[nrow(output_file$geodynamic_sim[[j]][[1]][[1]]$stt_all), 4])
-        num_col_file[[j]] <- unname(output_file$geodynamic_sim[[j]][[1]][[1]]$stt_all[nrow(output_file$geodynamic_sim[[j]][[1]][[1]]$stt_all), 5])
+        num_col_file[j] <- unname(output_file$geodynamic_sim[[j]][[1]][[1]]$stt_all[nrow(output_file$geodynamic_sim[[j]][[1]][[1]]$stt_all), 5])
       }
       num_spec[[i]] <- num_spec_file
       num_col[[i]] <- num_col_file
