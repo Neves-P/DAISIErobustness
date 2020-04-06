da4a18135121d21564f1e2a093baba01f7b54f73
Neves-P <p.m.santos.neves@rug.nl>
2020-04-06 16:32:45 +0200

    Rename load_results to get_run_stats. Add load_results

--- scripts/Neves_et_al_2020/load_results.R
+++ scripts/Neves_et_al_2020/load_results.R
@@ -1,5 +1,12 @@
-
-load_results <- function(param_space_name) {
+#' Simple statistics of pipeline runs
+#'
+#' @inheritParams default_params_doc
+#'
+#' @return A list with a vector per parameter set with each replicate's number
+#'   of species at the present, number of colonizing lineages at the present,
+#'   number of failed MLs and number of failed sim constraints
+#' @author Joshua Lambert, Pedro Neves
+get_run_stats <- function(param_space_name) {
   path <- file.path("results", param_space_name)
   results_file_names <- list.files(path = path)
   str_output_file <- list()
@@ -37,21 +44,56 @@ load_results <- function(param_space_name) {
 }
 
 
+#' Ratio of many entries have less than 15 species
+#'
+#' @param metadata num_spec or num_col as returned by get_run_stats
+#'
+#' @return A numeric with the ratio
+#'
+#' @examples
+# ratios_less_15 <- lapply(out$num_spec, count_less_15) # Ratio
 count_less_15 <- function(metadata) {
   sum(as.numeric(metadata < 15)) / length(metadata)
 }
 
-ratios_less_15 <- lapply(out$num_spec, count_less_15) # Ratio
-mean_num_spec <- lapply(out$num_spec, mean) # Ratio
-sd_num_spec <- lapply(out$num_spec, sd) # Ratio
-hist(unlist(ratios_less_15))
-hist(unlist(out$num_spec))
-hist(unlist(mean_num_spec), xlim = c(0, 150), breaks = 100)
-hist(unlist(sd_num_spec))
-
+#' Ratio of many entries have more than 15 species
+#'
+#' @param metadata num_spec or num_col as returned by get_run_stats
+#'
+#' @return A numeric with the ratio
+#'
+#' @examples
+# ratios_over_100 <- lapply(out$num_spec, count_over_100) # Ratio
 count_over_100 <- function(metadata) {
   sum(as.numeric(metadata > 100)) / length(metadata)
 }
-# Ratio of over 100 species, per param set
-lapply(out$num_spec, count_over_100)
+
+
+
+#' Load all results of a param_space
+#'
+#' @param param_space_name
+#'
+#' @return A list with the loaded results
+load_results <- function(param_space_name) {
+  results_list <- list()
+  path <- file.path("results", param_space_name)
+  results_file_names <- list.files(path = path)
+  for (i in seq_along(results_file_names)) {
+    param_set_name <- tools::file_path_sans_ext(results_file_names[i])
+    load(file.path(path, results_file_names)[i])
+    results_list[[param_set_name]] <- assign(
+      param_set_name,
+      value = output_file
+    )
+  }
+  results_list
+}
+
+# mean_num_spec <- lapply(out$num_spec, mean) # Ratio
+# sd_num_spec <- lapply(out$num_spec, sd) # Ratio
+# hist(unlist(ratios_less_15))
+# hist(unlist(out$num_spec))
+# hist(unlist(mean_num_spec), xlim = c(0, 100), breaks = 300)
+# hist(unlist(sd_num_spec))
 
