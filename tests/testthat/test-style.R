ce657110d5b4ba26c8e3947f98ef34587ed7a3f9
Neves-P <p.m.santos.neves@rug.nl>
2020-05-22 14:37:58 +0200

    Suggest lintr and nicer lintr expectation

--- tests/testthat/test-style.R
+++ tests/testthat/test-style.R
@@ -1,3 +1,6 @@
-test_that("Package style", {
-  lintr::expect_lint_free()
-})
+if (requireNamespace("lintr", quietly = TRUE)) {
+  context("lints")
+  test_that("Package Style", {
+    lintr::expect_lint_free()
+  })
+}
