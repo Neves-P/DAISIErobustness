3f6460a479c78aefc581703a8ddba9af4430b440
Neves-P <p.m.santos.neves@rug.nl>
2020-06-10 19:38:16 +0200

    oceanic_sim is not exported

--- R/oceanic_sim.R
+++ R/oceanic_sim.R
@@ -3,7 +3,6 @@
 #' @inheritParams default_params_doc
 #' @author Joshua Lambert, Pedro Neves, Shu Xie
 #' @return Output from \code{\link[DAISIE]{DAISIE_sim_constant_rate}()}
-#' @export
 oceanic_sim <- function(ml,
                         sim_pars) {
   oceanic_sim <- list()
