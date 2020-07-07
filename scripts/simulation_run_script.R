38a83aa0f90bb1c4fda8baff75ddc59c2ff69538
Neves-P <p.m.santos.neves@rug.nl>
2020-07-07 11:49:57 +0200

    Convert simulation script to function

--- scripts/simulation_run_script.R
+++ scripts/simulation_run_script.R
@@ -1,99 +1,38 @@
-# Template for the geodynamics study nonoceanic has already ran
-## First load up all the parameter spaces in the study
-
-#### oceanic_ontogeny
-param_space_name_oceanic_ontogeny <- "oceanic_ontogeny"
-# Create params for the experiment
-param_space_oceanic_ontogeny <- DAISIErobustness::load_param_space(
-  param_space_name = param_space_name_oceanic_ontogeny
-)
+run_simulations_peregrine <- function(param_space_name,
+                                      cluster_partition = "gelifes") {
 
 
-#### oceanic_sea_level
-param_space_name_oceanic_sea_level <- "oceanic_sea_level"
-# Create params for the experiment
-param_space_oceanic_sea_level <- DAISIErobustness::load_param_space(
-  param_space_name = param_space_name_oceanic_sea_level
-)
+  # Create params for the experiment
+  param_space <- DAISIErobustness::load_param_space(
+    param_space_name = param_space_name
+  )
 
-#### oceanic_ontogeny_sea_level
-param_space_name_oceanic_ontogeny_sea_level <- "oceanic_ontogeny_sea_level"
-# Create params for the experiment
-param_space_oceanic_ontogeny_sea_level <- DAISIErobustness::load_param_space(
-  param_space_name = param_space_name_oceanic_ontogeny_sea_level
-)
 
-#### nonoceanic
-param_space_name_nonoceanic <- "nonoceanic"
-# Create params for the experiment
-param_space_nonoceanic <- DAISIErobustness::load_param_space(
-  param_space_name = param_space_name_nonoceanic
-)
+  ## Calculate number of jobs to run
+  total_runs <- nrow(param_space)
 
-#### nonoceanic_sea_level
-param_space_name_nonoceanic_sea_level <- "nonoceanic_sea_level"
-# Create params for the experiment
-param_space_nonoceanic_sea_level <- DAISIErobustness::load_param_space(
-  param_space_name = param_space_name_nonoceanic_sea_level
-)
+  # Make params list
+  params <- vector("list", total_runs)
 
-#### oceanic_sea_level
-param_space_name_nonoceanic_land_bridge <- "nonoceanic_land_bridge"
-# Create params for the experiment
-param_space_nonoceanic_land_bridge <- DAISIErobustness::load_param_space(
-  param_space_name = param_space_name_nonoceanic_land_bridge
-)
+  # Populate params list with necessary arguments
+  for (i in seq_len(total_runs)) {
+    params[[i]] <- list(
+      param_space_name = param_space_name,
+      param_set = i,
+      replicates = 1000,
+      pipeline = "novel_sim",
+      save_output = TRUE
+    )
+  }
 
-## Calculate number of jobs to run
-total_runs <- nrow(param_space_oceanic_ontogeny) +
-  nrow(param_space_oceanic_ontogeny_sea_level) +
-  nrow(param_space_oceanic_sea_level) +
-  nrow(param_space_nonoceanic) +
-  nrow(param_space_nonoceanic_sea_level) +
-  nrow(param_space_nonoceanic_land_bridge)
-
-# Make params list
-params <- vector("list", total_runs)
-
-# Make indices of each job
-indices <- c(
-  seq(nrow(param_space_oceanic_ontogeny)),
-  seq(nrow(param_space_oceanic_ontogeny_sea_level)),
-  seq(nrow(param_space_oceanic_sea_level)),
-  seq(nrow(param_space_nonoceanic)),
-  seq(nrow(param_space_nonoceanic_sea_level)),
-  seq(nrow(param_space_nonoceanic_land_bridge))
-)
-
-# Assign a param_space_name to each job
-param_space_names <- c(
-  rep("oceanic_ontogeny", nrow(param_space_oceanic_ontogeny)),
-  rep("oceanic_ontogeny_sea_level", nrow(param_space_oceanic_ontogeny_sea_level)),
-  rep("oceanic_sea_level", nrow(param_space_oceanic_sea_level)),
-  rep("nonoceanic", nrow(param_space_nonoceanic)),
-  rep("nonoceanic_sea_level", nrow(param_space_nonoceanic_sea_level)),
-  rep("nonoceanic_land_bridge", nrow(param_space_nonoceanic_land_bridge))
-)
-
-# Populate params list with necessary arguments
-for (i in seq_len(total_runs)) {
-  params[[i]] <- list(
-    param_space_name = param_space_names[i],
-    param_set = indices[i],
-    replicates = 1000,
-    pipeline = "novel_sim",
-    save_output = TRUE
+  # Start RStudio job that submits cluster jobs
+  jap::pocket_experiment(
+    github_name = "Neves-P",
+    project_name = "DAISIErobustness",
+    function_name = "run_robustness",
+    params = params,
+    cluster_folder = "data",
+    cluster_partition = cluster_partition,
+    delete_on_cluster = FALSE
   )
 }
-
-# Start RStudio job that submits cluster jobs
-jap::pocket_experiment(
-  github_name = "Neves-P",
-  project_name = "DAISIErobustness",
-  function_name = "run_robustness",
-  params = params,
-  cluster_folder = "data",
-  cluster_partition = "gelifes",
-  delete_on_cluster = FALSE
-)
-
