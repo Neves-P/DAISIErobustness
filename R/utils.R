99ae9e934176502c71bd9a005eb4cbba74e12d52
Neves-P <p.m.santos.neves@rug.nl>
2020-08-05 14:13:53 +0200

    Safer choose.dir() use

--- R/utils.R
+++ R/utils.R
@@ -6,7 +6,9 @@
 #' @author Joshua Lambert, Pedro Neves
 get_available_results <- function(param_space_name) {
   testit::assert(is.character(param_space_name))
-  path <- choose.dir()
+  if (interactive() && .Platform$OS.type == "windows") {
+    path <- utils::choose.dir(getwd(), "Choose a suitable folder")
+  }
   all_file_names <- list.files(path = path)
   param_sets <- sub(".*_ *(.*?) *.RData.*", "\\1", all_file_names)
   out <- sort(as.numeric(param_sets))
