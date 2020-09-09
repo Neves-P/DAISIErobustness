3a89433ce276f3836db6a1dd5b11641514b613da
Neves-P <p.m.santos.neves@rug.nl>
2020-09-09 17:18:29 +0200

    bash has great syntax

--- bash/submit_run_robustness_peregrine.sh
+++ bash/submit_run_robustness_peregrine.sh
@@ -23,8 +23,8 @@ replicate_range_start=$5
 replicate_range_end=$6
 load_from_file=$7
 
-$for_length= wc -l DAISIErobustness/data/${param_space_name}.csv
-$for_length= $(( $for_length - 1 ))
+for_length=wc -l DAISIErobustness/data/${param_space_name}.csv
+for_length=$(( $for_length - 1 ))
 
 for param_set in {1..${for_length}}
 do
