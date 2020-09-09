f6f908eff42e25970c66c8bfd535e3f81aba8942
Neves-P <p.m.santos.neves@rug.nl>
2020-09-09 16:03:42 +0200

    Pass all arguments to Rscript

--- bash/submit_run_robustness_peregrine.sh
+++ bash/submit_run_robustness_peregrine.sh
@@ -11,19 +11,26 @@
 mkdir -p results
 ml R
 
-# Arguments to follow the Rscript are as follows:
 # See DAISIErobustness::run_robustness() documentation for help.
-# param_space_name <- args[1]
-# param_set <- args[2]
-# replicates <- args[3]
-# pipeline <- args[4]
-# distance_method <- args[5]
-# replicate_range_start <- args[6]
-# replicate_range_end <- args[7]
-# load_from_file <- args[8]
-
+# Arguments to follow the Rscript are as follows:
 param_space_name=$1
+param_set=$2
+replicates=$3
+pipeline=$4
+distance_method=$5
+replicate_range_start=$6
+replicate_range_end=$7
+load_from_file=$8
 
 git clone https://github.com/Neves-P/DAISIErobustness.git || (cd DAISIErobustness ; git pull)
 Rscript -e "remotes::install_github('Neves-P/DAISIErobustness')"
-Rscript DAISIErobustness/scripts/run_robustness_peregrine.R ${param_space_name}
+Rscript DAISIErobustness/scripts/run_robustness_peregrine.R ${param_space_name} \
+                                                            ${param_set} \
+                                                            ${replicates} \
+                                                            ${pipeline} \
+                                                            ${distance_method} \
+                                                            ${replicate_range_start} \
+                                                            ${replicate_range_end} \
+                                                            ${load_from_file}
+
+
--- scripts/run_robustness_peregrine.R
+++ scripts/run_robustness_peregrine.R
@@ -1,24 +1,14 @@
 args <- commandArgs(TRUE)
 print(args)
 
-param_space_name <- args[1]
-# param_set <- args[2]
-# replicates <- args[3]
-# pipeline <- args[4]
-# distance_method <- args[5]
-# replicate_range_start <- args[6]
-# replicate_range_end <- args[7]
-# load_from_file <- args[8]
-
-# replicate_range <- c(replicate_range_start, replicate_range_end)
 DAISIErobustness::run_robustness(
-  param_space_name = param_space_name,
-  param_set = 1,
-  replicates = 10,
-  pipeline = "full",
-  distance_method = "abs",
+  param_space_name = args[1],
+  param_set = args[2],
+  replicates = args[3],
+  pipeline = args[4],
+  distance_method = args[5],
   save_output = TRUE,
-  replicate_range = NULL,
-  load_from_file = FALSE
+  replicate_range = c(args[6], args[7]),
+  load_from_file = args[8]
 )
 
