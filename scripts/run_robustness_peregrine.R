f74751db7639da5731371df8d6d0895b0644869e
Neves-P <p.m.santos.neves@rug.nl>
2020-09-09 15:44:27 +0200

    Temporarily use 1 argument

--- scripts/run_robustness_peregrine.R
+++ scripts/run_robustness_peregrine.R
@@ -2,23 +2,23 @@ args <- commandArgs()
 
 
 param_space_name <- args[1]
-param_set <- args[2]
-replicates <- args[3]
-pipeline <- args[4]
-distance_method <- args[5]
-replicate_range_start <- args[6]
-replicate_range_end <- args[7]
-load_from_file <- args[8]
+# param_set <- args[2]
+# replicates <- args[3]
+# pipeline <- args[4]
+# distance_method <- args[5]
+# replicate_range_start <- args[6]
+# replicate_range_end <- args[7]
+# load_from_file <- args[8]
 
-replicate_range <- c(replicate_range_start, replicate_range_end)
+# replicate_range <- c(replicate_range_start, replicate_range_end)
 DAISIErobustness::run_robustness(
   param_space_name = param_space_name,
-  param_set = param_set,
-  replicates = replicates,
-  pipeline = pipeline,
-  distance_method = distance_method,
+  param_set = 1,
+  replicates = 10,
+  pipeline = "full",
+  distance_method = "abs",
   save_output = TRUE,
-  replicate_range = replicate_range,
-  load_from_file = load_from_file
+  replicate_range = NULL,
+  load_from_file = FALSE
 )
 
