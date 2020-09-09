9c0cfb3254deea38b7a165663ac639c3659d07a9
Neves-P <p.m.santos.neves@rug.nl>
2020-09-09 15:05:29 +0200

    Use /home/

--- bash/submit_run_robustness_peregrine.sh
+++ bash/submit_run_robustness_peregrine.sh
@@ -3,12 +3,11 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --job-name=robustness
-#SBATCH --output=/data/${USER}/logs/robustness.log
+#SBATCH --output=/logs/robustness.log
 #SBATCH --mem=5GB
 #SBATCH --partition=gelifes
 
 # Before running make sure logs folder has been created.
-cd /data/${USER}/
 mkdir -p results
 ml R
 
@@ -24,4 +23,4 @@ ml R
 # load_from_file <- args[8]
 
 git clone https://github.com/Neves-P/DAISIErobustness.git || (cd DAISIErobustness ; git pull)
-Rscript /data/${USER}/DAISIErobustness/scripts/run_robustness_peregrine.R
+Rscript /DAISIErobustness/scripts/run_robustness_peregrine.R
