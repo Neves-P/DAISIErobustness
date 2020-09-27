0a3c887b0adc3f70f6f3864a6e9b2e5a11de8464
joshwlambert <j.w.l.lambert@rug.nl>
2020-09-27 11:11:44 +0200

    remove cond argument closes #50

--- R/oceanic_sim.R
+++ R/oceanic_sim.R
@@ -4,28 +4,23 @@
 #' @author Joshua Lambert, Pedro Neves, Shu Xie
 #' @return Output from \code{\link[DAISIE]{DAISIE_sim_constant_rate}()}
 oceanic_sim <- function(ml,
-                        sim_pars,
-                        cond) {
+                        sim_pars) {
   oceanic_sim <- list()
   for (i in seq_along(ml)) {
     if (is.character(ml[[i]])) {
       oceanic_sim[[i]] <- "ML didn't converge"
     } else {
-      num_col <- NULL
-      while (num_col < cond || is.null(num_col)) {
-        oceanic_sim[[i]] <- DAISIE::DAISIE_sim_constant_rate(
-          time = sim_pars$time,
-          M = sim_pars$M,
-          pars = as.numeric(ml[[i]][1:5]),
-          hyper_pars = DAISIE::create_hyper_pars(d = 0, x = 0),
-          replicates = 1,
-          plot_sims = FALSE,
-          verbose = FALSE,
-          sample_freq = Inf
-        )
-        stt_all <- oceanic_sim[[i]][[1]][[1]]$stt_all
-        num_col <- as.numeric(stt_all[nrow(stt_all), "present"])
-      }
+      oceanic_sim[[i]] <- DAISIE::DAISIE_sim_constant_rate(
+        time = sim_pars$time,
+        M = sim_pars$M,
+        pars = as.numeric(ml[[i]][1:5]),
+        hyper_pars = DAISIE::create_hyper_pars(d = 0, x = 0),
+        replicates = 1,
+        plot_sims = FALSE,
+        cond = 5,
+        verbose = FALSE,
+        sample_freq = Inf
+      )
     }
   }
   return(oceanic_sim)
