0b3d399b451d9d003126c299eb517d6e98cc6e9e
joshwlambert <j.w.l.lambert@rug.nl>
2020-09-28 22:49:07 +0200

    removed sim_constraints from create_output_file_name tests

--- tests/testthat/test-create_output_file_name.R
+++ tests/testthat/test-create_output_file_name.R
@@ -4,62 +4,34 @@ test_that("use", {
 
   param_space_name <- "oceanic_ontogeny"
   param_set <- 1
-  sim_constraints <- TRUE
   ml_constraints <- FALSE
   expect_equal(
     create_output_file_name(
       param_space_name = param_space_name,
       param_set = param_set,
-      sim_constraints = sim_constraints,
       ml_constraints = ml_constraints
     ), expected = "failed_cond_oceanic_ontogeny_param_set_1.RData"
   )
 
-  sim_constraints <- FALSE
   ml_constraints <- TRUE
   expect_equal(
     create_output_file_name(
       param_space_name = param_space_name,
       param_set = param_set,
-      sim_constraints = sim_constraints,
-      ml_constraints = ml_constraints
-    ), expected = "failed_cond_oceanic_ontogeny_param_set_1.RData"
-  )
-
-  sim_constraints <- TRUE
-  ml_constraints <- TRUE
-  expect_equal(
-    create_output_file_name(
-      param_space_name = param_space_name,
-      param_set = param_set,
-      sim_constraints = sim_constraints,
       ml_constraints = ml_constraints
     ), expected = "passed_cond_oceanic_ontogeny_param_set_1.RData"
-  )
-
-  sim_constraints <- FALSE
-  ml_constraints <- FALSE
-  expect_equal(
-    create_output_file_name(
-      param_space_name = param_space_name,
-      param_set = param_set,
-      sim_constraints = sim_constraints,
-      ml_constraints = ml_constraints
-    ), expected = "failed_cond_oceanic_ontogeny_param_set_1.RData"
   )
 })
 
 test_that("abuse", {
   param_space_name <- "oceanic_ontogeny"
   param_set <- 1
-  sim_constraints <- NULL
   ml_constraints <- NULL
 
   expect_error(
     create_output_file_name(
       param_space_name = param_space_name,
       param_set = param_set,
-      sim_constraints = sim_constraints,
       ml_constraints = ml_constraints
     )
   )
