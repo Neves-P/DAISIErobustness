0db80781fa4f6598e175319e4403592f99aaf8dc
joshwlambert <j.w.l.lambert@rug.nl>
2020-09-28 22:48:44 +0200

    changed oceanic_ontogeny simulation for nonoceanic simulation in calc_error tests

--- tests/testthat/test-calc_error.R
+++ tests/testthat/test-calc_error.R
@@ -4,7 +4,7 @@ test_that("test calc_error output is correct", {
   if (Sys.getenv("TRAVIS") != "" && Sys.info()[[1]] != "Darwin" ||
       Sys.getenv("APPVEYOR") != "") {
     param_space <- load_param_space(
-      param_space_name = "oceanic_ontogeny")
+      param_space_name = "nonoceanic")
     set.seed(
       1,
       kind = "Mersenne-Twister",
@@ -12,28 +12,28 @@ test_that("test calc_error output is correct", {
       sample.kind = "Rejection"
     )
     sim_pars <- extract_param_set(
-      param_space_name = "oceanic_ontogeny",
+      param_space_name = "nonoceanic",
       param_space = param_space,
       param_set = 1)
     novel_sim <- run_novel_sim(
-      param_space_name = "oceanic_ontogeny",
+      param_space_name = "nonoceanic",
       sim_pars = sim_pars,
       replicates = 2)
-    #ML output from oceanic_ontogeny param_set 1 with seed 1
-    novel_ml <- list(data.frame("lambda_c" = 4.93245551463291e-08,
-                                "mu" = 0.2079398955033904,
-                                "K" = 1.096019787831134,
-                                "gamma" = 0.03235948962897911,
-                                "lambda_a" = 1.333597017889932,
-                                "loglik" = -328.671086110396,
+    #ML output from nonoceanic param_set 1 with seed 1
+    novel_ml <- list(data.frame("lambda_c" = 0.239060106362,
+                                "mu" = 0.243562502681,
+                                "K" = 140931.626879,
+                                "gamma" = 0.009083381509,
+                                "lambda_a" = 0.738638320593,
+                                "loglik" = -125.64119536,
                                 "df" = 5,
                                 "conv" = 0),
-                     data.frame("lambda_c" = 1.255363296522673e-08,
-                                "mu" = 0.03845159846645289,
-                                "K" = 3.297396105095508,
-                                "gamma" = 0.0242967074651809,
-                                "lambda_a" = 0.7106141463253953,
-                                "loglik" = -315.3743622623609,
+                     data.frame("lambda_c" = 0.244398608737,
+                                "mu" = 0.426446095366,
+                                "K" = 1576714.56541,
+                                "gamma" = 0.0126943736142,
+                                "lambda_a" = 0.859859935595,
+                                "loglik" = -147.529038158,
                                 "df" = 5,
                                 "conv" = 0))
     oceanic_sim <- oceanic_sim(
@@ -46,14 +46,14 @@ test_that("test calc_error output is correct", {
       distance_method = "abs"
     )
     expect_length(error, 3)
-    expect_equal(error$spec_error, list(nltt = c(7.753141634762742,
-                                                 11.973944813421863),
-                                        num_spec_error = c(6, 3),
-                                        num_col_error = c(8, 4)))
-    expect_equal(error$endemic_error, list(nltt = c(43.55934832128516,
-                                                    22.14770440482302)))
-    expect_equal(error$nonendemic_error, list(nltt = c(46.02495065811670,
-                                                       31.50089644626357)))
+    expect_equal(error$spec_error, list(nltt = c(27.6793419737,
+                                                 13.6710711632),
+                                        num_spec_error = c(7, 5),
+                                        num_col_error = c(5, 8)))
+    expect_equal(error$endemic_error, list(nltt = c(29.52636388708,
+                                                    9.89480200345)))
+    expect_equal(error$nonendemic_error, list(nltt = c(9.89480200345,
+                                                       12.00627796269)))
   } else {
     skip("Run only on TRAVIS or AppVeyor")
   }
