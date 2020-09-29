9896d897a32f42d795b671f08a3f1c4ee6104d1a
xieshu95 <xieshu95@gmail.com>
2020-09-29 17:31:22 +0200

    update trait param space

--- scripts/generate_trait_param_space.R
+++ scripts/generate_trait_param_space.R
@@ -1,10 +1,11 @@
 ### main_part
 ### keep a constant mean of rates,and increasing |rate1 - rate2|
+#### 1.low mean rates for all
 time <- c(2.5, 5)
 M <- 500
 M2 <- 500
 lac <- c(0.2, 0.4)   ###mean_lac = 0.5
-mu <- c(0.4, 0.6)    ###mean_mu = 0.8
+mu <- c(0.4, 0.6, 1.0)    ###mean_mu = 0.8
 K <- c(40, Inf)
 gam <- c(0.01, 0.015)  ###mean_gam = 0.02
 laa <- c(0.2, 0.4)   ###mean_laa = 0.5
@@ -33,7 +34,7 @@ time <- c(2.5, 5)
 M <- 500
 M2 <- 500
 lac <- c(0.4, 0.6)   ###mean_lac = 0.8
-mu <- c(0.6, 1.0)    ###mean_mu = 1.2
+mu <- c(0.6, 1.0, 1.4)    ###mean_mu = 1.2
 K <- c(40, Inf)
 gam <- c(0.01, 0.03)  ###mean_gam = 0.04
 laa <- c(0.5, 0.8)   ###mean_laa = 1
@@ -62,7 +63,7 @@ time <- c(2.5, 5)
 M <- 500
 M2 <- 500
 lac <- c(0.2, 0.4)   ###mean_lac = 0.5
-mu <- c(0.6, 1.0)    ###mean_mu = 1.2
+mu <- c(0.6, 1.0, 1.4)    ###mean_mu = 1.2
 K <- c(40, Inf)
 gam <- c(0.01, 0.03)  ###mean_gam = 0.04
 laa <- c(0.5, 0.8)   ###mean_laa = 1
@@ -91,7 +92,7 @@ time <- c(2.5, 5)
 M <- 500
 M2 <- 500
 lac <- c(0.2, 0.4)   ###mean_lac = 0.5
-mu <- c(0.6, 1.0)    ###mean_mu = 1.2
+mu <- c(0.6, 1.0, 1.4)    ###mean_mu = 1.2
 K <- c(40, Inf)
 gam <- c(0.01, 0.015)  ###mean_gam = 0.02
 laa <- c(0.2, 0.4)   ###mean_laa = 0.5
@@ -120,7 +121,7 @@ time <- c(2.5, 5)
 M <- 500
 M2 <- 500
 lac <- c(0.4, 0.6)   ###mean_lac = 0.8
-mu <- c(0.6, 1.0)    ###mean_mu = 1.2
+mu <- c(0.6, 1.0, 1.4)    ###mean_mu = 1.2
 K <- c(40, Inf)
 gam <- c(0.01, 0.015)  ###mean_gam = 0.02
 laa <- c(0.5, 0.8)   ###mean_laa = 1
@@ -150,7 +151,7 @@ time <- c(2.5, 5)
 M <- 500
 M2 <- 500
 lac <- c(0.4, 0.6)   ###mean_lac = 0.8
-mu <- c(0.6, 1.0)    ###mean_mu = 1.2
+mu <- c(0.6, 1.0, 1.4)    ###mean_mu = 1.2
 K <- c(40, Inf)
 gam <- c(0.01, 0.03)  ###mean_gam = 0.04
 laa <- c(0.2, 0.4)   ###mean_laa = 0.5
@@ -171,7 +172,7 @@ trait_laa <- cbind(
   trait_laa,
   lac2 = 1.6 - trait_laa[, 4] ,
   mu2 = 2.4 - trait_laa[, 5] ,
-  gam2 = 0.04 - trait_laa[, 6],
+  gam2 = 0.08 - trait_laa[, 6],
   laa2 = 1 - trait_laa[, 7])
 
 
@@ -180,7 +181,7 @@ time <- c(2.5, 5)
 M <- 500
 M2 <- 500
 lac <- c(0.4, 0.6)   ###mean_lac = 0.8
-mu <- c(0.6, 1.0)    ###mean_mu = 1.2
+mu <- c(0.6, 1.0, 1.4)    ###mean_mu = 1.2
 K <- c(40, Inf)
 gam <- c(0.01, 0.015)  ###mean_gam = 0.02
 laa <- c(0.2, 0.4)   ###mean_laa = 0.5
@@ -210,7 +211,7 @@ time <- c(2.5, 5)
 M <- 500
 M2 <- 500
 lac <- c(0.2, 0.4)   ###mean_lac = 0.5
-mu <- c(0.6, 1.0)    ###mean_mu = 1.2
+mu <- c(0.6, 1.0, 1.4)    ###mean_mu = 1.2
 K <- c(40, Inf)
 gam <- c(0.01, 0.015)  ###mean_gam = 0.02
 laa <- c(0.5, 0.8)   ###mean_laa = 1
@@ -239,7 +240,7 @@ time <- c(2.5, 5)
 M <- 500
 M2 <- 500
 lac <- c(0.2, 0.4)   ###mean_lac = 0.5
-mu <- c(0.6, 1.0)    ###mean_mu = 1.2
+mu <- c(0.6, 1.0, 1.4)    ###mean_mu = 1.2
 K <- c(40, Inf)
 gam <- c(0.01, 0.03)  ###mean_gam = 0.04
 laa <- c(0.2, 0.4)   ###mean_laa = 0.5
@@ -268,7 +269,7 @@ time <- c(2.5, 5)
 M <- 500
 M2 <- 500
 lac <- c(0.4, 0.6)   ###mean_lac = 0.8
-mu <- c(0.6, 1.0)    ###mean_mu = 1.2
+mu <- c(0.6, 1.0, 1.4)    ###mean_mu = 1.2
 K <- c(40, Inf)
 gam <- c(0.01, 0.03)  ###mean_gam = 0.04
 laa <- c(0.5, 0.8)   ###mean_laa = 1
@@ -297,7 +298,7 @@ time <- c(2.5, 5)
 M <- 500
 M2 <- 500
 lac <- c(0.4, 0.6)   ###mean_lac = 0.8
-mu <- c(0.6, 1.0)    ###mean_mu = 1.2
+mu <- c(0.6, 1.0, 1.4)    ###mean_mu = 1.2
 K <- c(40, Inf)
 gam <- c(0.01, 0.03)  ###mean_gam = 0.04
 laa <- c(0.5, 0.8)   ###mean_laa = 1
@@ -320,6 +321,66 @@ trait_high_rates_trans <- cbind(
   mu2 = 2.4 - trait_high_rates_trans[, 5] ,
   gam2 = 0.08 - trait_high_rates_trans[, 6],
   laa2 = 2 - trait_high_rates_trans[, 7])
+
+### 12. high rates with unbalance transition(exchange)
+time <- c(2.5, 5)
+M <- 500
+M2 <- 500
+lac <- c(0.4, 0.6)   ###mean_lac = 0.8
+mu <- c(0.6, 1.0, 1.4)    ###mean_mu = 1.2
+K <- c(40, Inf)
+gam <- c(0.01, 0.03)  ###mean_gam = 0.04
+laa <- c(0.5, 0.8)   ###mean_laa = 1
+trans <- 2
+trans2 <- 0.5
+trait_high_rates_trans2 <- expand.grid(
+  time = time,
+  M = M,
+  M2 = M2,
+  lac = lac,
+  mu = mu,
+  gam = gam,
+  laa = laa,
+  trans = trans,
+  trans2 = trans2,
+  K = K)
+trait_high_rates_trans2 <- cbind(
+  trait_high_rates_trans2,
+  lac2 = 1.6 - trait_high_rates_trans2[, 4] ,
+  mu2 = 2.4 - trait_high_rates_trans2[, 5] ,
+  gam2 = 0.08 - trait_high_rates_trans2[, 6],
+  laa2 = 2 - trait_high_rates_trans2[, 7])
+
+####13. mean rates of each group
+time <- c(2.5, 5)
+M <- 500
+M2 <- 500
+lac <- c(0.5, 0.8)
+mu <- c(0.8, 1.2)
+K <- c(40, Inf)
+gam <- c(0.02, 0.04)
+laa <- c(0.5, 1)
+trans <- 0
+trans2 <- 0
+trait_mean <- expand.grid(
+  time = time,
+  M = M,
+  M2 = M2,
+  lac = lac,
+  mu = mu,
+  gam = gam,
+  laa = laa,
+  trans = trans,
+  trans2 = trans2,
+  K = K)
+trait_mean <- cbind(
+  trait_mean,
+  lac2 = trait_mean[, 4] ,
+  mu2 = trait_mean[, 5] ,
+  gam2 = trait_mean[, 6],
+  laa2 = trait_mean[, 7])
+
+
 trait <- rbind(
   trait_low_rates,
   trait_high_rates,
@@ -331,10 +392,13 @@ trait <- rbind(
   trait_lac_gamma,
   trait_lac_laa,
   trait_high_rates_notrans,
-  trait_high_rates_trans)
+  trait_high_rates_trans,
+  trait_high_rates_trans2,
+  trait_mean)
 
 write.csv2(
   trait,
   "data/trait.csv",
   row.names = FALSE
 )
+
--- tests/testthat/test-load_param_space.R
+++ tests/testthat/test-load_param_space.R
@@ -195,7 +195,7 @@ test_that("test load_param_space loads trait parameter space", {
   trait <- load_param_space(
     param_space_name = "trait"
   )
-  expect_equal(nrow(trait), 704)
+  expect_equal(nrow(trait), 1216)
   expect_equal(ncol(trait), 14)
   expect_equal(names(trait),
                c("time", "M", "M2", "lac", "mu", "gam", "laa", "trans",
