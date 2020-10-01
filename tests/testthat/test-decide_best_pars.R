345aa789f12e6bce75bde8d57057f2210994e547
Neves-P <p.m.santos.neves@rug.nl>
2020-10-01 17:26:01 +0200

    decide_best_pars chooses individual best pars

--- tests/testthat/test-decide_best_pars.R
+++ tests/testthat/test-decide_best_pars.R
@@ -1,18 +1,13 @@
 context("decide_best_pars")
 
-test_that("use, pass both constraints", {
+test_that("use", {
   # Pass difference
   ml_res_initpars_1 <- list(data.frame(1, 2, 3, 4, 5, 6))
   ml_res_initpars_2 <- list(data.frame(1, 2, 3, 4, 5, 6))
 
-  novel_ml_constraints_1 <- TRUE
-  novel_ml_constraints_2 <- TRUE
-
   obtained <- decide_best_pars(
     ml_res_initpars_1 = ml_res_initpars_1,
-    ml_res_initpars_2 = ml_res_initpars_2,
-    novel_ml_constraints_1 = novel_ml_constraints_1,
-    novel_ml_constraints_2 = novel_ml_constraints_2
+    ml_res_initpars_2 = ml_res_initpars_2
   )
 
   expected <- list(data.frame(
@@ -26,16 +21,13 @@ test_that("use, pass both constraints", {
 
   expect_equal(object = obtained, expected = expected)
 
-  # Fail difference
 
   ml_res_initpars_1 <- list(data.frame(1, 2, 3, 4, 5, 6))
   ml_res_initpars_2 <- list(data.frame(10, 20, 30, 40, 50, 60))
 
   obtained <- decide_best_pars(
     ml_res_initpars_1 = ml_res_initpars_1,
-    ml_res_initpars_2 = ml_res_initpars_2,
-    novel_ml_constraints_1 = novel_ml_constraints_1,
-    novel_ml_constraints_2 = novel_ml_constraints_2
+    ml_res_initpars_2 = ml_res_initpars_2
   )
 
   expected <- list(data.frame(
@@ -48,107 +40,40 @@ test_that("use, pass both constraints", {
   ))
 
   expect_equal(object = obtained, expected = expected)
+
+  # One set failed
+
+  expected <- list(data.frame(
+    "X1" = 1,
+    "X2" = 2,
+    "X3" = 3,
+    "X4" = 4,
+    "X5" = 5,
+    "X6" = 6
+  ))
+
+  expect_equal(object = obtained, expected = expected)
+
+
+  ml_res_initpars_1 <- list(data.frame(1, 2, 3, 4, 5, 6))
+  ml_res_initpars_2 <- data.frame("ML didn't converge")
+
+  obtained <- decide_best_pars(
+    ml_res_initpars_1 = ml_res_initpars_1,
+    ml_res_initpars_2 = ml_res_initpars_2
+  )
+
+  expected <- list(data.frame(
+    "X1" = 1,
+    "X2" = 2,
+    "X3" = 3,
+    "X4" = 4,
+    "X5" = 5,
+    "X6" = 6
+  ))
+
 })
 
-test_that("use, fail constraints", {
-  # pass difference fail constraint
-  ml_res_initpars_1 <- list(data.frame(1, 2, 3, 4, 5, 6))
-  ml_res_initpars_2 <- list(data.frame(1, 2, 3, 4, 5, 6))
-
-  novel_ml_constraints_1 <- FALSE
-  novel_ml_constraints_2 <- TRUE
-
-  obtained <- decide_best_pars(
-    ml_res_initpars_1 = ml_res_initpars_1,
-    ml_res_initpars_2 = ml_res_initpars_2,
-    novel_ml_constraints_1 = novel_ml_constraints_1,
-    novel_ml_constraints_2 = novel_ml_constraints_2
-  )
-
-  expected <- list(data.frame(
-    "X1" = 1,
-    "X2" = 2,
-    "X3" = 3,
-    "X4" = 4,
-    "X5" = 5,
-    "X6" = 6
-  ))
-
-  expect_equal(object = obtained, expected = expected)
-
-  # Same as above, FAIL another one
-  novel_ml_constraints_1 <- TRUE
-  novel_ml_constraints_2 <- FALSE
-
-  obtained <- decide_best_pars(
-    ml_res_initpars_1 = ml_res_initpars_1,
-    ml_res_initpars_2 = ml_res_initpars_2,
-    novel_ml_constraints_1 = novel_ml_constraints_1,
-    novel_ml_constraints_2 = novel_ml_constraints_2
-  )
-
-  expected <- list(data.frame(
-    "X1" = 1,
-    "X2" = 2,
-    "X3" = 3,
-    "X4" = 4,
-    "X5" = 5,
-    "X6" = 6
-  ))
-
-  expect_equal(object = obtained, expected = expected)
-
-
-  # Fail difference
-
-  ml_res_initpars_1 <- list(data.frame(1, 2, 3, 4, 5, 6))
-  ml_res_initpars_2 <- list(data.frame(10, 20, 30, 40, 50, 60))
-
-  obtained <- decide_best_pars(
-    ml_res_initpars_1 = ml_res_initpars_1,
-    ml_res_initpars_2 = ml_res_initpars_2,
-    novel_ml_constraints_1 = novel_ml_constraints_1,
-    novel_ml_constraints_2 = novel_ml_constraints_2
-  )
-
-  expected <- list(data.frame(
-    "X1" = 1,
-    "X2" = 2,
-    "X3" = 3,
-    "X4" = 4,
-    "X5" = 5,
-    "X6" = 6
-  ))
-
-  expect_equal(object = obtained, expected = expected)
-
-  # Fail difference, fail other set
-
-  ml_res_initpars_1 <- list(data.frame(1, 2, 3, 4, 5, 6))
-  ml_res_initpars_2 <- list(data.frame(10, 20, 30, 40, 50, 5))
-
-  # Same as above, FAIL another one
-  novel_ml_constraints_1 <- FALSE
-  novel_ml_constraints_2 <- TRUE
-
-  obtained <- decide_best_pars(
-    ml_res_initpars_1 = ml_res_initpars_1,
-    ml_res_initpars_2 = ml_res_initpars_2,
-    novel_ml_constraints_1 = novel_ml_constraints_1,
-    novel_ml_constraints_2 = novel_ml_constraints_2
-  )
-
-  expected <- list(data.frame(
-    "X10" = 10,
-    "X20" = 20,
-    "X30" = 30,
-    "X40" = 40,
-    "X50" = 50,
-    "X5" = 5
-  ))
-
-  expect_equal(object = obtained, expected = expected)
-})
 
 
 
@@ -158,9 +83,6 @@ test_that("abuse", {
   ml_res_initpars_2 <- list(data.frame(1, 2, 3, 4, 5))
   expect_error(decide_best_pars(
     ml_res_initpars_1 = ml_res_initpars_1,
-    ml_res_initpars_2 = ml_res_initpars_2,
-    novel_ml_constraints_1 = novel_ml_constraints_1,
-    novel_ml_constraints_2 = novel_ml_constraints_2
-  )
-  )
+    ml_res_initpars_2 = ml_res_initpars_2
+  ))
 })
