20351f68272c1d8dbc4e84dc8629799d9d156d62
joshwlambert <j.w.l.lambert@rug.nl>
2020-11-17 20:54:51 +0100

    updated submit_run_robustness_param_set_regular and submit_run_robustness_peregrine_regular bash scripts

--- bash/submit_run_robustness_peregrine_regular.sh
+++ bash/submit_run_robustness_peregrine_regular.sh
@@ -42,28 +42,10 @@
 #   oceanic
 # replicates - Total number of replicates to be simulated, or are present in
 #   the input simulation.
-# pipeline - Whether the full pipeline should run, only the simulations, or only
-#   the analysis. Options:
-#     full - runs simulations followed analysis.
-#     novel_sim - runs only simulation portion of pipeline and returns output.
-#     analysis - runs only analysis portion of pipeline and returns output.
 # distance_method - If the absolute or squared distance between nLTTs should be
 #   computed. Options:
 #     abs - (default)
 #     squ
-# replicate_range_start - NULL or integer. If NULL, analysis will be done on all
-#   replicates. If an integer, then the value will mark the start of the window
-#   of replicates to be analysed.
-# replicate_range_end - NULL or integer. If NULL, analysis will be done on all
-#   replicates. If an integer, then the value will mark the end of the window
-#   of replicates to be analysed.
-# load_from_file - Boolean. FALSE indicates the simulations used in the analysis
-#   portion of the pipeline will not be loaded from a file and are expected to
-#   be created beforehand or by the pipeline call itself. TRUE indicates
-#   simulations are to be read from disk in the home/$USER/sims/ directory.
-#   For a "full" or "novel_sim" pipeline, the normal use is FALSE.
-#   For an "analysis" run, TRUE is usually used.
-#   This argument allows decoupling simulation and analysis jobs.
 ################################################################################
 ##### Before running make sure logs folder has been created! ####
 ## Usage example running simulations for the oceanic_ontogeny param space, 1000
@@ -85,11 +67,7 @@ Rscript -e "remotes::install_github('Neves-P/DAISIErobustness')"
 
 param_space_name=$1
 replicates=$2
-pipeline=$3
-distance_method=$4
-replicate_range_start=$5
-replicate_range_end=$6
-load_from_file=$7
+distance_method=$3
 
 for_length=`wc -l DAISIErobustness/data/${param_space_name}.csv | cut -f1 -d' '`
 for_length=$(( ${for_length} - 1 ))
@@ -99,9 +77,5 @@ do
   sbatch DAISIErobustness/bash/submit_run_robustness_param_set_regular.sh ${param_space_name} \
                                                              ${param_set} \
                                                              ${replicates} \
-                                                             ${pipeline} \
-                                                             ${distance_method} \
-                                                             ${replicate_range_start} \
-                                                             ${replicate_range_end} \
-                                                             ${load_from_file}
+                                                             ${distance_method}
 done
