9f8885f46d93c9b13bc11d3cfaedd52163ec9ea4
Neves-P <10762799+Neves-P@users.noreply.github.com>
2020-11-19 19:48:00 +0000

    Fix calc_error_metrics tests. Progress #51

--- tests/testthat/test-calc_error_metrics.R
+++ tests/testthat/test-calc_error_metrics.R
@@ -2,23 +2,29 @@ context("calc_error_metrics")
 
 test_that("test calc_error_metrics output is correct when all error
           distributions are equal", {
-  spec_error <- list(nltt = c(1, 1, 1, 1, 1),
-                     num_spec_error = c(1, 1, 1, 1, 1),
-                     num_col_error = c(1, 1, 1, 1, 1))
-  endemic_error <- list(nltt = c(1, 1, 1, 1, 1))
-  nonendemic_error <- list(nltt = c(1, 1, 1, 1, 1))
-  spec_baseline_error <- list(nltt = c(1, 1, 1, 1, 1),
-                              num_spec_error = c(1, 1, 1, 1, 1),
-                              num_col_error = c(1, 1, 1, 1, 1))
-  endemic_baseline_error <- list(nltt = c(1, 1, 1, 1, 1))
-  nonendemic_baseline_error <- list(nltt = c(1, 1, 1, 1, 1))
+  spec_nltt_error <- c(1, 1, 1, 1)
+  num_spec_error <- c(1, 1, 1, 1)
+  num_col_error <- c(1, 1, 1, 1)
+  endemic_nltt_error <- c(1, 1, 1, 1)
+  nonendemic_nltt_error <- c(1, 1, 1, 1)
+  spec_baseline_nltt_error <- c(1, 1, 1, 1)
+  num_spec_baseline_error <- c(1, 1, 1, 1)
+  num_col_baseline_error <- c(1, 1, 1, 1)
+  endemic_baseline_nltt_error <- c(1, 1, 1, 1)
+  nonendemic_baseline_nltt_error <- c(1, 1, 1, 1)
+
   error_metrics <- calc_error_metrics(
-    spec_error = spec_error,
-    endemic_error = endemic_error,
-    nonendemic_error = nonendemic_error,
-    spec_baseline_error = spec_baseline_error,
-    endemic_baseline_error = endemic_baseline_error,
-    nonendemic_baseline_error = nonendemic_baseline_error)
+    spec_nltt_error = spec_nltt_error,
+    num_spec_error = num_spec_error,
+    num_col_error = num_col_error,
+    endemic_nltt_error = endemic_nltt_error,
+    nonendemic_nltt_error = nonendemic_nltt_error,
+    spec_baseline_nltt_error = spec_baseline_nltt_error,
+    num_spec_baseline_error = num_spec_baseline_error,
+    num_col_baseline_error = num_col_baseline_error,
+    endemic_baseline_nltt_error = endemic_baseline_nltt_error,
+    nonendemic_baseline_nltt_error = nonendemic_baseline_nltt_error
+  )
   expect_equal(error_metrics$num_spec_mean_diff, 0)
   expect_equal(error_metrics$num_spec_sd_diff, 0)
   expect_equal(error_metrics$num_col_mean_diff, 0)
@@ -33,23 +39,30 @@ test_that("test calc_error_metrics output is correct when all error
 
 test_that("test calc_error_metrics output is correct when all error
           distributions are unequal", {
-  spec_error <- list(nltt = c(1, 2, 3, 4, 5),
-                     num_spec_error = c(1, 2, 3, 4, 5),
-                     num_col_error = c(1, 2, 3, 4, 5))
-  endemic_error <- list(nltt = c(1, 2, 3, 4, 5))
-  nonendemic_error <- list(nltt = c(1, 2, 3, 4, 5))
-  spec_baseline_error <- list(nltt = c(1, 1, 1, 1, 1),
-                              num_spec_error = c(1, 1, 1, 1, 1),
-                              num_col_error = c(1, 1, 1, 1, 1))
-  endemic_baseline_error <- list(nltt = c(1, 1, 1, 1, 1))
-  nonendemic_baseline_error <- list(nltt = c(1, 1, 1, 1, 1))
-  error_metrics <- calc_error_metrics(
-    spec_error = spec_error,
-    endemic_error = endemic_error,
-    nonendemic_error = nonendemic_error,
-    spec_baseline_error = spec_baseline_error,
-    endemic_baseline_error = endemic_baseline_error,
-    nonendemic_baseline_error = nonendemic_baseline_error)
+            spec_nltt_error <- c(1, 2, 3, 4, 5)
+            num_spec_error <- c(1, 1, 1, 1, 5)
+            num_col_error <- c(1, 2, 3, 4, 5)
+            endemic_nltt_error <- c(1, 2, 3, 4, 5)
+            nonendemic_nltt_error <- c(1, 2, 3, 4, 5)
+            spec_baseline_nltt_error <- c(1, 1, 1, 1, 1)
+            num_spec_baseline_error <- c(1, 1, 1, 1, 1)
+            num_col_baseline_error <- c(1, 1, 1, 1, 1)
+            endemic_baseline_nltt_error <- c(1, 1, 1, 1, 1)
+            nonendemic_baseline_nltt_error <- c(1, 1, 1, 1, 1)
+
+            error_metrics <- calc_error_metrics(
+              spec_nltt_error = spec_nltt_error,
+              num_spec_error = num_spec_error,
+              num_col_error = num_col_error,
+              endemic_nltt_error = endemic_nltt_error,
+              nonendemic_nltt_error = nonendemic_nltt_error,
+              spec_baseline_nltt_error = spec_baseline_nltt_error,
+              num_spec_baseline_error = num_spec_baseline_error,
+              num_col_baseline_error = num_col_baseline_error,
+              endemic_baseline_nltt_error = endemic_baseline_nltt_error,
+              nonendemic_baseline_nltt_error = nonendemic_baseline_nltt_error
+            )
+
   expect_true(error_metrics$num_spec_mean_diff != 0)
   expect_true(error_metrics$num_spec_sd_diff != 0)
   expect_true(error_metrics$num_col_mean_diff != 0)
