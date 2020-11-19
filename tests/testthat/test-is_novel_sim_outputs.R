01df7431ae25231b770344b22153a6f07d111f7e
Neves-P <10762799+Neves-P@users.noreply.github.com>
2020-11-19 20:03:53 +0000

    Correct structure for test-is_novel_sim_outputs. Progress #51

--- tests/testthat/test-is_novel_sim_outputs.R
+++ tests/testthat/test-is_novel_sim_outputs.R
@@ -4,7 +4,6 @@ test_that("use", {
   novel_sim <- list()
   # Nested list that replicates novel_sim() output
   novel_sim[[1]] <-
-    list(
       list(
         list(
           "island_age" = 1,
@@ -12,13 +11,12 @@ test_that("use", {
           "stt_all" = c(1, 0)
         )
       )
-    )
   novel_sim_no_age <- novel_sim
   novel_sim_no_not_present <- novel_sim
   novel_sim_no_stt_all <- novel_sim
-  novel_sim_no_age[[1]][[1]][[1]][["island_age"]] <- NULL
-  novel_sim_no_not_present[[1]][[1]][[1]][["not_present"]] <- NULL
-  novel_sim_no_stt_all[[1]][[1]][[1]][["stt_all"]] <- NULL
+  novel_sim_no_age[[1]][[1]][["island_age"]] <- NULL
+  novel_sim_no_not_present[[1]][[1]][["not_present"]] <- NULL
+  novel_sim_no_stt_all[[1]][[1]][["stt_all"]] <- NULL
   expect_true(is_novel_sim_outputs(novel_sim = novel_sim))
   expect_false(is_novel_sim_outputs(novel_sim = novel_sim_no_age))
   expect_false(is_novel_sim_outputs(novel_sim = novel_sim_no_not_present))
