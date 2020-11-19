0b2a60aa4c286ae18db5b8e4f030666f1f14d0ab
Neves-P <10762799+Neves-P@users.noreply.github.com>
2020-11-19 19:54:24 +0000

    No more ml_constraints. Progress #51 Better abuse tests. Progress #52

--- tests/testthat/test-create_output_file_name.R
+++ tests/testthat/test-create_output_file_name.R
@@ -4,35 +4,42 @@ test_that("use", {
 
   param_space_name <- "oceanic_ontogeny"
   param_set <- 1
-  ml_constraints <- FALSE
   expect_equal(
     create_output_file_name(
       param_space_name = param_space_name,
-      param_set = param_set,
-      ml_constraints = ml_constraints
-    ), expected = "failed_cond_oceanic_ontogeny_param_set_1.RData"
-  )
-
-  ml_constraints <- TRUE
-  expect_equal(
-    create_output_file_name(
-      param_space_name = param_space_name,
-      param_set = param_set,
-      ml_constraints = ml_constraints
-    ), expected = "passed_cond_oceanic_ontogeny_param_set_1.RData"
+      param_set = param_set
+    ), expected = "oceanic_ontogeny_param_set_1.RData"
   )
 })
 
 test_that("abuse", {
   param_space_name <- "oceanic_ontogeny"
-  param_set <- 1
-  ml_constraints <- NULL
+  param_set <- NULL
 
   expect_error(
     create_output_file_name(
       param_space_name = param_space_name,
-      param_set = param_set,
-      ml_constraints = ml_constraints
+      param_set = param_set
+    )
+  )
+
+  param_space_name <- NULL
+  param_set <- 1
+
+  expect_error(
+    create_output_file_name(
+      param_space_name = param_space_name,
+      param_set = param_set
+    )
+  )
+
+  param_space_name <- NULL
+  param_set <- NULL
+
+  expect_error(
+    create_output_file_name(
+      param_space_name = param_space_name,
+      param_set = param_set
     )
   )
 })
