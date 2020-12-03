2864b62d12a056cb86439dda937868e35f649faf
Neves-P <10762799+Neves-P@users.noreply.github.com>
2020-12-03 19:54:49 +0000

    Remove function

--- R/utils.R
+++ R/utils.R
@@ -1,20 +1,3 @@
-#' Get param set IDs stored in results folder
-#'
-#' @inheritParams default_params_doc
-#'
-#' @return A vector with param_set IDs
-#' @author Joshua Lambert, Pedro Neves
-get_available_results <- function(param_space_name) {
-  testit::assert(is.character(param_space_name))
-  if (interactive() && .Platform$OS.type == "windows") {
-    path <- utils::choose.dir(getwd(), "Choose a suitable folder")
-  }
-  all_file_names <- list.files(path = path)
-  param_sets <- sub(".*_ *(.*?) *.RData.*", "\\1", all_file_names)
-  out <- sort(as.numeric(param_sets))
-  return(out)
-}
-
 #' Measures if the input is a valid collection of simulation
 #' outputs.
 #'
