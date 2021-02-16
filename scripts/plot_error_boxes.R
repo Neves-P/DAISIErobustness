d41769d1fb304fc9b5cf7ef22bc2973ed14d307a
Neves-P <p.m.santos.neves@rug.nl>
2021-02-16 14:03:40 +0100

    plot_error_boxes progress

--- scripts/plot_error_boxes.R
+++ scripts/plot_error_boxes.R
@@ -1,27 +1,12 @@
-ontogeny_spec_nltt_mean_diff <- runif(n = 100, min = 0, max = 100)
-sea_level_spec_nltt_mean_diff <- runif(n = 100, min = 0, max = 100)
-ontogeny_sea_level_spec_nltt_mean_diff <- runif(n = 100, min = 0, max = 100)
-nonoceanic_spec_nltt_mean_diff <- runif(n = 100, min = 0, max = 100)
-nonoceanic_land_bridge_spec_nltt_mean_diff <- runif(n = 100, min = 0, max = 100)
-
-data <- data.frame(ontogeny_spec_nltt_mean_diff,
-                   sea_level_spec_nltt_mean_diff,
-                   ontogeny_sea_level_spec_nltt_mean_diff,
-                   nonoceanic_spec_nltt_mean_diff,
-                   nonoceanic_land_bridge_spec_nltt_mean_diff)
-colnames(data) <- c("Ontogeny", "Sea Level", "Ontogeny + Sea Level",
-                    "Non-oceanic", "Non-oceanic + land-bridge")
-data <- tidyr::gather(data)
-
-ggplot2::ggplot(data = data, ggplot2::aes(y = value, x = key, fill = key)) +
-  ggplot2::geom_boxplot()
-
-ggplot2::ggplot(data = data, ggplot2::aes(y = value, x = key, fill = key)) +
-  ggplot2::geom_violin()
-
 plot_error_boxes <- function(error_metrics_list,
-                            error = "spec_nltt",
-                            parameter_spaces) {
+                             error = "spec_nltt",
+                             error_metrics_names) {
+  colnames(data) <- c("key", "value")
+
+  data <- data.frame(island = c(rep("maui_nui", 180), rep("kauai", 187)), spec_nltt_diff = c(maui_nui, kauai))
+
+  ggplot2::ggplot(data = data, ggplot2::aes(y = value, x = key, fill = key)) +
+    ggplot2::geom_boxplot() + ggplot2::xlab("Island") + ggplot2::ylab(expression(P[95]*" species number" * Delta * " nLTT"))
 
 }
 
