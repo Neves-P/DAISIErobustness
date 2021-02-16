42ca77607c7c29daaccbb17644f5745f34f7e8b4
Neves-P <p.m.santos.neves@rug.nl>
2021-02-16 14:03:31 +0100

    calc_stat_diff() takes param_set_ranges

--- scripts/calc_stat_diff.R
+++ scripts/calc_stat_diff.R
@@ -17,41 +17,58 @@ calc_stat_diff <- function(folder_path, error, param_set_range = NULL) {
     n_files > 0
   )
   message(n_files, " data files found.")
+  message("Looking for parameter set ",
+          min(param_set_range),
+          " to ",
+          max(param_set_range),
+          ".")
 
-  pb <- utils::txtProgressBar(min = 0, max = n_files, style = 3)
+  pb <- utils::txtProgressBar(
+    min = min(param_set_range),
+    max = max(param_set_range),
+    style = 3
+  )
 
   stat_diff <- c()
 
-  for (i in data_file_indices) {
-    load(file.path(folder_path, files[i]))
+  for (i in param_set_range) {
+    file_to_load <- grep(paste0("_", i, ".RData"),
+                         files,
+                         value = TRUE,
+                         fixed = TRUE)
 
-    if (error == "spec_nltt") {
-      geodynamic_error <- output$spec_nltt_error
-      oceanic_error <- output$spec_baseline_nltt_error
-    } else if (error == "endemic_nltt") {
-      geodynamic_error <- output$endemic_nltt_error
-      oceanic_error <- output$endemic_baseline_nltt_error
+    if (!identical(file_to_load, character())) {
+      load(file.path(folder_path, file_to_load))
 
-    } else if (error == "nonendemic_nltt") {
-      geodynamic_error <- output$nonendemic_nltt_error
-      oceanic_error <- output$nonendemic_baseline_nltt_error
+      if (error == "spec_nltt") {
+        geodynamic_error <- output$spec_nltt_error
+        oceanic_error <- output$spec_baseline_nltt_error
+      } else if (error == "endemic_nltt") {
+        geodynamic_error <- output$endemic_nltt_error
+        oceanic_error <- output$endemic_baseline_nltt_error
 
-    } else if (error == "num_spec") {
-      geodynamic_error <- output$num_spec_error
-      oceanic_error <- output$num_spec_baseline_error
+      } else if (error == "nonendemic_nltt") {
+        geodynamic_error <- output$nonendemic_nltt_error
+        oceanic_error <- output$nonendemic_baseline_nltt_error
 
-    } else if (error == "num_col") {
-      geodynamic_error <- output$num_col_error
-      oceanic_error <- output$num_col_baseline_error
+      } else if (error == "num_spec") {
+        geodynamic_error <- output$num_spec_error
+        oceanic_error <- output$num_spec_baseline_error
+
+      } else if (error == "num_col") {
+        geodynamic_error <- output$num_col_error
+        oceanic_error <- output$num_col_baseline_error
+      }
+
+      sorted_oceanic_error <- sort(oceanic_error, decreasing = FALSE)
+      boundary <- sorted_oceanic_error[950]
+
+      stat_diff[i] <- (sum(geodynamic_error > boundary) + 1) /
+        (length(sorted_oceanic_error) + 1)
     }
-
-    sorted_oceanic_error <- sort(oceanic_error, decreasing = FALSE)
-    boundary <- sorted_oceanic_error[950]
-
-    stat_diff[i] <- (sum(geodynamic_error > boundary) + 1) /
-      (length(sorted_oceanic_error) + 1)
     utils::setTxtProgressBar(pb, i)
   }
+  stat_diff <- stat_diff[!is.na(stat_diff)]
 
   message("\nTime elapsed: ", Sys.time() - start_time)
   return(stat_diff)
