d954167866bd27cb75c106dd4df965039853040b
Neves-P <p.m.santos.neves@rug.nl>
2021-03-10 11:41:44 +0100

    Tweak plot_error_boxes. Progress #59

--- scripts/plot_error_boxes.R
+++ scripts/plot_error_boxes.R
@@ -1,7 +1,7 @@
 plot_error_boxes <- function(error_metrics_list,
                              error = "spec_nltt",
-                             error_metrics_names
-                             ) {
+                             error_metrics_names,
+                             x_axis_text) {
   error_metrics_sizes <- sapply(X = error_metrics_list, FUN = length)
   larger_vector_size <- max(error_metrics_sizes)
   for (i in seq_along(error_metrics_list)) {
@@ -14,46 +14,38 @@ plot_error_boxes <- function(error_metrics_list,
   }
 
   if (error == "spec_nltt") {
-    error_label <- expression(P[95] * " species number " * Delta * "nLTT")
+    error_label <- expression(P[95] ~ Delta * "STT")
   } else if (error == "endemic_nltt") {
-    error_label <- expression(P[95] * " endemic species number " * Delta * "nLTT")
+    error_label <- expression(P[95] ~ Delta * "endTT")
   } else if (error == "nonendemic_nltt") {
-    error_label <- expression(P[95]* " non-endemic species number " * Delta * "nLTT")
+    error_label <- expression(P[95] ~ Delta * "nonendTT")
   } else if (error == "num_spec") {
-    error_label <- expression(P[95]* " species number")
+    error_label <- expression(P[95] * " Species Number")
   } else if (error == "num_col") {
-    error_label <- expression(P[95]* " colonist number")
+    error_label <- expression(P[95] * " Colonist Number")
   }
 
   data <- as.data.frame(do.call(cbind, error_metrics_list))
   colnames(data) <- error_metrics_names
   data <- tidyr::gather(data)
-  # island_name <- c(rep("Maui Nui", larger_vector_size),
-  #   rep("Kauai", larger_vector_size),
-  #   rep("Maui Nui", larger_vector_size),
-  #   rep("Kauai", larger_vector_size),
-  #   rep("Maui Nui", larger_vector_size),
-  #   rep("Kauai", larger_vector_size),
-  #   rep("Maui Nui", larger_vector_size),
-  #   rep("Kauai", larger_vector_size))
-
   data$key <- factor(
     data$key,
     error_metrics_names
   )
-  # data <- cbind(data, island_name)
+
   p <- ggplot2::ggplot(data = data, ggplot2::aes(y = value, x = key, fill = key)) +
     ggplot2::theme_bw() +
     ggplot2::geom_boxplot(na.rm = TRUE, show.legend = FALSE) +
-    ggplot2::scale_fill_brewer(palette = "Pastel1") +
-    # ggplot2::scale_x_discrete(labels = xlabels) +
-    ggplot2::geom_hline(yintercept = 0.05, linetype = "dashed", size = 0.75) +
-    ggplot2::xlab("Geodynamic scenario") +
+    ggplot2::scale_fill_manual(values = c(rep("#66C2A5", 3), rep("#FC8D62", 2))) +
+    ggplot2::geom_hline(yintercept = 0.05, linetype = "dashed", size = 0.5) +
     ggplot2::ylab(error_label) +
-    ggplot2::theme(axis.title.y = ggplot2::element_text(size = ggplot2::rel(1.3))) +
-    ggplot2::theme(axis.title.x = ggplot2::element_text(size = ggplot2::rel(1.4))) +
-    ggplot2::theme(axis.text.y = ggplot2::element_text(size = ggplot2::rel(1.3))) +
-    ggplot2::theme(axis.text.x = ggplot2::element_text(size = ggplot2::rel(1.4)))
+    ggplot2::xlab(x_axis_text) +
+    ggplot2::scale_y_continuous(labels = function(x){sprintf("%.3f", x)}) +
+    ggplot2::theme(axis.title.y = ggplot2::element_text(size = 10)) +
+    ggplot2::theme(axis.title.x = ggplot2::element_text(size = 10)) +
+    ggplot2::theme(axis.text.y = ggplot2::element_text(size = 8)) +
+    ggplot2::theme(axis.text.x = ggplot2::element_text(size = 8)) +
+    ggplot2::theme(plot.margin = ggplot2::margin(6, 0.2, 6, 0.2))
  p
 }
 
