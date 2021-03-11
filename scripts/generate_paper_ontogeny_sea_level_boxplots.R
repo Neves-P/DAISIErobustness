4c231a8dff6c84a9bcf4ce3294493d931276cfcd
Neves-P <p.m.santos.neves@rug.nl>
2021-03-11 02:46:27 +0100

    Plot in 3 parts

--- scripts/generate_paper_ontogeny_sea_level_boxplots.R
+++ scripts/generate_paper_ontogeny_sea_level_boxplots.R
@@ -1,5 +1,23 @@
 # Boxplots for oceanic_ontogeny vs oceanic_ontogeny_sea_level for
 # Neves et al 2021
+
+oceanic_ontogeny_stat_diff <- compile_stat_diffs(
+  scenario = "oceanic_ontogeny",
+  chunk_size = 48,
+  total_length = 384
+)
+oceanic_sea_level_stat_diff <- compile_stat_diffs(
+  scenario = "oceanic_sea_level",
+  chunk_size = 48,
+  total_length = 768
+)
+oceanic_ontogeny_sea_level_stat_diff <- compile_stat_diffs(
+  scenario = "oceanic_ontogeny_sea_level",
+  chunk_size = 48,
+  total_length = 768
+)
+
+
 oceanic_ontogeny_metrics_names <- c(
   "l_l",
   "h_l",
@@ -34,7 +52,7 @@ xlabel_vec <- c(expression(atop("Low"~x, "Low"~d)),
                 expression(atop("High"~x,"Low"~d)),
                 expression(atop("High"~x,"High"~d)))
 oceanic_sea_level_plots <- generate_paper_plots(
-  list_to_plot = oceanic_sea_level$stat_diffs,
+  list_to_plot = oceanic_sea_level_stat_diff$stat_diffs,
   error_metrics_names = oceanic_sea_level_metrics_names,
   x_axis_text = "Hyperparameters",
   scenario = "oceanic_sea_level",
@@ -42,8 +60,17 @@ oceanic_sea_level_plots <- generate_paper_plots(
   save = FALSE
 )
 oceanic_ontogeny_plots <- generate_paper_plots(
-  list_to_plot = oceanic_ontogeny$stat_diffs,
+  list_to_plot = oceanic_ontogeny_stat_diff$stat_diffs,
   error_metrics_names = oceanic_ontogeny_metrics_names,
+  x_axis_text = "Hyperparameters",
+  scenario = "oceanic_sea_level",
+  xlabels = xlabel_vec,
+  save = FALSE
+)
+
+oceanic_ontogeny_sea_level_plots <- generate_paper_plots(
+  list_to_plot = oceanic_ontogeny_sea_level_stat_diff$stat_diffs,
+  error_metrics_names = oceanic_sea_level_metrics_names,
   x_axis_text = "Hyperparameters",
   scenario = "oceanic_sea_level",
   xlabels = xlabel_vec,
@@ -58,6 +85,23 @@ prow <- cowplot::plot_grid(
   hjust = -0.2,
   nrow = 1
 )
+prow_2 <- cowplot::plot_grid(
+  oceanic_ontogeny_plots[[1]] + ggplot2::theme(
+    legend.position = "none",
+    axis.title.x = ggplot2::element_blank()
+  ), oceanic_sea_level_plots[[1]] + ggplot2::theme(
+    legend.position = "none",
+    axis.title.y = ggplot2::element_blank(),
+    axis.title.x = ggplot2::element_blank()
+    ), oceanic_ontogeny_sea_level_plots[[1]] + ggplot2::theme(
+      legend.position = "none",
+      axis.title.y = ggplot2::element_blank(),
+      axis.title.x = ggplot2::element_blank()
+    ), align = 'vh',
+  labels = c("A", "B", "C"),
+  hjust = -0.2,
+  nrow = 1
+)
 
 legend <- cowplot::get_legend(
   # create some space to the left of the legend
@@ -65,13 +109,28 @@ legend <- cowplot::get_legend(
 )
 
 final_hyperpars <- cowplot::plot_grid(prow, legend, rel_widths = c(3, 0.6))
+final_hyperpars_2 <- cowplot::plot_grid(
+  prow_2,
+  legend,
+  rel_widths = c(3, 0.6)
+)
+
+cowplot::ggdraw(cowplot::add_sub(final_hyperpars_2, "Hyperparameters", vpadding=grid::unit(0, "lines"),y = 5, x = 0.5, vjust = 4.5))
 
 ggplot2::ggsave(
-  plot = final_hyperpars,
-  filename = "hyperpars_tall.png",
+  plot = final_hyperpars_2,
+  filename = "hyperpars_2_short.png",
   device = "png",
   width = 5.2,
-  height = 3.9,
+  height = 2.9,
   dpi = 300
-  # compression = "lzw"
+)
+ggplot2::ggsave(
+  plot = final_hyperpars,
+  filename = "hyperpars_tall.tif",
+  device = "tiff",
+  width = 5.2,
+  height = 3.9,
+  dpi = 300,
+  compression = "lzw"
 )
