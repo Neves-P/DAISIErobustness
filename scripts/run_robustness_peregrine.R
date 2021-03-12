c9de36f2a1379d4cc10d9d0b4c6a9f6d2fa9175f
Neves-P <p.m.santos.neves@rug.nl>
2021-03-12 15:48:42 +0100

    Use DAISIEutils::print_main_header()

--- scripts/run_robustness_peregrine.R
+++ scripts/run_robustness_peregrine.R
@@ -1,11 +1,23 @@
 args <- commandArgs(TRUE)
 
+param_space_name <- args[1]
+param_set <- as.numeric(args[2])
+replicates <- as.numeric(args[3])
+distance_method <- args[4]
+save_output <- TRUE
+M <- 1000
+
+DAISIEutils::print_main_header(
+  run_name = param_space_name,
+  M = M,
+  metadata = param_set
+)
 
 DAISIErobustness::run_robustness(
   param_space_name = args[1],
   param_set = as.numeric(args[2]),
   replicates = as.numeric(args[3]),
   distance_method = args[4],
-  save_output = TRUE
+  save_output = save_output
 )
 
