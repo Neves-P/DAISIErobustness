d97057b3467827e6798654f7eb31fdb64bc6ff9f
Neves-P <p.m.santos.neves@rug.nl>
2021-03-30 19:36:03 +0200

    Split compile_stat_diffs

---
+++ scripts/plots/compile_stat_diffs.R
@@ -0,0 +1,21 @@
+# Accessory functions for boxplot plotting for Neves et al 2021
+compile_stat_diffs <- function(scenario, chunk_size, total_length) {
+  folder_path <- file.path("G:\\Discos partilhados\\DAISIE-RUG\\Josh_Pedro_paper\\results", scenario)
+  n_chunks <- total_length / chunk_size
+  testit::assert("Is whole number", identical(round(n_chunks), n_chunks))
+
+  lower_range <- seq(from = 1, to = total_length, by = chunk_size)
+  upper_range <- seq(from = 0, to = total_length, by = chunk_size)[-1]
+  testit::assert(identical(length(lower_range), length(upper_range)))
+  testit::assert(identical(upper_range[length(upper_range)], total_length))
+
+  stat_diffs <- list()
+  n_data <- c()
+  for (i in seq_len(n_chunks)) {
+    stat_diffs[[i]] <- calc_stat_diff(folder_path, lower_range[i]:upper_range[i])
+    n_data[i] <- length(stat_diffs[[i]][[1]])
+  }
+  return(list(
+    stat_diffs = stat_diffs,
+    n_data = n_data))
+}
--- scripts/plots/plot_stat_diffs_boxplots.R
+++ scripts/plots/plot_stat_diffs_boxplots.R
@@ -1,25 +1,3 @@
-# Accessory functions for boxplot plotting for Neves et al 2021
-compile_stat_diffs <- function(scenario, chunk_size, total_length) {
-  folder_path <- file.path("G:\\Discos partilhados\\DAISIE-RUG\\Josh_Pedro_paper\\results", scenario)
-  n_chunks <- total_length / chunk_size
-  testit::assert("Is whole number", identical(round(n_chunks), n_chunks))
-
-  lower_range <- seq(from = 1, to = total_length, by = chunk_size)
-  upper_range <- seq(from = 0, to = total_length, by = chunk_size)[-1]
-  testit::assert(identical(length(lower_range), length(upper_range)))
-  testit::assert(identical(upper_range[length(upper_range)], total_length))
-
-  stat_diffs <- list()
-  n_data <- c()
-  for (i in seq_len(n_chunks)) {
-    stat_diffs[[i]] <- calc_stat_diff(folder_path, lower_range[i]:upper_range[i])
-    n_data[i] <- length(stat_diffs[[i]][[1]])
-  }
-  return(list(
-    stat_diffs = stat_diffs,
-    n_data = n_data))
-}
-
 generate_paper_boxplots <- function(list_to_plot,
                                     error_metrics_names,
                                     x_axis_text,
@@ -65,3 +43,4 @@ generate_paper_boxplots <- function(list_to_plot,
   }
   return(output_list)
 }
+
