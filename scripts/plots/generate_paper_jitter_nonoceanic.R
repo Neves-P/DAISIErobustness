f341d6f98e82a6678377cb05771fd4f6d6d539ee
Neves-P <p.m.santos.neves@rug.nl>
2021-04-12 15:47:40 +0200

    Add nonoceanic jitter plot functions

---
+++ scripts/plots/generate_paper_jitter_nonoceanic.R
@@ -0,0 +1,53 @@
+generate_paper_jitter_nonoceanic <- function(list_to_plot,
+                                             error_metrics_names,
+                                             x_axis_text,
+                                             scenario,
+                                             xlabels,
+                                             save = TRUE) {
+  metric_to_plot <- c(
+    "spec_nltt",
+    "endemic_nltt",
+    "nonendemic_nltt",
+    "num_spec",
+    "num_col"
+  )
+
+  error_metrics_list <- list()
+  output_list <- list()
+  # Cycle over metrics
+  for (i in seq_along(metric_to_plot)) {
+    # Cycle over param space divisions
+    for (j in seq_along(list_to_plot)) {
+      error_metrics_list[[j]] <- list_to_plot[[j]][[i]]
+    }
+    p <- plot_error_jitters_nonoceanic(
+      error_metrics_list = error_metrics_list,
+      error = metric_to_plot[i],
+      error_metrics_names = error_metrics_names,
+      xlabels = xlabels,
+      x_axis_text = x_axis_text
+    )
+    if (save) {
+      ggplot2::ggsave(
+        plot = p,
+        filename = paste0(scenario, "_", metric_to_plot[i], ".eps"),
+        device = "eps",
+        width = 168,
+        height = 100,
+        units = "mm"
+      )
+      ggplot2::ggsave(
+        plot = p,
+        filename = paste0(scenario, "_", metric_to_plot[i], ".png"),
+        device = "png",
+        width = 168,
+        height = 100,
+        units = "mm",
+        dpi = 600
+      )
+    } else {
+      output_list[[i]] <- p
+    }
+  }
+  return(output_list)
+}
