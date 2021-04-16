0b7527d1d4912bd3e16cda80fc6e1103fac45fb6
Neves-P <p.m.santos.neves@rug.nl>
2021-04-16 19:32:54 +0200

    Non-grouped plot displays N

--- scripts/plots/plot_error_jitters.R
+++ scripts/plots/plot_error_jitters.R
@@ -1,7 +1,7 @@
 plot_error_jitters <- function(error_metrics_list,
-                             error = "spec_nltt",
-                             error_metrics_names,
-                             x_axis_text) {
+                               error = "spec_nltt",
+                               error_metrics_names,
+                               x_axis_text) {
   error_metrics_sizes <- sapply(X = error_metrics_list, FUN = length)
   larger_vector_size <- max(error_metrics_sizes)
   for (i in seq_along(error_metrics_list)) {
@@ -33,18 +33,27 @@ plot_error_jitters <- function(error_metrics_list,
     error_metrics_names
   )
 
-  p <- ggplot2::ggplot(data = data, ggplot2::aes(y = value, x = key, color = key)) +
-    ggplot2::theme_bw() +
-    ggplot2::geom_jitter() +
-    ggplot2::ylim(0, 1) +
-    ggplot2::geom_hline(yintercept = 0.05, linetype = "dashed", size = 0.5) +
-    ggplot2::ylab(error_label) +
-    ggplot2::xlab(x_axis_text) +
-    ggplot2::theme(axis.title.y = ggplot2::element_text(size = 10)) +
-    ggplot2::theme(axis.title.x = ggplot2::element_blank()) +
-    ggplot2::theme(axis.text.y = ggplot2::element_text(size = 8)) +
-    ggplot2::theme(axis.text.x = ggplot2::element_text()) +
-    ggplot2::theme(legend.position = "none") +
-    ggplot2::theme(plot.margin = ggplot2::margin(6, 0.2, 6, 0.2))
-  p
-}
+
+  # Compute N per strip and make it into legend
+  data_no_na <- na.omit(data)
+  n_df <- data_no_na %>% dplyr::group_by(key) %>%
+    dplyr::tally()
+  label_ns <- c()
+  label_ns <- paste0(error_metrics_names, "\nN = ", as.character(n_df$n))
+
+
+    p <- ggplot2::ggplot(data = data, ggplot2::aes(y = value, x = key, color = key)) +
+      ggplot2::theme_bw() +
+      ggplot2::geom_jitter() +
+      ggplot2::ylim(0, 1) +
+      ggplot2::geom_hline(yintercept = 0.05, linetype = "dashed", size = 0.5) +
+      ggplot2::ylab(error_label) +
+      ggplot2::scale_x_discrete(labels = label_ns) +
+      ggplot2::theme(axis.title.y = ggplot2::element_text(size = 8)) +
+      ggplot2::theme(axis.title.x = ggplot2::element_blank()) +
+      ggplot2::theme(axis.text.x = ggplot2::element_text(size = 8)) +
+      # ggplot2::theme(axis.text.x = ggplot2::element_text()) +
+      ggplot2::theme(legend.position = "none") +
+      ggplot2::theme(plot.margin = ggplot2::margin(6, 0.2, 6, 0.2))
+    p
+  }
