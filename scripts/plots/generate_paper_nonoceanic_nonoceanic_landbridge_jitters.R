4f424b6834689c009f2db8036c8d45387d6653e1
Neves-P <p.m.santos.neves@rug.nl>
2021-04-16 20:04:02 +0200

    Fix nonoceanic_lb

--- scripts/plots/generate_paper_nonoceanic_nonoceanic_landbridge_jitters.R
+++ scripts/plots/generate_paper_nonoceanic_nonoceanic_landbridge_jitters.R
@@ -1,51 +1,87 @@
-# Boxplots for nonoceanic vs nonoceanic_land_bridge for
-# Neves et al 2021
+# Strip charts for nonoceanic_land_bridge lb effect for Neves et al 2021
 source("scripts/plots/compile_stat_diffs.R")
-source("scripts/plots/generate_paper_jitter_nonoceanic.R")
-source("scripts/plots/plot_error_jitters_nonoceanic.R")
-
-nonoceanic_stat_diff <- compile_stat_diffs(
-  scenario = "nonoceanic",
-  chunk_size = 48,
-  total_length = 576
+source("scripts/plots/plot_error_jitters_grouped.R")
+source("scripts/plots/generate_paper_jitter_plots.R")
+source("scripts/plots/save_faceted.R")
+nonoceanic_land_bridge_stat_diff <- compile_stat_diffs(
+  scenario = "nonoceanic_land_bridge",
+  chunk_size = 32,
+  total_length = 256
 )
 
-nonoceanic_metrics_names <- c(
-  "l_l",
-  "h_l",
-  "l_h",
-  "h_h",
-  "l_l",
-  "h_l",
-  "l_h",
-  "h_h",
-  "l_l",
-  "h_l",
-  "l_h",
-  "h_h"
+nonoceanic_land_bridge_metrics_names <- c(
+  "l",
+  "h",
+  "l",
+  "h",
+  "l",
+  "h",
+  "l",
+  "h"
 )
 
-xlabel_vec_nonoceanic <- c(expression(atop(Low~x[s], Low~x[n])),
-                           expression(atop(High~x[s], Low~x[n])),
-                           expression(atop(Low~x[s], High~x[n])),
-                           expression(atop(High~x[s], High~x[n])))
-
-nonoceanic_plots <- generate_paper_jitter_nonoceanic(
-  list_to_plot = nonoceanic_stat_diff$stat_diffs,
-  error_metrics_names = nonoceanic_metrics_names,
-  x_axis_text = "Non-oceanic sampling parameters",
-  scenario = "nonoceanic",
-  xlabels = xlabel_vec_nonoceanic,
-  save = TRUE
+xlabel_vec <- c("2",
+                "10",
+                "2",
+                "10")
+nonoceanic_land_bridge_plots <- generate_paper_jitter_plots(
+  list_to_plot = nonoceanic_land_bridge_stat_diff$stat_diffs,
+  error_metrics_names = nonoceanic_land_bridge_metrics_names,
+  x_axis_text = "Land-bridge immigration multiplier",
+  scenario = "nonoceanic_land_bridge",
+  xlabels = xlabel_vec,
+  save = TRUE,
+  add_plot_title = TRUE
 )
 
 
-
-# Faceted
+# Figure out ylim
+plot_1_ylim <-
+  ggplot2::layer_scales(nonoceanic_land_bridge_plots[[1]])$y$range$range[2]
+plot_2_ylim <-
+  ggplot2::layer_scales(nonoceanic_land_bridge_plots[[2]])$y$range$range[2]
+plot_3_ylim <-
+  ggplot2::layer_scales(nonoceanic_land_bridge_plots[[3]])$y$range$range[2]
+faceted_ylim <- max(plot_1_ylim, plot_2_ylim, plot_3_ylim)
 prow <- cowplot::plot_grid(
-  nonoceanic_plots[[1]] + ggplot2::theme(legend.position = "none", axis.title.x = ggplot2::element_blank()) + ggplot2::theme(plot.margin = ggplot2::margin(6, 2, 6, 2)),
-  nonoceanic_plots[[2]] + ggplot2::theme(legend.position = "none", axis.title.x = ggplot2::element_blank()) + ggplot2::theme(plot.margin = ggplot2::margin(6, 2, 6, 2)),
-  nonoceanic_plots[[3]] + ggplot2::theme(legend.position = "none", axis.title.x = ggplot2::element_blank()) + ggplot2::theme(plot.margin = ggplot2::margin(6, 2, 6, 2)),
+  nonoceanic_land_bridge_plots[[1]] + ggplot2::theme(
+    legend.position = "none",
+    axis.title.x = ggplot2::element_blank(),
+    plot.margin = ggplot2::margin(6, 1.5, 6, 1.5)) +
+    ggplot2::ylim(0, faceted_ylim),
+  nonoceanic_land_bridge_plots[[2]] + ggplot2::theme(
+    legend.position = "none",
+    axis.title.x = ggplot2::element_blank(),
+    plot.margin = ggplot2::margin(6, 1.5, 6, 1.5)) +
+    ggplot2::ylim(0, faceted_ylim),
+  nonoceanic_land_bridge_plots[[3]] + ggplot2::theme(
+    legend.position = "none",
+    axis.title.x = ggplot2::element_blank(),
+    plot.margin = ggplot2::margin(6, 1.5, 6, 1.5)) +
+    ggplot2::ylim(0, faceted_ylim),
+  align = 'vh',
+  labels = c("a", "b", "c"),
+  hjust = -0.2,
+  nrow = 1
+)
+
+# Figure out ylim
+plot_1_ylim <-
+  ggplot2::layer_scales(nonoceanic_land_bridge_plots[[4]])$y$range$range[2]
+plot_2_ylim <-
+  ggplot2::layer_scales(nonoceanic_land_bridge_plots[[5]])$y$range$range[2]
+faceted_ylim <- max(plot_1_ylim, plot_2_ylim)
+prow_spec_col <- cowplot::plot_grid(
+  nonoceanic_land_bridge_plots[[4]] + ggplot2::theme(
+    legend.position = "none",
+    axis.title.x = ggplot2::element_blank(),
+    plot.margin = ggplot2::margin(6, 1.5, 6, 1.5)) +
+    ggplot2::ylim(0, faceted_ylim),
+  nonoceanic_land_bridge_plots[[5]] + ggplot2::theme(
+    legend.position = "none",
+    axis.title.x = ggplot2::element_blank(),
+    plot.margin = ggplot2::margin(6, 1.5, 6, 1.5)) +
+    ggplot2::ylim(0, faceted_ylim),
   align = 'vh',
   labels = c("a", "b", "c"),
   hjust = -0.2,
@@ -54,29 +90,35 @@ prow <- cowplot::plot_grid(
 
 legend <- cowplot::get_legend(
   # create some space to the left of the legend
-  nonoceanic_plots[[1]] + ggplot2::theme(legend.box.margin = ggplot2::margin(0, 0, 0, 4))
+  nonoceanic_land_bridge_plots[[1]] + ggplot2::theme(legend.box.margin = ggplot2::margin(0, 0, 0, 2))
 )
 
-nonoceanic_facet <- cowplot::plot_grid(
-  prow,
-  legend,
-  rel_widths = c(3, 0.4)
-)
+final_land_bridge <- cowplot::plot_grid(prow, legend, rel_widths = c(3, 0.6))
+final_land_bridge_spec_col <- cowplot::plot_grid(prow_spec_col, legend, rel_widths = c(3, 0.6))
 
-final_nonoceanic_facet <- cowplot::add_sub(
-  nonoceanic_facet,
-  "Non-oceanic sampling parameters",
+final_land_bridge <- cowplot::add_sub(
+  final_land_bridge,
+  "Land-bridge immigration multiplier",
   vpadding = grid::unit(0, "lines"),
   y = 5,
   x = 0.5,
   vjust = 4.5,
-  size = 8
+  size = 10
 )
 
+final_land_bridge_spec_col <- cowplot::add_sub(
+  final_land_bridge_spec_col,
+  "Land-bridge immigration multiplier",
+  vpadding = grid::unit(0, "lines"),
+  y = 5,
+  x = 0.5,
+  vjust = 4.5,
+  size = 10
+)
 
 ggplot2::ggsave(
-  plot = final_nonoceanic_facet,
-  filename = "nonoceanic_fact.png",
+  plot = final_land_bridge,
+  filename = "nonoceanic_lb_facet.png",
   device = "png",
   width = 168,
   height = 100,
@@ -84,8 +126,26 @@ ggplot2::ggsave(
   dpi = 600
 )
 ggplot2::ggsave(
-  plot = final_nonoceanic_facet,
-  filename = "nonoceanic_fact.eps",
+  plot = final_land_bridge,
+  filename = "nonoceanic_lb_facet.eps",
+  device = "eps",
+  width = 168,
+  height = 100,
+  units = "mm"
+)
+
+ggplot2::ggsave(
+  plot = final_land_bridge_spec_col,
+  filename = "nonoceanic_lb_spec_col_facet.png",
+  device = "png",
+  width = 168,
+  height = 100,
+  units = "mm",
+  dpi = 600
+)
+ggplot2::ggsave(
+  plot = final_land_bridge_spec_col,
+  filename = "nonoceanic_lb_spec_col_facet.eps",
   device = "eps",
   width = 168,
   height = 100,
