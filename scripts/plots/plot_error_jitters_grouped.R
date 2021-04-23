d9aaaa54a73fc4b9358b62a2cc8cc04ba88da15f
Neves-P <p.m.santos.neves@rug.nl>
2021-04-23 17:11:45 +0200

    Pre-process data in tidy_data

--- scripts/plots/plot_error_jitters_grouped.R
+++ scripts/plots/plot_error_jitters_grouped.R
@@ -1,35 +1,23 @@
-plot_error_jitters_grouped <- function(error_metrics_list,
-                                       error = "spec_nltt",
-                                       error_metrics_names,
+plot_error_jitters_grouped <- function(error = "spec_nltt",
                                        xlabels,
                                        x_axis_text,
                                        scenario,
                                        save,
-                                       n_ages = 2,
                                        add_plot_title = TRUE) {
 
-  error_metrics_sizes <- sapply(X = error_metrics_list, FUN = length)
-  larger_vector_size <- max(error_metrics_sizes)
-  for (i in seq_along(error_metrics_list)) {
-    n_elements_to_append <- larger_vector_size - length(error_metrics_list[[i]])
 
-    error_metrics_list[[i]] <- c(
-      error_metrics_list[[i]],
-      rep(NA, n_elements_to_append)
-    )
-  }
 
   # y axis labels
   if (error == "spec_nltt") {
-    error_label <- expression(P[95] ~ Delta * "STT")
+    error_label <- expression(ED[95] ~ Delta * "STT")
   } else if (error == "endemic_nltt") {
-    error_label <- expression(P[95] ~ Delta * "ESTT")
+    error_label <- expression(ED[95] ~ Delta * "ESTT")
   } else if (error == "nonendemic_nltt") {
-    error_label <- expression(P[95] ~ Delta * "NESTT")
+    error_label <- expression(ED[95] ~ Delta * "NESTT")
   } else if (error == "num_spec") {
-    error_label <- expression(P[95] * " Species Number")
+    error_label <- expression(ED[95] * " Species Number")
   } else if (error == "num_col") {
-    error_label <- expression(P[95] * " Colonist Number")
+    error_label <- expression(ED[95] * " Colonist Number")
   }
 
   # Title
@@ -45,33 +33,7 @@ plot_error_jitters_grouped <- function(error_metrics_list,
     plot_title <- "Non-oceanic land-bridge"
   }
 
-  data <- as.data.frame(do.call(cbind, error_metrics_list))
-  data <- tidyr::gather(data)
-  pars_scenario <- unlist(lapply(X = error_metrics_names,
-                                 FUN = rep,
-                                 larger_vector_size))
-  data$key <- factor(pars_scenario,
-                     ordered = TRUE,
-                     levels = unique(pars_scenario)
-  )
-  if (n_ages == 2) {
-    Island <- factor(c(rep("Young", length(pars_scenario) / 2),
-                       rep("Old", length(pars_scenario) / 2)),
-                     levels = c("Young", "Old"),
-                     ordered = FALSE)
-  } else if (n_ages == 3) {
-    Island <- factor(c(rep("Young", length(pars_scenario) / 3),
-                       rep("Old", length(pars_scenario) / 3),
-                       rep("Ancient", length(pars_scenario) / 3)),
-                     levels = c("Young", "Old", "Ancient"),
-                     ordered = FALSE)
-  }
-  data <- cbind(data, Island)
 
-  # Compute N per strip and make it into legend
-  data_no_na <- na.omit(data)
-  n_df <- data_no_na %>% dplyr::group_by(key, Island) %>%
-    dplyr::tally() %>% dplyr::arrange(Island)
   label_ns <- c()
   for (i in seq_along(unique(n_df$key))) {
     matched_ns <- as.character(n_df$n[which(n_df$key[i] == n_df$key)])
