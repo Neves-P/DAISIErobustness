094f60d2d9b9a42804717b06bf6a74dcf9017d03
Neves-P <p.m.santos.neves@rug.nl>
2021-04-23 17:11:16 +0200

    Resize histogram

--- scripts/plots/generate_paper_histogram.R
+++ scripts/plots/generate_paper_histogram.R
@@ -1,11 +1,11 @@
 # Barplots for Neves et al 2021
 source("scripts/plots/plot_error_dists.R")
 # Nonoceanic 13 - hide legend
-load("G:/Shared drives/DAISIE-RUG/Josh_Pedro_paper/results/nonoceanic/nonoceanic_param_set_13.RData")
+load("G:/Discos partilhados/DAISIE-RUG/Josh_Pedro_paper/results/nonoceanic/nonoceanic_param_set_13.RData")
 barplot_13 <- plot_error_dists(output, "spec_nltt")
 
 # Nonoceanic 161
-load("G:/Shared drives/DAISIE-RUG/Josh_Pedro_paper/results/nonoceanic/nonoceanic_param_set_161.RData")
+load("G:/Discos partilhados/DAISIE-RUG/Josh_Pedro_paper/results/nonoceanic/nonoceanic_param_set_161.RData")
 barplot_161 <- plot_error_dists(output, "spec_nltt")
 
 # Arrange in grid, hide legends
@@ -13,7 +13,7 @@ side_by_side_plot <- cowplot::plot_grid(
   barplot_13 + ggplot2::theme(legend.position = "none"),
   barplot_161 +  ggplot2::theme(legend.position = "none"),
   ncol = 2,
-  labels = "AUTO"
+  labels = c("(a)", "(b)")
 )
 
 ## Make legend
@@ -32,18 +32,18 @@ side_by_side_plot_legend <- cowplot::plot_grid(
 # Save plot
 ggplot2::ggsave(
   plot = side_by_side_plot_legend,
-  filename = "hist_spec_nltt_nonoceanic_13_161.tif",
-  device = "tiff",
-  width = 5.2,
-  height = 2.9,
-  dpi = 300,
-  compression = "lzw"
+  filename = "hist_spec_nltt_nonoceanic_13_161.eps",
+  device = "eps",
+  width = 168,
+  height = 100,
+  units = "mm"
 )
 ggplot2::ggsave(
   plot = side_by_side_plot_legend,
   filename = "hist_spec_nltt_nonoceanic_13_161.png",
   device = "png",
-  width = 5.2,
-  height = 2.9,
-  dpi = 300
+  width = 168,
+  height = 100,
+  units = "mm",
+  dpi = 600
 )
