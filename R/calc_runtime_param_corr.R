3805a453a7ec3b31557379b097c925db67af0261
Neves-P <p.m.santos.neves@rug.nl>
2021-07-01 16:37:25 +0200

    Fix doc

--- R/calc_runtime_param_corr.R
+++ R/calc_runtime_param_corr.R
@@ -1,11 +1,11 @@
 #' Calculate correlation between parameters
 #'
-#' @param logs_folder_path
+#' @inheritParams default_params_doc
 #'
 #' @return
 #' @export
 #'
 #' @examples
-calc_runtime_param_corr <- function(logs_folder_path) {
-  jobs_runtime <- get_runtime_params(logs_folder_path = logs_folder_path)
+calc_runtime_param_corr <- function(jobs_runtime) {
+
 }
