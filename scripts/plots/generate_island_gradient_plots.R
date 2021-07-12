66e96252fbe7806a389cafb20de7617dd40e9cbc
Neves-P <p.m.santos.neves@rug.nl>
2021-07-12 16:13:38 +0200

    Save island gradient plots correctly

--- scripts/plots/generate_island_gradient_plots.R
+++ scripts/plots/generate_island_gradient_plots.R
@@ -1,8 +1,10 @@
 # Strip charts for oceanic sea level gradient param spaces for Neves et al 2021
+# Fig S5-S8
 source("scripts/plots/compile_ed95s.R")
 source("scripts/plots/plot_error_stripchart_grouped.R")
 source("scripts/plots/generate_paper_stripchart_plots.R")
 source("scripts/plots/save_faceted.R")
+source("scripts/plots/tidy_data.R")
 
 oceanic_sea_level_ed95 <- compile_ed95s(
   scenario = "oceanic_sea_level",
@@ -41,8 +43,44 @@ oceanic_ontongeny_sea_level_plots <- generate_paper_stripchart_plots(
   scenario = "oceanic_ontogeny_sea_level",
   xlabels = xlabel_vec,
   save = FALSE,
-   add_plot_title = FALSE
+  add_plot_title = FALSE
 )
 
 saved_faceted(oceanic_ontongeny_sea_level_plots[1], oceanic_ontongeny_sea_level_plots[2], oceanic_ontongeny_sea_level_plots[3], sub_text = "Island gradient")
+ggplot2::ggsave(
+  plot = oceanic_ontongeny_sea_level_plots[[4]],
+  filename = paste0("Island_gradient_ontogeny_sea_level_num_spec.png"),
+  device = "png",
+  width = 168,
+  height = 100,
+  units = "mm",
+  dpi = 600
+)
+ggplot2::ggsave(
+  plot = oceanic_ontongeny_sea_level_plots[[4]],
+  filename = paste0("Island_gradient_ontogeny_sea_level_num_spec.pdf"),
+  device = "pdf",
+  width = 168,
+  height = 100,
+  units = "mm"
+)
+
 saved_faceted(oceanic_sea_level_plots[1], oceanic_sea_level_plots[2], oceanic_sea_level_plots[3], sub_text = "Island gradient")
+ggplot2::ggsave(
+  plot = oceanic_sea_level_plots[[4]],
+  filename = paste0("Island_gradient_sea_level_num_spec.png"),
+  device = "png",
+  width = 168,
+  height = 100,
+  units = "mm",
+  dpi = 600
+)
+ggplot2::ggsave(
+  plot = oceanic_sea_level_plots[[4]],
+  filename = paste0("Island_gradient_sea_level_num_spec.pdf"),
+  device = "pdf",
+  width = 168,
+  height = 100,
+  units = "mm"
+)
+
