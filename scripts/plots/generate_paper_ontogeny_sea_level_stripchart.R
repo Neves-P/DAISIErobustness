3d90830881f7c9e1c068ac9b6f7dee1f75a91bd0
Neves-P <p.m.santos.neves@rug.nl>
2021-07-12 16:11:10 +0200

    stat_diff -> ed95

--- scripts/plots/generate_paper_ontogeny_sea_level_stripchart.R
+++ scripts/plots/generate_paper_ontogeny_sea_level_stripchart.R
@@ -1,25 +1,25 @@
 # Strip charts for oceanic param spaces for Neves et al 2021
+# Fig 5 and Fig S1-4
 source("scripts/plots/compile_ed95s.R")
 source("scripts/plots/plot_error_stripchart_grouped.R")
 source("scripts/plots/generate_paper_stripchart_plots.R")
 source("scripts/plots/save_faceted.R")
 
-oceanic_ontogeny_stat_diff <- compile_ed95s(
+oceanic_ontogeny_ed95 <- compile_ed95s(
   scenario = "oceanic_ontogeny",
   chunk_size = 48,
   total_length = 384
 )
-oceanic_sea_level_stat_diff <- compile_ed95s(
+oceanic_sea_level_ed95 <- compile_ed95s(
   scenario = "oceanic_sea_level",
   chunk_size = 48,
   total_length = 768
 )
-oceanic_ontogeny_sea_level_stat_diff <- compile_ed95s(
+oceanic_ontogeny_sea_level_ed95 <- compile_ed95s(
   scenario = "oceanic_ontogeny_sea_level",
   chunk_size = 48,
   total_length = 768
 )
-
 
 oceanic_ontogeny_metrics_names <- c(
   "l_l",
@@ -57,15 +57,16 @@ xlabel_vec <- c("Low x  \nLow d",
 
 
 oceanic_sea_level_plots <- generate_paper_stripchart_plots(
-  list_to_plot = oceanic_sea_level_stat_diff$stat_diffs,
+  list_to_plot = oceanic_sea_level_ed95$ed95s,
   error_metrics_names = oceanic_sea_level_metrics_names,
   x_axis_text = "Hyperparameters",
   scenario = "oceanic_sea_level",
   xlabels = xlabel_vec,
   save = FALSE
 )
+
 oceanic_ontogeny_plots <- generate_paper_stripchart_plots(
-  list_to_plot = oceanic_ontogeny_stat_diff$stat_diffs,
+  list_to_plot = oceanic_ontogeny_ed95$ed95s,
   error_metrics_names = oceanic_ontogeny_metrics_names,
   x_axis_text = "Hyperparameters",
   scenario = "oceanic_ontogeny",
@@ -74,7 +75,7 @@ oceanic_ontogeny_plots <- generate_paper_stripchart_plots(
 )
 
 oceanic_ontogeny_sea_level_plots <- generate_paper_stripchart_plots(
-  list_to_plot = oceanic_ontogeny_sea_level_stat_diff$stat_diffs,
+  list_to_plot = oceanic_ontogeny_sea_level_ed95$ed95s,
   error_metrics_names = oceanic_sea_level_metrics_names,
   x_axis_text = "Hyperparameters",
   scenario = "oceanic_ontogeny_sea_level",
