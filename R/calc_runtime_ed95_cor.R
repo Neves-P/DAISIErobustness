0a545a801263e90a913ca8a9ad2e21fbfc0fa650
Neves-P <p.m.santos.neves@rug.nl>
2021-07-15 18:01:09 +0200

    Calcs correlations for all params spaces

--- R/calc_runtime_ed95_cor.R
+++ R/calc_runtime_ed95_cor.R
@@ -17,49 +17,40 @@ calc_runtime_ed95_cor <- function(runtime_params, ed95_param_sets) {
                          "nonoceanic",
                          "nonoceanic_land_bridge")
   out <- list()
-  for (i in seq_along(param_space_names)) {
 
-    param_space_data_frame <- subset(
-      merged_data_frame,
-      merged_data_frame$param_space_name == param_space_names[i]
-    )
-    # plot
+  spec_nltt_cor <- stats::cor(
+    merged_data_frame$ed95_spec_nltt,
+    merged_data_frame$runtime,
+    use = "complete.obs"
+  )
+  endemic_nltt_cor <- stats::cor(
+    merged_data_frame$ed95_endemic_nltt,
+    merged_data_frame$runtime,
+    use = "complete.obs"
+  )
+  nonendemic_nltt_cor <- stats::cor(
+    merged_data_frame$ed95_nonendemic_nltt,
+    merged_data_frame$runtime,
+    use = "complete.obs"
+  )
+  num_spec_cor <- stats::cor(
+    merged_data_frame$ed95_num_spec,
+    merged_data_frame$runtime,
+    use = "complete.obs"
+  )
+  num_col_cor <- stats::cor(
+    merged_data_frame$ed95_num_col,
+    merged_data_frame$runtime,
+    use = "complete.obs"
+  )
 
-    spec_nltt_cor <- stats::cor(
-      param_space_data_frame$ed95_spec_nltt,
-      param_space_data_frame$runtime,
-      use = "complete.obs"
-    )
-    endemic_nltt_cor <- stats::cor(
-      param_space_data_frame$ed95_endemic_nltt,
-      param_space_data_frame$runtime,
-      use = "complete.obs"
-    )
-    nonendemic_nltt_cor <- stats::cor(
-      param_space_data_frame$ed95_nonendemic_nltt,
-      param_space_data_frame$runtime,
-      use = "complete.obs"
-    )
-    num_spec_cor <- stats::cor(
-      param_space_data_frame$ed95_num_spec,
-      param_space_data_frame$runtime,
-      use = "complete.obs"
-    )
-    num_col_cor <- stats::cor(
-      param_space_data_frame$ed95_num_col,
-      param_space_data_frame$runtime,
-      use = "complete.obs"
-    )
+  out <- data.frame(
+    spec_nltt_cor,
+    endemic_nltt_cor,
+    nonendemic_nltt_cor,
+    num_spec_cor,
+    num_col_cor
+  )
 
-    out[[i]] <- data.frame(
-      spec_nltt_cor,
-      endemic_nltt_cor,
-      nonendemic_nltt_cor,
-      num_spec_cor,
-      num_col_cor
-    )
-
-  }
-  names(out) <- param_space_names
   out
 }
