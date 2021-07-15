24f11a36398eb10fa296c3005f9b55842049eb7b
Neves-P <p.m.santos.neves@rug.nl>
2021-07-15 18:52:05 +0200

    Fix reference with log file name

--- tests/testthat/test-get_runtime_params.R
+++ tests/testthat/test-get_runtime_params.R
@@ -7,7 +7,15 @@ test_that("get_runtime_params works", {
       rep("nonoceanic_land_bridge", 2), rep("oceanic_sea_level", 4)
     ),
     "param_set" = c(92, 123, 237, 279, 473, 569),
-    "runtime" = c(137675, 83973, 272609, 211851, 348261, 369715)
+    "runtime" = c(137675, 83973, 272609, 211851, 348261, 369715),
+    "log_name" = c(
+      "robustness-20595773.log",
+      "robustness-20595804.log",
+      "robustness-20596177.log",
+      "robustness-20596219.log",
+      "robustness-20596414.log",
+      "robustness-20596511.log"
+    )
   )
   expect_equal(runtime_params, expected)
 })
