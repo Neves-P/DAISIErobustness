9dfa4ebdc16e1011bf4fe44ed674263b5bcc1037
Neves-P <p.m.santos.neves@rug.nl>
2021-07-21 21:28:31 +0200

    Revert changes

--- R/extract_param_set.R
+++ R/extract_param_set.R
@@ -10,31 +10,31 @@ extract_param_set <- function(param_space_name,
                               param_set) {
   sim_pars <- list()
   if (param_space_name %in% c("nonoceanic", "oceanic")) {
-    sim_pars$time <- as.numeric(param_space$time[param_set])
-    sim_pars$M <- as.numeric(param_space$M[param_set]) #nolint
-    sim_pars$pars <- as.numeric(c(param_space$lac[param_set],
-                                  param_space$mu[param_set],
-                                  param_space$K[param_set],
-                                  param_space$gam[param_set],
-                                  param_space$laa[param_set]))
-    sim_pars$nonoceanic_pars <- as.numeric(c(param_space$x_s[param_set],
-                                             param_space$x_nonend[param_set]))
+    sim_pars$time <- param_space$time[param_set]
+    sim_pars$M <- param_space$M[param_set] #nolint
+    sim_pars$pars <- c(param_space$lac[param_set],
+                       param_space$mu[param_set],
+                       param_space$K[param_set],
+                       param_space$gam[param_set],
+                       param_space$laa[param_set])
+    sim_pars$nonoceanic_pars <- c(param_space$x_s[param_set],
+                                  param_space$x_nonend[param_set])
   }
   if (param_space_name == "nonoceanic_land_bridge") {
-    sim_pars$time <- as.numeric(param_space$time[param_set])
-    sim_pars$M <- as.numeric(param_space$M[param_set]) #nolint
-    sim_pars$pars <- as.numeric(c(param_space$lac_1[param_set],
-                                  param_space$mu_1[param_set],
-                                  param_space$K_1[param_set],
-                                  param_space$gam_1[param_set],
-                                  param_space$laa_1[param_set],
-                                  param_space$lac_2[param_set],
-                                  param_space$mu_2[param_set],
-                                  param_space$K_2[param_set],
-                                  param_space$gam_2[param_set],
-                                  param_space$laa_2[param_set]))
-    sim_pars$nonoceanic_pars <- as.numeric(c(param_space$x_s[param_set],
-                                             param_space$x_nonend[param_set]))
+    sim_pars$time <- param_space$time[param_set]
+    sim_pars$M <- param_space$M[param_set] #nolint
+    sim_pars$pars <- c(param_space$lac_1[param_set],
+                       param_space$mu_1[param_set],
+                       param_space$K_1[param_set],
+                       param_space$gam_1[param_set],
+                       param_space$laa_1[param_set],
+                       param_space$lac_2[param_set],
+                       param_space$mu_2[param_set],
+                       param_space$K_2[param_set],
+                       param_space$gam_2[param_set],
+                       param_space$laa_2[param_set])
+    sim_pars$nonoceanic_pars <- c(param_space$x_s[param_set],
+                                  param_space$x_nonend[param_set])
 
     sim_pars$shift_times <-
       eval(str2expression(param_space$shift_times[param_set]))
@@ -44,50 +44,46 @@ extract_param_set <- function(param_space_name,
     "oceanic_sea_level",
     "oceanic_ontogeny_sea_level"
   )) {
-    sim_pars$time <- as.numeric(param_space$time[param_set])
-    sim_pars$M <- as.numeric(param_space$M[param_set]) #nolint
-    sim_pars$pars <- as.numeric(c(param_space$lac[param_set],
-                                  param_space$mu[param_set],
-                                  param_space$K[param_set],
-                                  param_space$gam[param_set],
-                                  param_space$laa[param_set]))
-    sim_pars$nonoceanic_pars <- as.numeric(c(param_space$x_s[param_set],
-                                             param_space$x_nonend[param_set]))
-    sim_pars$island_ontogeny <- as.character(
-      param_space$island_ontogeny[param_set])
-    sim_pars$sea_level <- as.character(param_space$sea_level[param_set])
+    sim_pars$time <- param_space$time[param_set]
+    sim_pars$M <- param_space$M[param_set] #nolint
+    sim_pars$pars <- c(param_space$lac[param_set],
+                       param_space$mu[param_set],
+                       param_space$K[param_set],
+                       param_space$gam[param_set],
+                       param_space$laa[param_set])
+    sim_pars$nonoceanic_pars <- c(param_space$x_s[param_set],
+                                  param_space$x_nonend[param_set])
+    sim_pars$island_ontogeny <- param_space$island_ontogeny[param_set]
+    sim_pars$sea_level <- param_space$sea_level[param_set]
     sim_pars$area_pars <- DAISIE::create_area_pars(
-      max_area = as.numeric(param_space$max_area[param_set]),
-      current_area = as.numeric(param_space$current_area[param_set]),
-      proportional_peak_t = as.numeric(param_space$peak_time[param_set]),
-      total_island_age = as.numeric(param_space$total_island_age[param_set]),
-      sea_level_amplitude = as.numeric(
-        param_space$sea_level_amplitude[param_set]),
-      sea_level_frequency = as.numeric(
-        param_space$sea_level_frequency[param_set]),
-      island_gradient_angle = as.numeric(
-        param_space$island_gradient_angle[param_set]))
+      max_area = param_space$max_area[param_set],
+      current_area = param_space$current_area[param_set],
+      proportional_peak_t = param_space$peak_time[param_set],
+      total_island_age = param_space$total_island_age[param_set],
+      sea_level_amplitude = param_space$sea_level_amplitude[param_set],
+      sea_level_frequency = param_space$sea_level_frequency[param_set],
+      island_gradient_angle = param_space$island_gradient_angle[param_set])
     sim_pars$hyper_pars <- DAISIE::create_hyper_pars(
-      d = as.numeric(param_space$d[param_set]),
-      x = as.numeric(param_space$x[param_set]))
-    sim_pars$extcutoff <- as.numeric(param_space$extcutoff[param_set])
+      d = param_space$d[param_set],
+      x = param_space$x[param_set])
+    sim_pars$extcutoff <- param_space$extcutoff[param_set]
   }
   if (param_space_name %in% c("trait_CES", "trait_trans")) {
-    sim_pars$time <- as.numeric(param_space$time[param_set])
-    sim_pars$M <- as.numeric(param_space$M[param_set]) #nolint
-    sim_pars$pars <- as.numeric(c(param_space$lac[param_set],
-                                  param_space$mu[param_set],
-                                  param_space$K[param_set],
-                                  param_space$gam[param_set],
-                                  param_space$laa[param_set]))
+    sim_pars$time <- param_space$time[param_set]
+    sim_pars$M <- param_space$M[param_set] #nolint
+    sim_pars$pars <- c(param_space$lac[param_set],
+                       param_space$mu[param_set],
+                       param_space$K[param_set],
+                       param_space$gam[param_set],
+                       param_space$laa[param_set])
     sim_pars$trait_pars <- list(
-      trans_rate = as.numeric(param_space$trans[param_set]),
-      immig_rate2 = as.numeric(param_space$gam2[param_set]),
-      ext_rate2 = as.numeric(param_space$mu2[param_set]),
-      ana_rate2 = as.numeric(param_space$laa2[param_set]),
-      clado_rate2 = as.numeric(param_space$lac2[param_set]),
-      trans_rate2 = as.numeric(param_space$trans2[param_set]),
-      M2 = as.numeric(param_space$M2[param_set])
+      trans_rate = param_space$trans[param_set],
+      immig_rate2 = param_space$gam2[param_set],
+      ext_rate2 = param_space$mu2[param_set],
+      ana_rate2 = param_space$laa2[param_set],
+      clado_rate2 = param_space$lac2[param_set],
+      trans_rate2 = param_space$trans2[param_set],
+      M2 = param_space$M2[param_set]
     )
   }
   return(sim_pars)
