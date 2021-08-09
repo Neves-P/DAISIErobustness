27773a3ce6e94e2b574f604ea91e1360f1d8205b
Neves-P <p.m.santos.neves@rug.nl>
2021-08-09 18:14:04 +0200

    immigration multiplier -> colonisation multiplier

--- scripts/plots/nonoceanic_land_bridge_multiplier_stripchart.R
+++ scripts/plots/nonoceanic_land_bridge_multiplier_stripchart.R
@@ -1,10 +1,10 @@
 # Strip charts for nonoceanic_land_bridge effect for Neves et al 2021
 # Fig 7 and Fig S13-S14
-source("scripts/plots/compile_ed95s.R")
-source("scripts/plots/plot_error_stripchart_grouped.R")
-source("scripts/plots/generate_paper_stripchart_plots.R")
-source("scripts/plots/save_faceted.R")
-source("scripts/plots/tidy_data.R")
+source("scripts/plots/functions/compile_ed95s.R")
+source("scripts/plots/functions/plot_error_stripchart_grouped.R")
+source("scripts/plots/functions/generate_paper_stripchart_plots.R")
+source("scripts/plots/functions/save_faceted.R")
+source("scripts/plots/functions/tidy_data.R")
 nonoceanic_land_bridge_stat_diff <- compile_ed95s(
   scenario = "nonoceanic_land_bridge",
   chunk_size = 32,
@@ -29,10 +29,10 @@ xlabel_vec <- c("2",
 nonoceanic_land_bridge_plots <- generate_paper_stripchart_plots(
   list_to_plot = nonoceanic_land_bridge_stat_diff$ed95s,
   error_metrics_names = nonoceanic_land_bridge_metrics_names,
-  x_axis_text = "Land-bridge immigration multiplier",
+  x_axis_text = "Land-bridge colonisation multiplier",
   scenario = "nonoceanic_land_bridge",
   xlabels = xlabel_vec,
-  save = FALSE,
+  save = TRUE,
   add_plot_title = FALSE
 )
 
@@ -41,5 +41,5 @@ saved_faceted(
   nonoceanic_land_bridge_plots[1],
   nonoceanic_land_bridge_plots[2],
   nonoceanic_land_bridge_plots[3],
-  "Land-bridge immigration multiplier"
+  "Land-bridge colonisation multiplier"
 )
