4129c7206eb37452905127c354fecfcc6854e967
Neves-P <p.m.santos.neves@rug.nl>
2021-11-29 13:47:19 +0100

    Install from DAISIE@onto_IW

--- bash/install_DAISIErobustness.sh
+++ bash/install_DAISIErobustness.sh
@@ -10,4 +10,4 @@
 mkdir -p logs
 mkdir -p results
 ml R
-Rscript -e "remotes::install_github('Neves-P/DAISIErobustness@revision_rates')"
+Rscript -e "remotes::install_github('Neves-P/DAISIErobustness@onto_IW')"
