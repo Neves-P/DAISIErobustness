0134f1c7ad521046359693cd90eda9dbd8749855
Neves-P <p.m.santos.neves@rug.nl>
2021-12-02 16:51:00 +0100

    assign eval() without docall

--- R/load_param_space.R
+++ R/load_param_space.R
@@ -6,12 +6,11 @@
 #' @export
 load_param_space <- function(param_space_name) {
   testit::assert(is_param_space_name(param_space_name))
-  do.call(
-    assign,
-    list(
-      "param_space",
-      eval(str2expression(paste0("DAISIErobustness::", param_space_name)))
-    )
-  )
+
+  assign(list(
+    "param_space",
+    eval(str2expression(paste0("DAISIErobustness::", param_space_name)))
+  ))
+
   return(param_space)
 }
