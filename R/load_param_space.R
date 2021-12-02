c68e5a72da03a3b387952e552730226541566581
Neves-P <p.m.santos.neves@rug.nl>
2021-12-02 16:58:23 +0100

    Use get0()

--- R/load_param_space.R
+++ R/load_param_space.R
@@ -7,8 +7,6 @@
 load_param_space <- function(param_space_name) {
   testit::assert(is_param_space_name(param_space_name))
 
-  param_space <- eval(str2expression(paste0(
-    "DAISIErobustness::", param_space_name
-  )))
+  param_space <- get0(param_space_name, envir = asNamespace("DAISIErobustness"))
   return(param_space)
 }
