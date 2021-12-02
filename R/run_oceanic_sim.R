f1e5cc61d676f89877f53c8c1ee813c9b8af6c40
Joshua Lambert <j.w.l.lambert@rug.nl>
2021-12-02 12:17:08 +0100

    simplified run_oceanic_sim

--- R/run_oceanic_sim.R
+++ R/run_oceanic_sim.R
@@ -6,31 +6,21 @@
 run_oceanic_sim <- function(ml,
                             sim_pars) {
   if ("trait_pars" %in% names(sim_pars)) {
-    oceanic_sim <- DAISIE::DAISIE_sim_cr(
-      time = sim_pars$time,
-      M = sim_pars$M + sim_pars$trait_pars$M2, # nolint
-      pars = as.numeric(ml[1:5]),
-      hyper_pars = DAISIE::create_hyper_pars(d = 0, x = 0),
-      replicates = 1,
-      divdepmodel = sim_pars$divdepmodel,
-      plot_sims = FALSE,
-      cond = 5,
-      verbose = FALSE,
-      sample_freq = Inf
-    )
+    mainland_species <- sim_pars$M + sim_pars$trait_pars$M2 # nolint
   } else {
-    oceanic_sim <- DAISIE::DAISIE_sim_cr(
-      time = sim_pars$time,
-      M = sim_pars$M,
-      pars = as.numeric(ml[1:5]),
-      hyper_pars = DAISIE::create_hyper_pars(d = 0, x = 0),
-      replicates = 1,
-      divdepmodel = sim_pars$divdepmodel,
-      plot_sims = FALSE,
-      cond = 5,
-      verbose = FALSE,
-      sample_freq = Inf
-    )
+    mainland_species <- sim_pars$M
   }
+  oceanic_sim <- DAISIE::DAISIE_sim_cr(
+    time = sim_pars$time,
+    M = mainland_species,
+    pars = as.numeric(ml[1:5]),
+    hyper_pars = DAISIE::create_hyper_pars(d = 0, x = 0),
+    replicates = 1,
+    divdepmodel = sim_pars$divdepmodel,
+    plot_sims = FALSE,
+    cond = 5,
+    verbose = FALSE,
+    sample_freq = Inf
+  )
   return(oceanic_sim)
 }
