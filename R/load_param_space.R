8a33e38f01f7588eeed8552cc13fec5aabc3864b
Neves-P <p.m.santos.neves@rug.nl>
2021-12-02 16:53:29 +0100

    Standard assign

--- R/load_param_space.R
+++ R/load_param_space.R
@@ -7,10 +7,8 @@
 load_param_space <- function(param_space_name) {
   testit::assert(is_param_space_name(param_space_name))
 
-  assign(list(
-    "param_space",
-    eval(str2expression(paste0("DAISIErobustness::", param_space_name)))
-  ))
-
+  param_space <- eval(str2expression(paste0(
+    "DAISIErobustness::", param_space_name
+  )))
   return(param_space)
 }
