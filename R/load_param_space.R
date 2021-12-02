c1a56c3b6d46a65ebf76f4903004f16c904c57a8
Joshua Lambert <j.w.l.lambert@rug.nl>
2021-12-02 12:16:51 +0100

    updated load_param_space

--- R/load_param_space.R
+++ R/load_param_space.R
@@ -6,7 +6,9 @@
 #' @export
 load_param_space <- function(param_space_name) {
   testit::assert(is_param_space_name(param_space_name))
-  param_space_path <- file.path("data", paste0(param_space_name, ".rda"))
+  param_space_path <- file.path(system.file(package = "DAISIErobustness"),
+                                "data",
+                                paste0(param_space_name, ".rda"))
   param_space <- get(load(param_space_path))
   return(param_space)
 }
