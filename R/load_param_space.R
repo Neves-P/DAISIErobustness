4b109d891f99bfa5cdebbf999871d6cd66a80bee
Neves-P <p.m.santos.neves@rug.nl>
2021-12-02 23:55:21 +0100

    try loading from inst/extdata/

--- R/load_param_space.R
+++ R/load_param_space.R
@@ -7,8 +7,9 @@
 load_param_space <- function(param_space_name) {
   testit::assert(is_param_space_name(param_space_name))
 
-  param_space <- eval(str2expression(paste0(
-    "DAISIErobustness:::", param_space_name
-  )))
+  param_space <- readRDS(system.file(
+    "extdata", paste0(param_space_name, ".rda", package = "DAISIErobustness")
+  ))
+
   return(param_space)
 }
