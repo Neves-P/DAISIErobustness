cf7f257fb15aad377d7fbed21f84f522559c6593
Neves-P <p.m.santos.neves@rug.nl>
2021-12-03 01:32:21 +0100

    Fix test-novel_sim references

--- tests/testthat/test-novel_sim.R
+++ tests/testthat/test-novel_sim.R
@@ -34,7 +34,7 @@ test_that("test run_novel_sim output is correct for
             skip_if(Sys.getenv("CI") == "", message = "Run only on CI")
 
             param_space <- load_param_space(
-              param_space_name = "oceanic_sea_level")
+              param_space_name = "oceanic_sea_level_cs")
             set.seed(
               1,
               kind = "Mersenne-Twister",
@@ -42,21 +42,21 @@ test_that("test run_novel_sim output is correct for
               sample.kind = "Rejection"
             )
             sim_pars <- extract_param_set(
-              param_space_name = "oceanic_sea_level",
+              param_space_name = "oceanic_sea_level_cs",
               param_space = param_space,
               param_set = 1)
             novel_sim <- run_novel_sim(
-              param_space_name = "oceanic_sea_level",
+              param_space_name = "oceanic_sea_level_cs",
               sim_pars = sim_pars)
             expect_length(novel_sim, 1)
             expect_length(novel_sim[[1]], 38)
-            expect_equal(novel_sim[[1]][[1]]$island_age, 2.55)
-            expect_equal(novel_sim[[1]][[1]]$not_present, 963)
-            expect_equal(nrow(novel_sim[[1]][[1]]$stt_all), 115)
+            expect_equal(novel_sim[[1]][[1]]$island_age, 5)
+            expect_equal(novel_sim[[1]][[1]]$not_present, 969)
+            expect_equal(nrow(novel_sim[[1]][[1]]$stt_all), 94)
             expect_equal(ncol(novel_sim[[1]][[1]]$stt_all), 5)
             expect_equal(novel_sim[[1]][[2]]$branching_times,
-                         c(2.55, 0.78784699357233001))
-            expect_equal(novel_sim[[1]][[2]]$stac, 4)
+                         c(5.0, 0.3414342016913))
+            expect_equal(novel_sim[[1]][[2]]$stac, 2)
             expect_equal(novel_sim[[1]][[2]]$missing_species, 0)
 })
 
@@ -111,13 +111,13 @@ test_that("test run_novel_sim output is correct for
               param_space_name = "nonoceanic_cs",
               sim_pars = sim_pars)
             expect_length(novel_sim, 1)
-            expect_length(novel_sim[[1]], 19)
+            expect_length(novel_sim[[1]], 25)
             expect_equal(novel_sim[[1]][[1]]$island_age, 2.55)
-            expect_equal(novel_sim[[1]][[1]]$not_present, 982)
-            expect_equal(nrow(novel_sim[[1]][[1]]$stt_all), 83)
+            expect_equal(novel_sim[[1]][[1]]$not_present, 976)
+            expect_equal(nrow(novel_sim[[1]][[1]]$stt_all), 51)
             expect_equal(ncol(novel_sim[[1]][[1]]$stt_all), 5)
             expect_equal(novel_sim[[1]][[2]]$branching_times,
-                         c(2.55, 0.52495868988335004))
+                         c(2.55, 2.55))
             expect_equal(novel_sim[[1]][[2]]$stac, 2)
             expect_equal(novel_sim[[1]][[2]]$missing_species, 0)
 })
@@ -142,13 +142,13 @@ test_that("test run_novel_sim output is correct for
               param_space_name = "nonoceanic_land_bridge_cs",
               sim_pars = sim_pars)
             expect_length(novel_sim, 1)
-            expect_length(novel_sim[[1]], 23)
+            expect_length(novel_sim[[1]], 24)
             expect_equal(novel_sim[[1]][[1]]$island_age, 2.55)
-            expect_equal(novel_sim[[1]][[1]]$not_present, 978)
-            expect_equal(nrow(novel_sim[[1]][[1]]$stt_all), 82)
+            expect_equal(novel_sim[[1]][[1]]$not_present, 977)
+            expect_equal(nrow(novel_sim[[1]][[1]]$stt_all), 81)
             expect_equal(ncol(novel_sim[[1]][[1]]$stt_all), 5)
             expect_equal(novel_sim[[1]][[2]]$branching_times,
-                         c(2.55, 2.09983694478869, 2.09199464856839))
+                         c(2.55, 2.09983694478869, 2.09179048465453))
             expect_equal(novel_sim[[1]][[2]]$stac, 2)
             expect_equal(novel_sim[[1]][[2]]$missing_species, 0)
 })
@@ -175,10 +175,10 @@ test_that("test run_novel_sim output is correct for trait", {
   expect_length(novel_sim[[1]], 32)
   expect_equal(novel_sim[[1]][[1]]$island_age, 5)
   expect_equal(novel_sim[[1]][[1]]$not_present, 969)
-  expect_equal(nrow(novel_sim[[1]][[1]]$stt_all), 79)
+  expect_equal(nrow(novel_sim[[1]][[1]]$stt_all), 94)
   expect_equal(ncol(novel_sim[[1]][[1]]$stt_all), 5)
   expect_equal(novel_sim[[1]][[2]]$branching_times,
-               c(5.0, 0.94991737976669999))
-  expect_equal(novel_sim[[1]][[2]]$stac, 4)
+               c(5.0, 0.3414342016913))
+  expect_equal(novel_sim[[1]][[2]]$stac, 2)
   expect_equal(novel_sim[[1]][[2]]$missing_species, 0)
 })
