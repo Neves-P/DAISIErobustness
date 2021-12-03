b2b3ddf576cd95c2f3271b4d90cb61dfd240bcde
Neves-P <p.m.santos.neves@rug.nl>
2021-12-03 20:18:26 +0100

    Fix paths

--- tests/testthat/test-plot_cor.R
+++ tests/testthat/test-plot_cor.R
@@ -1,11 +1,11 @@
 test_that("Plot works", {
   skip_if(Sys.getenv("CI") == "", message = "Run only on CI")
   runtime_params <- get_runtime_params(
-    file.path("testdata", "logs"
+    file.path("testdata", "logs")
   )
 
   results_total <- calc_ed95_param_set(
-    file.path("testdata", "results"
+    file.path("testdata", "results")
   )
 
   runtime_pars <- merge(
@@ -15,10 +15,10 @@ test_that("Plot works", {
   plot_cor(runtime_pars)
   expect_known_output(
     "runtime_ed95_corr.pdf",
-    "testdata/plots/runtime_ed95_corr.pdf"
+    file.path("testdata", "plots", "runtime_ed95_corr.pdf")
   )
   expect_known_output(
     "runtime_ed95_corr.png",
-    "testdata/plots/runtime_ed95_corr.png"
+    file.path("testdata", "plots", "runtime_ed95_corr.png")
   )
 })
