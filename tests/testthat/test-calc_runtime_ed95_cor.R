594397e3213fd38ecc5d70a3cae8e14ef14b0f11
Neves-P <p.m.santos.neves@rug.nl>
2021-12-03 15:57:23 +0100

    Update test-calc_runtime_ed955_cor references

--- tests/testthat/test-calc_runtime_ed95_cor.R
+++ tests/testthat/test-calc_runtime_ed95_cor.R
@@ -1,10 +1,10 @@
 test_that("calc_runtime_ed95_cor works", {
   skip_if(Sys.getenv("CI") == "", message = "Run only on CI")
   skip("Needs new reference files from runs")
-  runtime_params <- get_runtime_params(file.path(getwd(), "testdata/logs"))
+  runtime_params <- get_runtime_params(file.path("testdata", "logs"))
 
   ed95 <- suppressMessages(
-    calc_ed95_param_set(file.path(getwd(), "testdata/results/"))
+    calc_ed95_param_set(file.path("testdata", "results"))
   )
 
   runtime_ed95_cor <- calc_runtime_ed95_cor(
@@ -14,11 +14,11 @@ test_that("calc_runtime_ed95_cor works", {
 
 
   expected_output <- data.frame(
-    spec_nltt_cor = -0.8637637401186395,
-    endemic_nltt_cor = -0.8579914245548418,
-    nonendemic_nltt_cor = -0.8578290635889865,
-    num_spec_cor = -0.1319857989792762,
-    num_col_cor = 0.2975526496905813
+    spec_nltt_cor = 0.291921902389,
+    endemic_nltt_cor = -0.0777961487649631,
+    nonendemic_nltt_cor = 0.0191562137145573 ,
+    num_spec_cor = 0.0865181123855519,
+    num_col_cor = 0.199292921739181
   )
 
   expect_equal(runtime_ed95_cor, expected_output)
