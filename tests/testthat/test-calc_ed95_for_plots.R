83a334baf0f98fa5a97feed92c35e9923b647a1f
Neves-P <p.m.santos.neves@rug.nl>
2021-12-03 20:03:38 +0100

    Update referene

--- tests/testthat/test-calc_ed95_for_plots.R
+++ tests/testthat/test-calc_ed95_for_plots.R
@@ -1,18 +1,17 @@
 test_that("calc_ed95_for_plots works", {
   skip_if(Sys.getenv("CI") == "", message = "Run only on CI")
-  skip("Needs new reference files from runs")
   ed95 <- suppressMessages(
     calc_ed95_for_plots(
-      file.path(getwd(), "testdata/results/nonoceanic_land_bridge/")
+      file.path("testdata", "results", "oceanic_sea_level_cs")
     )
   )
 
   expected_output <- list(
-    ed95_spec_nltt = c(0.996003996003996, 0.999000999000999),
-    ed95_endemic_nltt = c(0.977022977022977, 0.974025974025974),
-    ed95_nonendemic_nltt = c(0.997002997002997, 0.998001998001998),
-    ed95_num_spec = c(0.05394605394605394, 0.04995004995004995),
-    ed95_num_col = c(0.02797202797202797, 0.04495504495504495)
+    ed95_spec_nltt = c(0.0784313725490196, 0.1960784313725490),
+    ed95_endemic_nltt = c(0.1176470588235294, 0.0784313725490196),
+    ed95_nonendemic_nltt = c(0.117647058823529, 0.196078431372549),
+    ed95_num_spec = c(0.0588235294117647, 0.2352941176470588),
+    ed95_num_col = c(0.0588235294117647, 0.1764705882352941)
   )
 
   expect_equal(ed95, expected_output)
