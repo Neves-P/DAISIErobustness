4880cfae1b5a82063d7e112fefe33dc3a0200c87
Neves-P <p.m.santos.neves@rug.nl>
2021-12-03 16:56:50 +0100

    Update reference

--- tests/testthat/test-get_runtime_params.R
+++ tests/testthat/test-get_runtime_params.R
@@ -1,21 +1,22 @@
 test_that("get_runtime_params works", {
   skip_if(Sys.getenv("CI") == "", message = "Run only on CI")
-  skip("Needs new reference files from runs")
 
-  runtime_params <- get_runtime_params(file.path(getwd(), "testdata/logs"))
+  runtime_params <- get_runtime_params(file.path(getwd(), "testdata", "logs"))
   expected <- data.frame(
     "param_space_name" = c(
-      rep("nonoceanic_land_bridge", 2), rep("oceanic_sea_level", 4)
+      rep("oceanic_ontogeny_sea_level_cs", 6), rep("oceanic_sea_level_cs", 2)
     ),
-    "param_set" = c(92, 123, 237, 279, 473, 569),
-    "runtime" = c(137675, 83973, 272609, 211851, 348261, 369715),
+    "param_set" = c(228, 230, 240, 251, 255, 483, 443, 445),
+    "runtime" = c(2748, 3757, 6379, 4641, 5326, 3713, 3303, 5834),
     "log_name" = c(
-      "robustness-20595773.log",
-      "robustness-20595804.log",
-      "robustness-20596177.log",
-      "robustness-20596219.log",
-      "robustness-20596414.log",
-      "robustness-20596511.log"
+      "robustness-22352504.log",
+      "robustness-22352506.log",
+      "robustness-22352516.log",
+      "robustness-22352527.log",
+      "robustness-22352531.log",
+      "robustness-22352759.log",
+      "robustness-22353749.log",
+      "robustness-22353751.log"
     )
   )
   expect_equal(runtime_params, expected)
