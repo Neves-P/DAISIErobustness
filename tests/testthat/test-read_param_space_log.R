4c41a2fdcf63581c1afc9aa9d112e2bd31eb5258
Neves-P <p.m.santos.neves@rug.nl>
2021-12-03 20:09:19 +0100

    Update reference and file

--- tests/testthat/test-read_param_space_log.R
+++ tests/testthat/test-read_param_space_log.R
@@ -1,16 +1,15 @@
 test_that("read_param_space_log works", {
   skip_if(Sys.getenv("CI") == "", message = "Run only on CI")
-  skip("Needs new reference files from runs")
   # Find test environment file path
   log_file_path <- list.files(
-    file.path(getwd(), "testdata/logs"),
+    file.path("testdata", "logs"),
     full.names = TRUE,
-    pattern = "robustness-20596511.log"
+    pattern = "robustness-22352504.log"
   )
 
   param_space_name <- read_param_space_name_log(
     log_file_path = log_file_path
   )
 
-  expect_equal(param_space_name, "oceanic_sea_level")
+  expect_equal(param_space_name, "oceanic_ontogeny_sea_level_cs")
 })
