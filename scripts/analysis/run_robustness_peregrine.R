b9339d57d5e2f79482e25854e13cc7f939e0fafc
Neves-P <p.m.santos.neves@rug.nl>
2021-12-06 16:11:26 +0100

    Tidy script

--- scripts/analysis/run_robustness_peregrine.R
+++ scripts/analysis/run_robustness_peregrine.R
@@ -5,15 +5,13 @@ param_set <- as.numeric(args[2])
 replicates <- as.numeric(args[3])
 distance_method <- args[4]
 save_output <- TRUE
-m <- 1000
-metadata <- paste0("This is parameter set ", param_set)
 
 library(DAISIErobustness)
 
 run_robustness(
-  param_space_name = args[1],
-  param_set = as.numeric(args[2]),
-  replicates = as.numeric(args[3]),
-  distance_method = args[4],
+  param_space_name = param_space_name,
+  param_set = param_set,
+  replicates = replicates,
+  distance_method = distance_method,
   save_output = save_output
 )
