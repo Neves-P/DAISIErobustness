c0347306b370ea6760a39360082b5fa2294c587c
Neves-P <p.m.santos.neves@rug.nl>
2021-12-07 23:06:38 +0100

    Better get_runtime_params()

--- R/get_runtime_params.R
+++ R/get_runtime_params.R
@@ -10,6 +10,7 @@
 #'   \item{\code{param_set}}{Atomic numeric vector with parameter space number.}
 #'   \item{\code{runtime}}{Atomic numeric vector with Peregrine job run time in
 #'   seconds.}
+#'   \item{\code{status}}{character with job status upon conclusion.}
 #'   \item{\code{log_name}}{Character vector with name of corresponding log file
 #'   to facilitate debugging.}
 #' }
@@ -22,28 +23,37 @@
 #' runtimes_data_frame <- get_runtime_params("logs/")
 #' }
 get_runtime_params <- function(logs_folder_path) {
-  log_files <- list.files(logs_folder_path, full.names = TRUE, pattern = ".log")
+  log_paths <- list.files(
+    logs_folder_path,
+    full.names = TRUE,
+    pattern = ".log"
+  )
 
   out <- data.frame(
     "param_space_name" = character(),
     "param_set" = numeric(),
     "runtime" = numeric(),
+    "status" = character(),
     "log_name" = character()
   )
 
-  for (i in seq_along(log_files)) {
-    param_space_name <- read_param_space_name_log(log_file_path = log_files[i])
-    param_set <- read_param_set_log(log_file_path = log_files[i])
-    runtime <- read_runtime_log(log_file_path = log_files[i])
+  for (i in seq_along(log_paths)) {
+    log_name <- basename(log_paths[i])
+
+    param_space_name <- read_param_space_name_log(log_file_path = log_paths[i])
+    param_set <- read_param_set_log(log_file_path = log_paths[i])
+    runtime <- read_runtime_log(log_file_path = log_paths[i])
+    status <- read_status_log(log_file_path = log_paths[i])
 
     out[i, "param_space_name"] <- param_space_name
     out[i, "param_set"] <- param_set
     out[i, "runtime"] <- runtime
-    out[i, "log_name"] <- basename(log_files[i])
+    out[i, "status"] <- status
+    out[i, "log_name"] <- log_name
   }
 
   out <- out[order(out$param_space_name, out$param_set), ]
-  testit::assert(nrow(out) == length(log_files))
+  testit::assert(nrow(out) == length(log_paths))
   testit::assert(is.data.frame(out))
   out
 }
