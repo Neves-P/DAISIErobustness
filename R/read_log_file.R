224bbd0349e8cbb50c1afc249bd793d83e66fb73
Neves-P <10762799+Neves-P@users.noreply.github.com>
2022-01-05 23:53:36 +0000

    Add read_log_file()

---
+++ R/read_log_file.R
@@ -0,0 +1,16 @@
+#' Read log file lines
+#'
+#' Simply sanitizes the input path and reads log files into a character vector.
+#'
+#' @param default_params_docs
+#'
+#' @return Character vector with the contents of the log file as returns,
+#'   \code{\link{readLines}()}.
+#' @export
+#'
+#' @author Pedro Neves
+read_log_file <- function(log_file_path) {
+  testit::assert(is.character(log_file_path) && length(log_file_path) > 0)
+  log_lines <- readLines(con = log_file_path, n = 2000)
+  return(log_lines)
+}
