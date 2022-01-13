8dcfa2008a6a1d3f139f1006a20263ff910610bd
Joshua Lambert <j.w.l.lambert@rug.nl>
2022-01-13 18:17:15 +0000

    removed problem test

--- tests/testthat/test-calc_ml.R
+++ tests/testthat/test-calc_ml.R
@@ -35,46 +35,6 @@ test_that("test calc_ml output is correct with geodynamic sim", {
   expect_equal(novel_ml$conv, 0)
 })
 
-test_that("test calc_ml output is correct with oceanic sim", {
-  skip_if(Sys.getenv("CI") == "", message = "Run only on CI")
-
-  param_space <- load_param_space(
-    param_space_name = "oceanic_ontogeny_cs")
-  novel_ml <- data.frame("lambda_c" = 1,
-                         "mu" = 1,
-                         "K" = 25,
-                         "gamma" = 0.01,
-                         "lambda_a" = 1,
-                         "loglik" = -90,
-                         "df" = 5,
-                         "conv" = 0)
-  sim_pars <- extract_param_set(
-    param_space_name = "oceanic_ontogeny_cs",
-    param_space = param_space,
-    param_set = 1)
-  set.seed(
-    1,
-    kind = "Mersenne-Twister",
-    normal.kind = "Inversion",
-    sample.kind = "Rejection"
-  )
-  oceanic_sim_1 <- run_oceanic_sim(
-    ml = novel_ml,
-    sim_pars = sim_pars)
-  oceanic_ml <- calc_ml(
-    sim = oceanic_sim_1,
-    initial_parameters = novel_ml)
-  expect_length(oceanic_ml, 8)
-  expect_equal(oceanic_ml$lambda_c, 1.27408536971302)
-  expect_equal(oceanic_ml$mu, 0.128003360531255)
-  expect_equal(oceanic_ml$K, 3.00002456076289)
-  expect_equal(oceanic_ml$gamma, 0.00468502700356342)
-  expect_equal(oceanic_ml$lambda_a, 1.25676620859537)
-  expect_equal(oceanic_ml$loglik, -84.1740838545781)
-  expect_equal(oceanic_ml$df, 5)
-  expect_equal(oceanic_ml$conv, 0)
-})
-
 test_that("test calc_ml output is correct for failed convergence", {
   skip("Optimize test - find non converging model")
   skip_if(Sys.getenv("CI") == "", message = "Run only on CI")
