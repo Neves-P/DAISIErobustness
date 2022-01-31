d5e17795f6f9ea1ad6f71eb0da8b1a8291020bba
Neves-P <p.m.santos.neves@rug.nl>
2022-01-31 11:42:01 +0100

    Update get_runtime_params() references

--- tests/testthat/test-get_runtime_params.R
+++ tests/testthat/test-get_runtime_params.R
@@ -5,17 +5,17 @@ test_that("get_runtime_params works", {
   expected <- data.frame(
     "param_space_name" = rep("continental_cs", 8),
     "param_set" = 1:8,
-    "runtime" = c(72, 52, 51, 81, 94, 77, 58, 60),
+    "runtime" = c(91, 119, 50, 83, 88, 88, 85, 127),
     "status" = rep("COMPLETED", 8),
     "log_name" = c(
-      "robustness-22656110.log",
-      "robustness-22656111.log",
-      "robustness-22656112.log",
-      "robustness-22656113.log",
-      "robustness-22656114.log",
-      "robustness-22656115.log",
-      "robustness-22656116.log",
-      "robustness-22656117.log"
+      "robustness-22891156.log",
+      "robustness-22891157.log",
+      "robustness-22891158.log",
+      "robustness-22891159.log",
+      "robustness-22891160.log",
+      "robustness-22891161.log",
+      "robustness-22891162.log",
+      "robustness-22891163.log"
     )
   )
   expect_equal(runtime_params, expected)
