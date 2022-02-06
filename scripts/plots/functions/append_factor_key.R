d2bd630000a71b581758cf478ae419a012dcdd0f
Neves-P <p.m.santos.neves@rug.nl>
2022-02-06 22:12:30 +0100

    Add sample_pars option

--- scripts/plots/functions/append_factor_key.R
+++ scripts/plots/functions/append_factor_key.R
@@ -1,5 +1,5 @@
 append_factor_key <- function(scenario_res, partition_by) {
-  if (partition_by == "hyperparameters") {
+  if (identical(partition_by, "hyperparameters")) {
     x <- scenario_res$x
     d <- scenario_res$d
 
@@ -17,12 +17,31 @@ append_factor_key <- function(scenario_res, partition_by) {
       all(as.character(key) == paste0(x_fac, d_fac))
     )
     out <- cbind(scenario_res, key)
-  } else if (partition_by == "gradient") {
+  } else if (identical(partition_by, "gradient")) {
     key <- factor(scenario_res$island_gradient_angle)
 
     testit::assert(
       "Key order is kept",
       all(key == scenario_res$island_gradient_angle)
+    )
+    out <- cbind(scenario_res, key)
+  } else if (identical(partition_by, "sample_parameters")) {
+    # TODO: Condense with x and d case in ontogeny?
+    x_s <- scenario_res$x_s
+    x_nonend <- scenario_res$x_nonend
+
+    testit::assert(length(unique(x_s)) == 2 && length(unique(x_nonend)) == 2)
+
+    x_s_fac <- factor(x_s, labels = c("l", "h"))
+    x_nonend_fac <- factor(x_nonend, labels = c("l", "h"))
+    key <- factor(
+      paste0(x_s_fac, x_nonend_fac),
+      ordered = FALSE,
+      levels = unique(paste0(x_s_fac, x_nonend_fac))
+    )
+    testit::assert(
+      "Key order is kept",
+      all(as.character(key) == paste0(x_s_fac, x_nonend_fac))
     )
     out <- cbind(scenario_res, key)
   }
