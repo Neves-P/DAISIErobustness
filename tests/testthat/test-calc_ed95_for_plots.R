14c33a0b6212580758d0a56a39d462033480cfd7
Neves-P <p.m.santos.neves@rug.nl>
2022-02-07 02:33:31 +0100

    Fix test

--- tests/testthat/test-calc_ed95_for_plots.R
+++ tests/testthat/test-calc_ed95_for_plots.R
@@ -1,18 +1,30 @@
 test_that("calc_ed95_for_plots works", {
   skip_if(Sys.getenv("CI") == "", message = "Run only on CI")
+  scenario <- load_param_space("continental_cs")
   ed95 <- suppressMessages(
     calc_ed95_for_plots(
-      file.path("testdata", "results", "continental_cs")
+      folder_path = file.path("testdata", "results", "continental_cs"),
+      scenario = scenario
     )
   )
-
-  expected_output <- list(
-    ed95_spec_nltt = c(1.00, 0.50, 0.75, 0.75, 1.00, 1.00, 0.75, 0.50),
-    ed95_endemic_nltt = c(1.00, 0.50, 0.75, 0.75, 1.00, 1.00, 1.00, 0.50),
-    ed95_nonendemic_nltt = c(0.50, 0.50, 1.00, 0.25, 0.25, 0.25, 0.25, 0.50),
-    ed95_num_spec = c(0.75, 0.25, 0.25, 0.25, 0.50, 1.00, 0.50, 0.50),
-    ed95_num_col = c(0.50, 0.25, 0.25, 0.50, 0.75, 0.75, 0.25, 0.25)
+  n_nas <- nrow(scenario) - 8
+  expected_output <- cbind(
+    scenario,
+    ed95_spec_nltt = c(
+      1.00, 0.50, 0.75, 0.75, 1.00, 1.00, 0.75, 0.50,
+      rep(NA, n_nas)),
+    ed95_endemic_nltt = c(
+      1.00, 0.50, 0.75, 0.75, 1.00, 1.00, 1.00, 0.50,
+      rep(NA, n_nas)),
+    ed95_nonendemic_nltt = c(
+      0.50, 0.50, 1.00, 0.25, 0.25, 0.25, 0.25, 0.50,
+      rep(NA, n_nas)),
+    ed95_num_spec = c(
+      0.75, 0.25, 0.25, 0.25, 0.50, 1.00, 0.50, 0.50,
+      rep(NA, n_nas)),
+    ed95_num_col = c(
+      0.50, 0.25, 0.25, 0.50, 0.75, 0.75, 0.25, 0.25,
+      rep(NA, n_nas))
   )
-
   expect_equal(ed95, expected_output)
 })
