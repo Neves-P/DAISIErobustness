84fcf4b7d38ac1ed02c21da5a524eb5fcc3b6add
Neves-P <p.m.santos.neves@rug.nl>
2022-02-07 18:11:14 +0100

    Add is_integration_error()

---
+++ R/is_integration_error.R
@@ -0,0 +1,12 @@
+#' Checks if log file shows integration error
+#'
+#'
+#' @inheritParams default_params_doc
+#'
+#' @return A boolean, \code{TRUE} if integration error occured, \code{FALSE} if
+#' not.
+#' @author Pedro Santos Neves
+#' @export
+is_integration_error_log <- function(log_lines) {
+  any(grepl("cpp_daisie_cs_runmod", log_lines))
+}
