0741d72a35ae755f2f669c817cbb1b73c6ae5b58
Neves-P <p.m.santos.neves@rug.nl>
2022-02-07 18:54:50 +0100

    Don't export is_integration_error_log()

--- R/is_integration_error.R
+++ R/is_integration_error.R
@@ -5,8 +5,8 @@
 #'
 #' @return A boolean, \code{TRUE} if integration error occured, \code{FALSE} if
 #' not.
+#' @keywords internal
 #' @author Pedro Santos Neves
-#' @export
 is_integration_error_log <- function(log_lines) {
   any(grepl("cpp_daisie_cs_runmod", log_lines))
 }
