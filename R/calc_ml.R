2bad422b95a14178f952f199e9a714fee59a82aa
Neves-P <p.m.santos.neves@rug.nl>
2022-02-07 18:10:57 +0100

    Suppress console output for log files

--- R/calc_ml.R
+++ R/calc_ml.R
@@ -16,45 +16,51 @@ calc_ml <- function(sim,
   DAISIE::DAISIE_CS_max_steps(1e8)
 
   if (optim_ana) {
-    ml <- DAISIE::DAISIE_ML_CS(
-      datalist = sim[[1]],
-      datatype = "single",
-      initparsopt = as.numeric(initial_parameters[1:5]),
-      idparsopt = c(1:5),
-      ddmodel = 11,
-      parsfix = NULL,
-      idparsfix = NULL,
-      cond = 5,
-      jitter = 1e-5,
-      methode = "odeint::runge_kutta_fehlberg78"
-    )
+    ml <- suppressMessages(invisible(capture.output(
+      DAISIE::DAISIE_ML_CS(
+        datalist = sim[[1]],
+        datatype = "single",
+        initparsopt = as.numeric(initial_parameters[1:5]),
+        idparsopt = c(1:5),
+        ddmodel = 11,
+        parsfix = NULL,
+        idparsfix = NULL,
+        cond = 5,
+        jitter = 1e-5,
+        methode = "odeint::runge_kutta_fehlberg78"
+      )
+    )))
   } else {
     fix_ana_zero <- all_endemic_clades(sim[[1]])
     if (fix_ana_zero) {
-      ml <- DAISIE::DAISIE_ML_CS(
-        datalist = sim[[1]],
-        datatype = "single",
-        initparsopt = as.numeric(initial_parameters[1:4]),
-        idparsopt = 1:4,
-        parsfix = 0,
-        idparsfix = 5,
-        cond = 5,
-        ddmodel = 11,
-        methode = "odeint::runge_kutta_fehlberg78",
-        jitter = 1e-5)
+      ml <- suppressMessages(invisible(capture.output(
+        DAISIE::DAISIE_ML_CS(
+          datalist = sim[[1]],
+          datatype = "single",
+          initparsopt = as.numeric(initial_parameters[1:4]),
+          idparsopt = 1:4,
+          parsfix = 0,
+          idparsfix = 5,
+          cond = 5,
+          ddmodel = 11,
+          methode = "odeint::runge_kutta_fehlberg78",
+          jitter = 1e-5)
+      )))
     } else {
-      ml <- DAISIE::DAISIE_ML_CS(
-        datalist = sim[[1]],
-        datatype = "single",
-        initparsopt = as.numeric(initial_parameters[1:4]),
-        idparsopt = 1:4,
-        parsfix = 100,
-        idparsfix = 5,
-        cond = 5,
-        ddmodel = 11,
-        methode = "odeint::runge_kutta_fehlberg78",
-        jitter = 1e-5)
+      ml <- suppressMessages(invisible(capture.output(
+        DAISIE::DAISIE_ML_CS(
+          datalist = sim[[1]],
+          datatype = "single",
+          initparsopt = as.numeric(initial_parameters[1:4]),
+          idparsopt = 1:4,
+          parsfix = 100,
+          idparsfix = 5,
+          cond = 5,
+          ddmodel = 11,
+          methode = "odeint::runge_kutta_fehlberg78",
+          jitter = 1e-5)
+      )))
+        }
     }
-  }
   return(ml)
 }
