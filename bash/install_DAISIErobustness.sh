28c0532f98df68cad6bdfe5a2854797d1809b38b
joshwlambert <j.w.l.lambert@rug.nl>
2022-02-07 12:08:13 +0100

    change install branch back to develop

--- bash/install_DAISIErobustness.sh
+++ bash/install_DAISIErobustness.sh
@@ -10,4 +10,4 @@
 mkdir -p logs
 mkdir -p results
 ml R
-Rscript -e "remotes::install_github('Neves-P/DAISIErobustness@rm_abs')"
+Rscript -e "remotes::install_github('Neves-P/DAISIErobustness@develop')"
